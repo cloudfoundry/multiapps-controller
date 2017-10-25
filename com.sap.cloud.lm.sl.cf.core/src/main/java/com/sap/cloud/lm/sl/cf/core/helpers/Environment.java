06b60e6bebd7681b928c996fdd44f718ece10720
Alexander Tsvetkov <alexander.tsvetkov.93@gmail.com>
2017-10-25 16:24:18 +0300

    Add hasVariable method in Environment class

--- com.sap.cloud.lm.sl.cf.core/src/main/java/com/sap/cloud/lm/sl/cf/core/helpers/Environment.java
+++ com.sap.cloud.lm.sl.cf.core/src/main/java/com/sap/cloud/lm/sl/cf/core/helpers/Environment.java
@@ -1,10 +1,16 @@
 package com.sap.cloud.lm.sl.cf.core.helpers;
 
+import com.sap.cloud.lm.sl.common.util.CommonUtil;
+
 // TODO: Use this class (instead of System.getenv()) in ConfigurationUtil, so that it can be tested.
 public class Environment {
 
     public String getVariable(String name) {
         return System.getenv(name);
+    }
+
+    public boolean hasVariable(String name) {
+        return !CommonUtil.isNullOrEmpty(System.getenv(name));
     }
 
 }
