f1530f3b459cd0312b693d81f46466f25c8eb81b
Alexander Tsvetkov <alexander.tsvetkov.93@gmail.com>
2017-11-16 15:04:45 +0200

    Remove unused methods in OperationFactory

--- com.sap.cloud.lm.sl.cf.core/src/main/java/com/sap/cloud/lm/sl/cf/core/helpers/OperationFactory.java
+++ com.sap.cloud.lm.sl.cf.core/src/main/java/com/sap/cloud/lm/sl/cf/core/helpers/OperationFactory.java
@@ -1,13 +1,10 @@
 package com.sap.cloud.lm.sl.cf.core.helpers;
 
-import java.text.MessageFormat;
 import java.time.ZonedDateTime;
 
 import org.springframework.stereotype.Component;
 
-import com.sap.cloud.lm.sl.cf.core.Constants;
 import com.sap.cloud.lm.sl.cf.core.dto.persistence.OperationDto;
-import com.sap.cloud.lm.sl.cf.core.message.Messages;
 import com.sap.cloud.lm.sl.cf.web.api.model.Operation;
 import com.sap.cloud.lm.sl.cf.web.api.model.ProcessType;
 import com.sap.cloud.lm.sl.cf.web.api.model.State;
@@ -55,38 +52,6 @@ public class OperationFactory {
 
     protected String toString(ZonedDateTime zonedDateTime) {
         return zonedDateTime == null ? null : Operation.DATE_TIME_FORMATTER.format(zonedDateTime);
-    }
-
-    protected String serializeProcessType(ProcessType processType) {
-        if (processType == null) {
-            return null;
-        }
-        if (processType.equals(ProcessType.DEPLOY)) {
-            return Constants.DEPLOY_SERIALIZED_NAME;
-        }
-        if (processType.equals(ProcessType.BLUE_GREEN_DEPLOY)) {
-            return Constants.BLUE_GREEN_DEPLOY_SERIALIZED_NAME;
-        }
-        if (processType.equals(ProcessType.UNDEPLOY)) {
-            return Constants.UNDEPLOY_SERIALIZED_NAME;
-        }
-        throw new IllegalStateException(MessageFormat.format(Messages.ILLEGAL_PROCESS_TYPE, processType.toString()));
-    }
-
-    protected ProcessType deserializeProcessType(String processType) {
-        if (processType == null) {
-            return null;
-        }
-        switch (processType) {
-            case Constants.DEPLOY_SERIALIZED_NAME:
-                return ProcessType.DEPLOY;
-            case Constants.BLUE_GREEN_DEPLOY_SERIALIZED_NAME:
-                return ProcessType.BLUE_GREEN_DEPLOY;
-            case Constants.UNDEPLOY_SERIALIZED_NAME:
-                return ProcessType.UNDEPLOY;
-            default:
-                throw new IllegalStateException(MessageFormat.format(Messages.ILLEGAL_PROCESS_TYPE, processType));
-        }
     }
 
     protected ProcessType toProcessType(String processType) {
