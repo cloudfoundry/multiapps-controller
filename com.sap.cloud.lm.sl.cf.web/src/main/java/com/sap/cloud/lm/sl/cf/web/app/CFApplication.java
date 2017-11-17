36d8202e09c14c858039672fd798873b2f8a761a
Mihail Tsonev <mihail.tsonev@sap.com>
2017-11-17 15:42:15 +0200

    Remove unused RolesResource

--- com.sap.cloud.lm.sl.cf.web/src/main/java/com/sap/cloud/lm/sl/cf/web/app/CFApplication.java
+++ com.sap.cloud.lm.sl.cf.web/src/main/java/com/sap/cloud/lm/sl/cf/web/app/CFApplication.java
@@ -8,14 +8,12 @@ import javax.ws.rs.core.Application;
 import com.sap.cloud.lm.sl.cf.web.resources.CFExceptionMapper;
 import com.sap.cloud.lm.sl.cf.web.resources.ConfigurationEntriesResource;
 import com.sap.cloud.lm.sl.cf.web.resources.CsrfTokenResource;
-import com.sap.cloud.lm.sl.cf.web.resources.RolesResource;
 
 public class CFApplication extends Application {
 
     @Override
     public Set<Class<?>> getClasses() {
         Set<Class<?>> classes = new HashSet<Class<?>>();
-        classes.add(RolesResource.class);
         classes.add(ConfigurationEntriesResource.class);
         classes.add(CFExceptionMapper.class);
         classes.add(CsrfTokenResource.class);
