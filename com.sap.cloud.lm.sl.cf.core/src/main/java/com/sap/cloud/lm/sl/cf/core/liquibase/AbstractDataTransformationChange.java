fdfc07e54fdf0c1400cc31fc7199ab458e15ac69
Alexander Tsvetkov <alexander.tsvetkov.93@gmail.com>
2017-11-18 15:10:40 +0200

    Make AbstractDataTransformationChange more generic
    Previously, AbstractDataTransformationChange was only usable if your
    table had a primary key of type long and you wanted to transform the
    data stored in a column of type string. The goal of this commit is to
    remove these limitations.
    
    JIRA: LMCROSSITXSADEPLOY-361

--- com.sap.cloud.lm.sl.cf.core/src/main/java/com/sap/cloud/lm/sl/cf/core/liquibase/AbstractDataTransformationChange.java
+++ com.sap.cloud.lm.sl.cf.core/src/main/java/com/sap/cloud/lm/sl/cf/core/liquibase/AbstractDataTransformationChange.java
@@ -3,64 +3,57 @@ package com.sap.cloud.lm.sl.cf.core.liquibase;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Map;
 
 import com.sap.cloud.lm.sl.persistence.util.JdbcUtil;
 
 import liquibase.database.jvm.JdbcConnection;
 import liquibase.exception.DatabaseException;
 
-public abstract class AbstractDataTransformationChange extends AbstractChange {
+public abstract class AbstractDataTransformationChange<OriginalDataType, TransformedDataType> extends AbstractChange {
 
     @Override
     protected void executeInTransaction(JdbcConnection jdbcConnection) throws Exception {
-        Map<Long, String> retrievedData = retrieveData(jdbcConnection);
-        Map<Long, String> transformedData = transformData(retrievedData);
+        OriginalDataType retrievedData = retrieveData(jdbcConnection);
+        TransformedDataType transformedData = transformData(retrievedData);
         updateTable(jdbcConnection, transformedData);
     }
 
-    public Map<Long, String> retrieveData(JdbcConnection jdbcConnection) throws DatabaseException, SQLException {
+    public OriginalDataType retrieveData(JdbcConnection jdbcConnection) throws DatabaseException, SQLException {
         PreparedStatement preparedStatement = null;
-        Map<Long, String> result = null;
+        OriginalDataType result = null;
 
         try {
             preparedStatement = jdbcConnection.prepareStatement(getSelectStatement());
             ResultSet resultSet = preparedStatement.executeQuery();
-            result = customExtractData(resultSet);
+            result = extractData(resultSet);
             resultSet.close();
-            logger.debug(String.format("Executed select for table '%s' returned '%s' entries", getTableName(), result.size()));
         } finally {
             JdbcUtil.closeQuietly(preparedStatement);
         }
         return result;
     }
 
-    public void updateTable(JdbcConnection jdbcConnection, Map<Long, String> transformedData) throws DatabaseException, SQLException {
+    public void updateTable(JdbcConnection jdbcConnection, TransformedDataType transformedData) throws DatabaseException, SQLException {
         PreparedStatement preparedStatement = null;
 
         try {
             preparedStatement = jdbcConnection.prepareStatement(getUpdateStatement());
-            for (Map.Entry<Long, String> entry : transformedData.entrySet()) {
-                customUpdate(preparedStatement, entry);
-            }
+            setUpdateStatementParameters(preparedStatement, transformedData);
             preparedStatement.executeBatch();
-            logger
-                .debug(String.format("Executed batch update for table '%s' and entries size '%s'", getTableName(), transformedData.size()));
         } finally {
             JdbcUtil.closeQuietly(preparedStatement);
         }
     }
 
-    public abstract Map<Long, String> transformData(Map<Long, String> retrievedData);
+    public abstract TransformedDataType transformData(OriginalDataType retrievedData);
 
-    public abstract Map<Long, String> customExtractData(ResultSet resultSet) throws SQLException;
-
-    public abstract String getTableName();
+    public abstract OriginalDataType extractData(ResultSet resultSet) throws SQLException;
 
     public abstract String getSelectStatement();
 
     public abstract String getUpdateStatement();
 
-    public abstract void customUpdate(PreparedStatement preparedStatement, Map.Entry<Long, String> entry) throws SQLException;
+    public abstract void setUpdateStatementParameters(PreparedStatement preparedStatement, TransformedDataType transformedData)
+        throws SQLException;
 
 }
