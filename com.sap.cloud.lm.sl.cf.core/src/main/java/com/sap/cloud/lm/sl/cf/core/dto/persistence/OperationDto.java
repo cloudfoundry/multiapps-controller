dfde66c649350ceded26267a3eec89b1c8a6ae20
Alexander Tsvetkov <alexander.tsvetkov.93@gmail.com>
2017-11-18 22:13:37 +0200

    Use predictable ordering in named queries for operations

--- com.sap.cloud.lm.sl.cf.core/src/main/java/com/sap/cloud/lm/sl/cf/core/dto/persistence/OperationDto.java
+++ com.sap.cloud.lm.sl.cf.core/src/main/java/com/sap/cloud/lm/sl/cf/core/dto/persistence/OperationDto.java
@@ -15,9 +15,9 @@ import javax.persistence.TemporalType;
 @Table(name = "operation")
 @NamedQueries({
     @NamedQuery(name = "find_mta_lock", query = "SELECT o FROM OperationDto o WHERE o.mtaId = :mtaId AND o.spaceId = :spaceId AND o.acquiredLock = true"),
-    @NamedQuery(name = "find_all", query = "SELECT o FROM OperationDto o"),
-    @NamedQuery(name = "find_all_in_space", query = "SELECT o FROM OperationDto o WHERE o.spaceId = :spaceId"),
-    @NamedQuery(name = "find_all_in_space_desc", query = "SELECT o FROM OperationDto o WHERE o.spaceId = :spaceId order by o.startedAt DESC"),
+    @NamedQuery(name = "find_all", query = "SELECT o FROM OperationDto o ORDER BY o.startedAt"),
+    @NamedQuery(name = "find_all_in_space", query = "SELECT o FROM OperationDto o WHERE o.spaceId = :spaceId ORDER BY o.startedAt"),
+    @NamedQuery(name = "find_all_in_space_desc", query = "SELECT o FROM OperationDto o WHERE o.spaceId = :spaceId ORDER BY o.startedAt DESC"),
     @NamedQuery(name = "find_all_active_in_space", query = "SELECT o FROM OperationDto o WHERE o.spaceId = :spaceId AND o.finalState is NULL"),
     @NamedQuery(name = "find_all_finished_in_space", query = "SELECT o FROM OperationDto o WHERE o.spaceId = :spaceId AND o.finalState is NOT NULL") })
 public class OperationDto {
