e811805cff5c6af1d482e895e926de219ea75bcd
Dimitar Donchev <dimitar.donchev@sap.com>
2017-11-21 14:12:56 +0200

    Configurable DB conection thread pool max sie
    Done via an environment variable DB_CONNECTION_THREADS.
    
    +Introduction of lambda argument mockito matcher
    
    BUG: NGPBUG-40375
    Change-Id: Ic0130943853399fc8dcb43a825e4942c53bac646

---
+++ com.sap.cloud.lm.sl.cf.core/src/test/java/com/sap/cloud/lm/sl/cf/core/util/LambdaArgumentMatcher.java
@@ -0,0 +1,19 @@
+package com.sap.cloud.lm.sl.cf.core.util;
+
+import java.util.function.Function;
+
+import org.mockito.ArgumentMatcher;
+
+public class LambdaArgumentMatcher<T> extends ArgumentMatcher<T> {
+
+    private Function<Object, Boolean> matcher;
+
+    public LambdaArgumentMatcher(java.util.function.Function<Object, Boolean> matcher) {
+        this.matcher = matcher;
+    }
+
+    @Override
+    public boolean matches(Object argument) {
+        return matcher.apply(argument);
+    }
+}
