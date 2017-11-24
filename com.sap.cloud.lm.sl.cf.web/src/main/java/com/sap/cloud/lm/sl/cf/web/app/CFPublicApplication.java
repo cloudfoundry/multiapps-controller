1f9320818aba4bcbf2b6991de34f77e1e5971d63
Alexander Tsvetkov <alexander.tsvetkov.93@gmail.com>
2017-11-24 14:19:39 +0200

    Implement health-check REST API
    JIRA: LMCROSSITXSADEPLOY-361

--- com.sap.cloud.lm.sl.cf.web/src/main/java/com/sap/cloud/lm/sl/cf/web/app/CFPublicApplication.java
+++ com.sap.cloud.lm.sl.cf.web/src/main/java/com/sap/cloud/lm/sl/cf/web/app/CFPublicApplication.java
@@ -5,6 +5,7 @@ import java.util.Set;
 
 import javax.ws.rs.core.Application;
 
+import com.sap.cloud.lm.sl.cf.web.resources.HealthCheckResource;
 import com.sap.cloud.lm.sl.cf.web.resources.PingResource;
 
 public class CFPublicApplication extends Application {
@@ -13,6 +14,7 @@ public class CFPublicApplication extends Application {
     public Set<Class<?>> getClasses() {
         Set<Class<?>> classes = new HashSet<Class<?>>();
         classes.add(PingResource.class);
+        classes.add(HealthCheckResource.class);
         return classes;
     }
 
