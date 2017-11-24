33044bcda8facbc2c1314ed879572b7e945b76d5
Alexander Tsvetkov <alexander.tsvetkov.93@gmail.com>
2017-11-24 14:19:06 +0200

    Implement HealthRetriever
    JIRA: LMCROSSITXSADEPLOY-361

---
+++ com.sap.cloud.lm.sl.cf.core/src/main/java/com/sap/cloud/lm/sl/cf/core/health/HealthRetriever.java
@@ -0,0 +1,53 @@
+package com.sap.cloud.lm.sl.cf.core.health;
+
+import java.time.ZonedDateTime;
+import java.util.Date;
+import java.util.List;
+import java.util.function.Supplier;
+
+import javax.inject.Inject;
+
+import org.springframework.stereotype.Component;
+
+import com.sap.cloud.lm.sl.cf.core.dao.OperationDao;
+import com.sap.cloud.lm.sl.cf.core.dao.filters.OperationFilter;
+import com.sap.cloud.lm.sl.cf.core.health.model.Health;
+import com.sap.cloud.lm.sl.cf.core.health.model.HealthCheckConfiguration;
+import com.sap.cloud.lm.sl.cf.core.util.Configuration;
+import com.sap.cloud.lm.sl.cf.web.api.model.Operation;
+
+@Component
+public class HealthRetriever {
+
+    private OperationDao operationDao;
+    private Configuration configuration;
+    private Supplier<ZonedDateTime> currentTimeSupplier;
+
+    @Inject
+    public HealthRetriever(OperationDao operationDao, Configuration configuration) {
+        this(operationDao, configuration, () -> ZonedDateTime.now());
+    }
+
+    protected HealthRetriever(OperationDao operationDao, Configuration configuration, Supplier<ZonedDateTime> currentTimeSupplier) {
+        this.operationDao = operationDao;
+        this.configuration = configuration;
+        this.currentTimeSupplier = currentTimeSupplier;
+    }
+
+    public Health getHealth() {
+        HealthCheckConfiguration healthCheckConfiguration = configuration.getHealthCheckConfiguration();
+        ZonedDateTime currentTime = currentTimeSupplier.get();
+        ZonedDateTime xSecondsAgo = currentTime.minusSeconds(healthCheckConfiguration.getTimeRangeInSeconds());
+        OperationFilter filter = new OperationFilter.Builder().mtaId(healthCheckConfiguration.getMtaId())
+            .spaceId(healthCheckConfiguration.getSpaceId())
+            .user(healthCheckConfiguration.getUserName())
+            .endedAfter(Date.from(xSecondsAgo.toInstant()))
+            .inFinalState()
+            .orderByEndTime()
+            .descending()
+            .build();
+        List<Operation> healthCheckOperations = operationDao.find(filter);
+        return Health.fromOperations(healthCheckOperations);
+    }
+
+}
