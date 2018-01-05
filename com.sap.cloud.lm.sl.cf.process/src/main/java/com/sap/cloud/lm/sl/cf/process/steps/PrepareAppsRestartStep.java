867f38b471399b771205714779c1cf054084dc23
Encho Belezirev <encho.belezirev@sap.com>
2018-01-05 16:46:57 +0200

    Fix restart of HDI modules after bg resume
    Change-Id: Ica03dc0c4496c05b1db59df713720203f9d48eec

--- com.sap.cloud.lm.sl.cf.process/src/main/java/com/sap/cloud/lm/sl/cf/process/steps/PrepareAppsRestartStep.java
+++ com.sap.cloud.lm.sl.cf.process/src/main/java/com/sap/cloud/lm/sl/cf/process/steps/PrepareAppsRestartStep.java
@@ -1,13 +1,7 @@
 package com.sap.cloud.lm.sl.cf.process.steps;
 
-import org.springframework.beans.factory.config.BeanDefinition;
-import org.springframework.context.annotation.Scope;
-import org.springframework.stereotype.Component;
-
 import com.sap.cloud.lm.sl.cf.process.Constants;
 
-@Component("prepareAppsRestartStep")
-@Scope(BeanDefinition.SCOPE_PROTOTYPE)
 public class PrepareAppsRestartStep extends PrepareAppsDeploymentStep {
 
     @Override
