6fdc70d420c71ffc0022091dea357c76ba864327
Dimitar Donchev <dimitar.donchev@sap.com>
2018-02-02 16:46:09 +0200

    deploy test tweaks -using spring music example content
    UNTRACKED

--- oq.tests/test_scenarios/CF Tests/CF Canary Tests/Sanity Check/2Deploy/env.sh
+++ oq.tests/test_scenarios/CF Tests/CF Canary Tests/Sanity Check/2Deploy/env.sh
@@ -5,3 +5,4 @@ if [ -f "${parent_dir}/env.sh" ] ; then
         source "${parent_dir}/env.sh";
 fi
 export STEP_PATH="${STEP_PATH}/Deploy"
+export ADDITIONAL_OPTIONS="-e ${TEST_WORKING_DIRECTORY}/${APP_LOCATION}/config.mtaext"
