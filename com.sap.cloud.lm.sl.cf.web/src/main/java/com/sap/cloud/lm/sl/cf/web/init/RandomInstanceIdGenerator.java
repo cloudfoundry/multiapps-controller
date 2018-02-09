4860392b0a4062c66b19b69ac5a9fde72464835d
Boyan Velinov <boyan.velinov@sap.com>
2018-02-09 17:54:27 +0200

    Quartz: Change instance id generation mechanism
    Jira: XSBUG-2603

---
+++ com.sap.cloud.lm.sl.cf.web/src/main/java/com/sap/cloud/lm/sl/cf/web/init/RandomInstanceIdGenerator.java
@@ -0,0 +1,15 @@
+package com.sap.cloud.lm.sl.cf.web.init;
+
+import java.util.UUID;
+
+import org.quartz.SchedulerException;
+import org.quartz.spi.InstanceIdGenerator;
+
+public class RandomInstanceIdGenerator implements InstanceIdGenerator {
+
+    @Override
+    public String generateInstanceId() throws SchedulerException {
+        UUID uuid = UUID.randomUUID();
+        return uuid.toString();
+    }
+}
