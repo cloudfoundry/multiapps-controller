e6a350d0f421576e48ffd5cd7cfc37f6c7e4e978
Encho Belezirev <encho.belezirev@sap.com>
2018-02-21 10:40:55 +0200

    Adopt the new timeout clients in the ExecutionWrapper
    Change-Id: I84d4fe699aa20e6412bd298397e2d84e29d35774

--- com.sap.cloud.lm.sl.cf.process/src/main/java/com/sap/cloud/lm/sl/cf/process/steps/ExecutionWrapper.java
+++ com.sap.cloud.lm.sl.cf.process/src/main/java/com/sap/cloud/lm/sl/cf/process/steps/ExecutionWrapper.java
@@ -4,6 +4,8 @@ import org.activiti.engine.delegate.DelegateExecution;
 import org.cloudfoundry.client.lib.CloudFoundryOperations;
 
 import com.sap.cloud.lm.sl.cf.client.ClientExtensions;
+import com.sap.cloud.lm.sl.cf.client.CloudFoundryOperationsExtended;
+import com.sap.cloud.lm.sl.cf.client.util.TimeoutExecutor;
 import com.sap.cloud.lm.sl.cf.core.cf.CloudFoundryClientProvider;
 import com.sap.cloud.lm.sl.cf.core.dao.ContextExtensionDao;
 import com.sap.cloud.lm.sl.cf.process.util.StepLogger;
@@ -35,19 +37,57 @@ public class ExecutionWrapper {
     }
 
     public CloudFoundryOperations getCloudFoundryClient() throws SLException {
+        CloudFoundryOperations cloudFoundryClientWithoutTimeout = getCloudFoundryClientWithoutTimeout();
+        decorateClient(cloudFoundryClientWithoutTimeout);
+        return cloudFoundryClientWithoutTimeout;
+    }
+
+    public CloudFoundryOperations getCloudFoundryClientWithoutTimeout() throws SLException {
         return StepsUtil.getCloudFoundryClient(context, clientProvider, stepLogger);
     }
 
     public CloudFoundryOperations getCloudFoundryClient(String org, String space) throws SLException {
+        CloudFoundryOperations cloudFoundryClientWithoutTimeout = getCloudFoundryClientWithoutTimeout(org, space);
+        decorateClient(cloudFoundryClientWithoutTimeout);
+        return cloudFoundryClientWithoutTimeout;
+    }
+
+    private void decorateClient(CloudFoundryOperations cloudfoundryOperations) {
+        if (cloudfoundryOperations instanceof CloudFoundryOperationsExtended) {
+            ((CloudFoundryOperationsExtended) cloudfoundryOperations).withTimeoutExecutor(TimeoutExecutor.getInstance());
+        }
+    }
+
+    public CloudFoundryOperations getCloudFoundryClientWithoutTimeout(String org, String space) throws SLException {
         return StepsUtil.getCloudFoundryClient(context, clientProvider, stepLogger, org, space);
     }
 
     public ClientExtensions getClientExtensions() throws SLException {
+        ClientExtensions clientExtensionsWithoutTimeout = getClientExtensionsWithoutTimeout();
+        decorateWithTimeout(clientExtensionsWithoutTimeout);
+        return clientExtensionsWithoutTimeout;
+    }
+
+    public ClientExtensions getClientExtensionsWithoutTimeout() throws SLException {
         return StepsUtil.getClientExtensions(context, clientProvider, stepLogger);
     }
 
     public ClientExtensions getClientExtensions(String org, String space) throws SLException {
+        ClientExtensions clientExtensionsWithoutTimeout = getClientExtensionsWithoutTimeout(org, space);
+        decorateWithTimeout(clientExtensionsWithoutTimeout);
+        return clientExtensionsWithoutTimeout;
+    }
+
+    public ClientExtensions getClientExtensionsWithoutTimeout(String org, String space) throws SLException {
         return StepsUtil.getClientExtensions(context, clientProvider, stepLogger, org, space);
+    }
+
+    private void decorateWithTimeout(ClientExtensions clientExtensions) {
+        if (clientExtensions == null) {
+            return;
+        }
+
+        clientExtensions.withTimeoutExecutor(TimeoutExecutor.getInstance());
     }
 
     public ProcessLoggerProviderFactory getProcessLoggerProviderFactory() {
