7a4cfb387aa00a84ee312814e9f79faf5d77d056
Kristian Shishoev <kshishoev@gmail.com>
2018-03-07 17:44:44 +0200

    Enhance customer data termination job to clean operations data

--- com.sap.cloud.lm.sl.cf.core/src/main/java/com/sap/cloud/lm/sl/cf/core/security/data/termination/DataTerminationService.java
+++ com.sap.cloud.lm.sl.cf.core/src/main/java/com/sap/cloud/lm/sl/cf/core/security/data/termination/DataTerminationService.java
@@ -3,6 +3,7 @@ package com.sap.cloud.lm.sl.cf.core.security.data.termination;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.inject.Inject;
 
@@ -17,11 +18,14 @@ import com.sap.cloud.lm.sl.cf.core.auditlogging.AuditLoggingProvider;
 import com.sap.cloud.lm.sl.cf.core.cf.clients.CFOptimizedEventGetter;
 import com.sap.cloud.lm.sl.cf.core.dao.ConfigurationEntryDao;
 import com.sap.cloud.lm.sl.cf.core.dao.ConfigurationSubscriptionDao;
+import com.sap.cloud.lm.sl.cf.core.dao.OperationDao;
+import com.sap.cloud.lm.sl.cf.core.dao.filters.OperationFilter;
 import com.sap.cloud.lm.sl.cf.core.message.Messages;
 import com.sap.cloud.lm.sl.cf.core.model.ConfigurationEntry;
 import com.sap.cloud.lm.sl.cf.core.model.ConfigurationSubscription;
 import com.sap.cloud.lm.sl.cf.core.util.Configuration;
 import com.sap.cloud.lm.sl.cf.core.util.SecurityUtil;
+import com.sap.cloud.lm.sl.cf.web.api.model.Operation;
 
 @Component
 public class DataTerminationService {
@@ -37,22 +41,40 @@ public class DataTerminationService {
     @Inject
     private ConfigurationSubscriptionDao subscriptionDao;
 
+    @Inject
+    private OperationDao operationDao;
+
     public void deleteOrphanUserData() {
         List<String> deleteSpaceEventsToBeDeleted = getDeleteSpaceEvents();
         for (String spaceId : deleteSpaceEventsToBeDeleted) {
-            
             deleteConfigurationSubscriptionOrphanData(spaceId);
-            AuditLoggingProvider.getFacade().logConfigDelete(
-                "Configuration subscription entry with spaceId:" + spaceId + " has been deleted from ConfigurationSubscription table");
+            AuditLoggingProvider.getFacade()
+                .logConfigDelete(
+                    "Configuration subscription entry with spaceId:" + spaceId + " has been deleted from ConfigurationSubscription table");
             deleteConfigurationEntryOrphanData(spaceId);
-            AuditLoggingProvider.getFacade().logConfigDelete(
-                "Configuration entry with spaceId:" + spaceId + " has been deleted from ConfigurationEntry table");
+            AuditLoggingProvider.getFacade()
+                .logConfigDelete("Configuration entry with spaceId:" + spaceId + " has been deleted from ConfigurationEntry table");
+            deleteUserOperationsOrphanData(spaceId);
         }
+    }
+
+    private void deleteUserOperationsOrphanData(String deleteEventSpaceId) {
+        OperationFilter operationFilter = new OperationFilter.Builder().isCleanedUp()
+            .spaceId(deleteEventSpaceId).build();
+        List<Operation> operationsToBeDeleted = operationDao.find(operationFilter);
+        List<String> result = operationsToBeDeleted.stream()
+            .map(cleanedUpOperation -> cleanedUpOperation.getProcessId())
+            .collect(Collectors.toList());
+        
+        operationDao.removeAll(result);
+        AuditLoggingProvider.getFacade()
+        .logConfigDelete(
+            "Operations for spaceId:" + deleteEventSpaceId + " with process ids:" + result.toString() + " has been deleted");
     }
 
     private void deleteConfigurationSubscriptionOrphanData(String spaceId) {
         List<ConfigurationSubscription> configurationSubscriptions = subscriptionDao.findAll(null, null, spaceId, null);
-        if(configurationSubscriptions.isEmpty()){
+        if (configurationSubscriptions.isEmpty()) {
             return;
         }
         subscriptionDao.removeAll(configurationSubscriptions);
@@ -60,19 +82,24 @@ public class DataTerminationService {
 
     private void deleteConfigurationEntryOrphanData(String spaceId) {
         List<ConfigurationEntry> configurationEntities = entryDao.find(spaceId);
-        if(configurationEntities.isEmpty()){
-           return;
+        if (configurationEntities.isEmpty()) {
+            return;
         }
         entryDao.removeAll(configurationEntities);
     }
 
     protected CloudFoundryClient getCFClient() {
 
-        CloudCredentials cloudCredentials = new CloudCredentials(Configuration.getInstance().getGlobalAuditorUser(),
-            Configuration.getInstance().getGlobalAuditorPassword(), SecurityUtil.CLIENT_ID, SecurityUtil.CLIENT_SECRET);
+        CloudCredentials cloudCredentials = new CloudCredentials(Configuration.getInstance()
+            .getGlobalAuditorUser(),
+            Configuration.getInstance()
+                .getGlobalAuditorPassword(),
+            SecurityUtil.CLIENT_ID, SecurityUtil.CLIENT_SECRET);
 
-        CloudFoundryClient cfClient = new CloudFoundryClient(cloudCredentials, Configuration.getInstance().getTargetURL(),
-            Configuration.getInstance().shouldSkipSslValidation());
+        CloudFoundryClient cfClient = new CloudFoundryClient(cloudCredentials, Configuration.getInstance()
+            .getTargetURL(),
+            Configuration.getInstance()
+                .shouldSkipSslValidation());
         cfClient.login();
 
         return cfClient;
