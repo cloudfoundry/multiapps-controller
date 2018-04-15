fa8dcaa358397a9237c7c83a693057b1364b313c
Alexander Tsvetkov <alexander.tsvetkov.93@gmail.com>
2018-04-15 10:25:56 +0300

    Simplify enum for step phases

--- com.sap.cloud.lm.sl.cf.process/src/main/java/com/sap/cloud/lm/sl/cf/process/steps/StepPhase.java
+++ com.sap.cloud.lm.sl.cf.process/src/main/java/com/sap/cloud/lm/sl/cf/process/steps/StepPhase.java
@@ -1,25 +1,5 @@
 package com.sap.cloud.lm.sl.cf.process.steps;
 
 public enum StepPhase {
-    EXECUTE("execute"), POLL("poll"), RETRY("retry"), WAIT("wait"), DONE("done");
-
-    private String type;
-
-    StepPhase(String type) {
-        this.type = type;
-    }
-
-    @Override
-    public String toString() {
-        return type;
-    }
-
-    public static StepPhase fromValue(String value) {
-        for (StepPhase stepType : StepPhase.values()) {
-            if (stepType.type.equals(value)) {
-                return stepType;
-            }
-        }
-        return null;
-    }
+    EXECUTE, POLL, RETRY, WAIT, DONE,
 }
