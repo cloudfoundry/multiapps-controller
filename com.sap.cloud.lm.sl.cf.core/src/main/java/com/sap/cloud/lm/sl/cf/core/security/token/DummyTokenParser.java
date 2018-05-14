nictas/cf-mta-deploy-service

Implement token parsers

com.sap.cloud.lm.sl.cf.core/src/main/java/com/sap/cloud/lm/sl/cf/core/security/token/DummyTokenParser.java
@@ -1,0 +1,53 @@
+package com.sap.cloud.lm.sl.cf.core.security.token;
+
+import javax.inject.Inject;
+
+import org.springframework.context.annotation.Condition;
+import org.springframework.context.annotation.ConditionContext;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.core.type.AnnotatedTypeMetadata;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.stereotype.Component;
+
+import com.sap.cloud.lm.sl.cf.client.util.TokenFactory;
+import com.sap.cloud.lm.sl.cf.core.util.Configuration;
+import com.sap.cloud.lm.sl.cf.core.util.SecurityUtil;
+
+@Component
+@Conditional(DummyTokenParser.DummyTokenParserCondition.class)
+@Order(Ordered.LOWEST_PRECEDENCE)
+public class DummyTokenParser extends AbstractTokenParser {
+
+    private final TokenFactory tokenFactory;
+
+    @Inject
+    public DummyTokenParser(TokenFactory tokenFactory) {
+        this.tokenFactory = tokenFactory;
+    }
+
+    @Override
+    protected OAuth2AccessToken parseInternal(String tokenString) {
+        if (tokenString.equals(TokenFactory.DUMMY_TOKEN)) {
+            return tokenFactory.createDummyToken("dummy", SecurityUtil.CLIENT_ID);
+        }
+        return null;
+    }
+
+    @Override
+    protected String getTokenFormat() {
+        return "DUMMY";
+    }
+
+    public static class DummyTokenParserCondition implements Condition {
+
+        @Override
+        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+            Configuration configuration = Configuration.getInstance();
+            return configuration.areDummyTokensEnabled();
+        }
+
+    }
+
+}

com.sap.cloud.lm.sl.cf.core/src/main/java/com/sap/cloud/lm/sl/cf/core/security/token/TokenParserChain.java
@@ -1,0 +1,34 @@
+package com.sap.cloud.lm.sl.cf.core.security.token;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TokenParserChain {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(TokenParserChain.class);
+
+    private final List<TokenParser> tokenParsers;
+
+    @Inject
+    public TokenParserChain(List<TokenParser> tokenParsers) {
+        LOGGER.info("Parser chain: " + tokenParsers.toString());
+        this.tokenParsers = tokenParsers;
+    }
+
+    public ParsedToken parse(String tokenString) {
+        for (TokenParser tokenParser : tokenParsers) {
+            ParsedToken parsedToken = tokenParser.parse(tokenString);
+            if (parsedToken != null) {
+                return parsedToken;
+            }
+        }
+        return null;
+    }
+
+}

cloudfoundry/multiapps-controller

Change-Id: Id01caf28feda0d59c95e9d7c1fcb2a319cd5b0f5

com.sap.cloud.lm.sl.cf.core/src/main/java/com/sap/cloud/lm/sl/cf/core/security/token/DummyTokenParser.java
@@ -1,0 +1,48 @@
+package com.sap.cloud.lm.sl.cf.core.security.token;
+
+import javax.inject.Inject;
+
+import org.springframework.context.annotation.Condition;
+import org.springframework.context.annotation.ConditionContext;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.core.type.AnnotatedTypeMetadata;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.stereotype.Component;
+
+import com.sap.cloud.lm.sl.cf.client.util.TokenFactory;
+import com.sap.cloud.lm.sl.cf.core.util.ApplicationConfiguration;
+import com.sap.cloud.lm.sl.cf.core.util.SecurityUtil;
+
+@Component
+@Conditional(DummyTokenParser.DummyTokenParserCondition.class)
+@Order(Ordered.LOWEST_PRECEDENCE)
+public class DummyTokenParser implements TokenParser {
+
+    private final TokenFactory tokenFactory;
+
+    @Inject
+    public DummyTokenParser(TokenFactory tokenFactory) {
+        this.tokenFactory = tokenFactory;
+    }
+
+    @Override
+    public OAuth2AccessToken parse(String tokenString) {
+        if (tokenString.equals(TokenFactory.DUMMY_TOKEN)) {
+            return tokenFactory.createDummyToken("dummy", SecurityUtil.CLIENT_ID);
+        }
+        return null;
+    }
+
+    public static class DummyTokenParserCondition implements Condition {
+
+        @Override
+        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+            ApplicationConfiguration configuration = ApplicationConfiguration.getInstance();
+            return configuration.areDummyTokensEnabled();
+        }
+
+    }
+
+}

com.sap.cloud.lm.sl.cf.core/src/main/java/com/sap/cloud/lm/sl/cf/core/security/token/TokenParserChain.java
@@ -1,0 +1,35 @@
+package com.sap.cloud.lm.sl.cf.core.security.token;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TokenParserChain {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(TokenParserChain.class);
+
+    private final List<TokenParser> tokenParsers;
+
+    @Inject
+    public TokenParserChain(List<TokenParser> tokenParsers) {
+        LOGGER.debug("Parser chain: " + tokenParsers.toString());
+        this.tokenParsers = tokenParsers;
+    }
+
+    public OAuth2AccessToken parse(String tokenString) {
+        for (TokenParser tokenParser : tokenParsers) {
+            OAuth2AccessToken parsedToken = tokenParser.parse(tokenString);
+            if (parsedToken != null) {
+                return parsedToken;
+            }
+        }
+        return null;
+    }
+
+}