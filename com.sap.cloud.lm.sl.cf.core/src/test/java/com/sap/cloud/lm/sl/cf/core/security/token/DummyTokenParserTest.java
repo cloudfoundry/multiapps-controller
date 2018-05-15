47a7491c33a6154bbb3b7a252771cb296b496bb4
Alexander Tsvetkov <alexander.tsvetkov.93@gmail.com>
2018-05-15 15:16:13 +0300

    Remove direct references to ApplicationConfiguration (Part 4)

--- com.sap.cloud.lm.sl.cf.core/src/test/java/com/sap/cloud/lm/sl/cf/core/security/token/DummyTokenParserTest.java
+++ com.sap.cloud.lm.sl.cf.core/src/test/java/com/sap/cloud/lm/sl/cf/core/security/token/DummyTokenParserTest.java
@@ -1,17 +1,35 @@
 package com.sap.cloud.lm.sl.cf.core.security.token;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
 
 import com.sap.cloud.lm.sl.cf.client.util.TokenFactory;
+import com.sap.cloud.lm.sl.cf.core.util.ApplicationConfiguration;
 
 public class DummyTokenParserTest {
 
+    @Mock
+    private ApplicationConfiguration applicationConfiguration;
+    private DummyTokenParser parser;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        this.parser = new DummyTokenParser(new TokenFactory(), applicationConfiguration);
+    }
+
     @Test
     public void testParseDummyToken() {
-        DummyTokenParser parser = new DummyTokenParser(new TokenFactory());
+        Mockito.when(applicationConfiguration.areDummyTokensEnabled())
+            .thenReturn(true);
+
         OAuth2AccessToken parsedToken = parser.parse("DUMMY");
+
         Assert.assertNotNull(parsedToken);
         Assert.assertNotNull(parsedToken.getValue());
         Assert.assertEquals("DUMMY", parsedToken.getValue());
@@ -22,8 +40,17 @@ public class DummyTokenParserTest {
     }
 
     @Test
-    public void testParseDummyTokenWithNoDummyTokenProvided() {
-        DummyTokenParser parser = new DummyTokenParser(new TokenFactory());
+    public void testParseDummyTokenWhenDummyTokensAreDisabled() {
+        Mockito.when(applicationConfiguration.areDummyTokensEnabled())
+            .thenReturn(false);
+        OAuth2AccessToken parsedToken = parser.parse("DUMMY");
+        Assert.assertNull(parsedToken);
+    }
+
+    @Test
+    public void testParseDummyTokenWhenTokenIsNotDummy() {
+        Mockito.when(applicationConfiguration.areDummyTokensEnabled())
+            .thenReturn(true);
         OAuth2AccessToken parsedToken = parser.parse("NOT_DUMMY_TOKEN_AT_ALL");
         Assert.assertNull(parsedToken);
     }
