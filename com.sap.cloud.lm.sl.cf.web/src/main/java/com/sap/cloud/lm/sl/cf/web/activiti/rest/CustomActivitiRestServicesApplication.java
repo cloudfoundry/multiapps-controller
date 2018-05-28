17cc7316f85bfa1f840d2335d174cbea9b9ba005
Alexander Tsvetkov <alexander.tsvetkov.93@gmail.com>
2018-05-28 18:18:14 +0300

    Remove Activiti REST Authenticator
    The Activiti REST API is no longer exposed in our backend, so there's no
    reason to keep the authenticator for it.

--- com.sap.cloud.lm.sl.cf.web/src/main/java/com/sap/cloud/lm/sl/cf/web/activiti/rest/CustomActivitiRestServicesApplication.java
+++
@@ -1,9 +0,0 @@
-package com.sap.cloud.lm.sl.cf.web.activiti.rest;
-
-import org.activiti.rest.service.application.ActivitiRestServicesApplication;
-
-public class CustomActivitiRestServicesApplication extends ActivitiRestServicesApplication {
-    public CustomActivitiRestServicesApplication() {
-        setRestAuthenticator(new CustomActivitiRestAuthenticator());
-    }
-}
