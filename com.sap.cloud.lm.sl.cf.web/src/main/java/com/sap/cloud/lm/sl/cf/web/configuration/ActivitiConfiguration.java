7567d9cc8bb17df53b79f1feff01daa09ed9b4e6
Alexander Tsvetkov <alexander.tsvetkov.93@gmail.com>
2018-06-01 13:11:04 +0300

    Add debug information to Activiti job executor IDs
    JIRA: LMCROSSITXSADEPLOY-1033

--- com.sap.cloud.lm.sl.cf.web/src/main/java/com/sap/cloud/lm/sl/cf/web/configuration/ActivitiConfiguration.java
+++ com.sap.cloud.lm.sl.cf.web/src/main/java/com/sap/cloud/lm/sl/cf/web/configuration/ActivitiConfiguration.java
@@ -3,7 +3,9 @@ package com.sap.cloud.lm.sl.cf.web.configuration;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Supplier;
 
 import javax.inject.Inject;
 import javax.sql.DataSource;
@@ -22,6 +24,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.core.io.Resource;
 import org.springframework.transaction.PlatformTransactionManager;
 
+import com.sap.cloud.lm.sl.cf.core.util.ApplicationConfiguration;
 import com.sap.cloud.lm.sl.cf.process.AbortFailedProcessCommandFactory;
 
 @Configuration
@@ -34,9 +37,12 @@ public class ActivitiConfiguration {
     private static final int JOB_EXECUTOR_CORE_POOL_SIZE = 8;
     private static final int JOB_EXECUTOR_MAX_POOL_SIZE = 32;
     private static final int JOB_EXECUTOR_LOCK_TIME_IN_MILLIS = (int) TimeUnit.MINUTES.toMillis(30);
+    private static final String JOB_EXECUTOR_ID_TEMPLATE = "ds-%s/%d/%s";
 
     @Value("classpath*:/com/sap/cloud/lm/sl/cf/process/*.bpmn")
     private Resource[] activitiResources;
+    protected Supplier<String> randomIdGenerator = () -> UUID.randomUUID()
+        .toString();
 
     @Inject
     @Bean
@@ -80,10 +86,11 @@ public class ActivitiConfiguration {
 
     @Inject
     @Bean
-    public JobExecutor jobExecutor() {
+    public JobExecutor jobExecutor(String jobExecutorId) {
         DefaultJobExecutor jobExecutor = new DefaultJobExecutor();
         scale(jobExecutor);
         jobExecutor.setLockTimeInMillis(JOB_EXECUTOR_LOCK_TIME_IN_MILLIS);
+        jobExecutor.setLockOwner(jobExecutorId);
         return jobExecutor;
     }
 
@@ -91,6 +98,21 @@ public class ActivitiConfiguration {
         jobExecutor.setQueueSize(JOB_EXECUTOR_QUEUE_SIZE);
         jobExecutor.setCorePoolSize(JOB_EXECUTOR_CORE_POOL_SIZE);
         jobExecutor.setMaxPoolSize(JOB_EXECUTOR_MAX_POOL_SIZE);
+    }
+
+    @Inject
+    @Bean
+    public String jobExecutorId(ApplicationConfiguration applicationConfiguration) {
+        String applicationId = applicationConfiguration.getApplicationId();
+        Integer applicationInstanceIndex = applicationConfiguration.getApplicationInstanceIndex();
+        if (applicationId == null || applicationInstanceIndex == null) {
+            return randomIdGenerator.get();
+        }
+        return buildJobExecutorId(applicationId, applicationInstanceIndex);
+    }
+
+    private String buildJobExecutorId(String applicationId, int applicationInstanceIndex) {
+        return String.format(JOB_EXECUTOR_ID_TEMPLATE, applicationId, applicationInstanceIndex, randomIdGenerator.get());
     }
 
 }
---
+++ com.sap.cloud.lm.sl.cf.web/src/test/java/com/sap/cloud/lm/sl/cf/web/configuration/ActivitiConfigurationTest.java
@@ -0,0 +1,57 @@
+package com.sap.cloud.lm.sl.cf.web.configuration;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import com.sap.cloud.lm.sl.cf.core.util.ApplicationConfiguration;
+
+public class ActivitiConfigurationTest {
+
+    private static final String APP_ID = "foo";
+    private static final int APP_INSTANCE_INDEX = 1;
+    private static final String RANDOM_ID = "bar";
+
+    @Mock
+    private ApplicationConfiguration applicationConfiguration;
+    private ActivitiConfiguration activitiConfiguration = new ActivitiConfiguration();
+
+    @BeforeEach
+    public void initMocks() {
+        MockitoAnnotations.initMocks(this);
+        activitiConfiguration.randomIdGenerator = () -> RANDOM_ID;
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    public void testJobExecutorId(String applicationId, Integer applicationInstanceIndex, String expectedJobExecutorId) {
+        Mockito.when(applicationConfiguration.getApplicationInstanceIndex())
+            .thenReturn(applicationInstanceIndex);
+        Mockito.when(applicationConfiguration.getApplicationId())
+            .thenReturn(applicationId);
+
+        String jobExecutorId = activitiConfiguration.jobExecutorId(applicationConfiguration);
+
+        assertEquals(expectedJobExecutorId, jobExecutorId);
+    }
+
+    public static Stream<Arguments> testJobExecutorId() {
+        return Stream.of(
+        // @formatter:off
+            Arguments.of(null, null, RANDOM_ID),
+            Arguments.of(APP_ID, null, RANDOM_ID),
+            Arguments.of(null, APP_INSTANCE_INDEX, RANDOM_ID),
+            Arguments.of(APP_ID, APP_INSTANCE_INDEX, "ds-foo/1/bar")
+        // @formatter:on
+        );
+    }
+
+}
