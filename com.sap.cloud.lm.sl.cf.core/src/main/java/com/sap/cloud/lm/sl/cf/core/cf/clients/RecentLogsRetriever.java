b0856fc60bf64de619acd03df3f3900f0ef3a188
= <valentin.velkov@sap.com>
2018-06-15 19:26:50 +0300

    Add limitation to application upload size (Zipbomb vulnerability fix)

--- com.sap.cloud.lm.sl.cf.core/src/main/java/com/sap/cloud/lm/sl/cf/core/cf/clients/RecentLogsRetriever.java
+++ com.sap.cloud.lm.sl.cf.core/src/main/java/com/sap/cloud/lm/sl/cf/core/cf/clients/RecentLogsRetriever.java
@@ -5,16 +5,19 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import javax.inject.Inject;
 
 import org.apache.commons.fileupload.MultipartStream;
 import org.apache.commons.fileupload.MultipartStream.MalformedStreamException;
 import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.ArrayUtils;
 import org.cloudfoundry.client.lib.CloudFoundryOperations;
 import org.cloudfoundry.client.lib.domain.ApplicationLog;
 import org.springframework.core.io.Resource;
@@ -26,7 +29,6 @@ import com.google.protobuf.InvalidProtocolBufferException;
 import com.sap.cloud.lm.sl.cf.client.events.EventFactory;
 import com.sap.cloud.lm.sl.cf.client.events.LogFactory.LogMessage;
 import com.sap.cloud.lm.sl.cf.client.events.LogFactory.LogMessage.MessageType;
-import com.sap.cloud.lm.sl.cf.client.util.StreamUtil;
 import com.sap.cloud.lm.sl.cf.core.message.Messages;
 import com.sap.cloud.lm.sl.common.SLException;
 
@@ -99,10 +101,24 @@ public class RecentLogsRetriever extends CustomControllerClient {
     }
 
     private void parseProtoBufPart(List<LogMessageConverter> parsedLogs, ByteArrayOutputStream part) throws InvalidProtocolBufferException {
-        EventFactory.Envelope logEnvelope = EventFactory.Envelope.parseFrom(StreamUtil.removeLeadingLine(part.toByteArray()));
+        EventFactory.Envelope logEnvelope = EventFactory.Envelope.parseFrom(removeLeadingNewLine(part.toByteArray()));
         if (logEnvelope.hasLogMessage()) {
             parsedLogs.add(new LogMessageConverter(logEnvelope.getLogMessage()));
         }
+    }
+    
+    private byte[] removeLeadingNewLine(byte[] data) {
+        if (data == null || data.length == 0) {
+            return data;
+        }
+        int from = 0;
+        if (data[0] == '\n') {
+            from = 1;
+        }
+        if (data[0] == '\r' && data[1] == '\n') {
+            from = 2;
+        }
+        return Arrays.copyOfRange(data, from, data.length);
     }
 
     private String getDopplerEndpoint(String loggregatorEndpoint) {
--- com.sap.cloud.lm.sl.cf.process/src/test/java/com/sap/cloud/lm/sl/cf/process/steps/UploadAppStepTest.java
+++ com.sap.cloud.lm.sl.cf.process/src/test/java/com/sap/cloud/lm/sl/cf/process/steps/UploadAppStepTest.java
@@ -13,6 +13,7 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
 import java.util.Arrays;
 
@@ -33,11 +34,11 @@ import org.mockito.stubbing.Answer;
 import org.springframework.http.HttpStatus;
 
 import com.sap.cloud.lm.sl.cf.client.lib.domain.CloudApplicationExtended;
-import com.sap.cloud.lm.sl.cf.client.util.InputStreamProducer;
-import com.sap.cloud.lm.sl.cf.core.util.ApplicationConfiguration;
 import com.sap.cloud.lm.sl.cf.process.Constants;
 import com.sap.cloud.lm.sl.cf.process.message.Messages;
 import com.sap.cloud.lm.sl.cf.process.steps.ScaleAppStepTest.SimpleApplication;
+import com.sap.cloud.lm.sl.cf.process.util.ApplicationArchiveExtractor;
+import com.sap.cloud.lm.sl.cf.process.util.StepLogger;
 import com.sap.cloud.lm.sl.common.SLException;
 import com.sap.cloud.lm.sl.common.util.MapUtil;
 import com.sap.cloud.lm.sl.persistence.processors.FileDownloadProcessor;
@@ -177,23 +178,25 @@ public class UploadAppStepTest {
         private class UploadAppStepMock extends UploadAppStep {
 
             @Override
-            public File saveToFile(String fileName, InputStreamProducer streamProducer) throws IOException {
-                InputStream stream = streamProducer.getNextInputStream();
-                assertNotNull(stream);
-                Files.copy(stream, appFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
-                return appFile;
+            protected Path extractFromMtar(InputStream appArchiveStream, String fileName, long maxSize) {
+                try {
+                    ApplicationArchiveExtractor extractor = getApplicationArchiveExtractor(appArchiveStream, fileName, maxSize, getStepLogger());
+                    InputStream stream = extractor.getNextEntryByName(fileName);
+                    assertNotNull(stream);
+                    Files.copy(stream, appFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+                    return appFile.toPath();
+                } catch (IOException e) {
+                    return null;
+                }
             }
 
-            @Override
-            InputStreamProducer getInputStreamProducer(InputStream appArchiveStream, String fileName, long maxStreamSize)
-                throws SLException {
+            private ApplicationArchiveExtractor getApplicationArchiveExtractor(InputStream appArchiveStream, String fileName, long maxSize, StepLogger logger) throws SLException {
                 if (!fileName.equals(APP_FILE)) {
-                    return super.getInputStreamProducer(appArchiveStream, fileName, maxStreamSize);
+                    return new ApplicationArchiveExtractor(appArchiveStream, fileName, maxSize, logger);
                 }
-                return new InputStreamProducer(getClass().getResourceAsStream(APP_FILE), fileName,
-                    ApplicationConfiguration.DEFAULT_MAX_RESOURCE_FILE_SIZE) {
+                return new ApplicationArchiveExtractor(getClass().getResourceAsStream(APP_FILE), APP_FILE, maxSize, logger) {
                     @Override
-                    public InputStream getNextInputStream() {
+                    public InputStream getNextEntryByName(String name) throws IOException {
                         return getClass().getResourceAsStream(APP_FILE);
                     }
                 };
