abacffd857d55a9baba6e6fa2c9e2bdf09fd888c
Alexander Tsvetkov <alexander.tsvetkov.93@gmail.com>
2018-07-09 14:27:46 +0300

    K8s: Add module and resource types
    JIRA: LMCROSSITXSADEPLOY-1101

--- com.sap.cloud.lm.sl.cf.core/src/main/java/com/sap/cloud/lm/sl/cf/core/k8s/ResourceTypes.java
+++ com.sap.cloud.lm.sl.cf.core/src/main/java/com/sap/cloud/lm/sl/cf/core/k8s/ResourceTypes.java
@@ -5,5 +5,7 @@ public class ResourceTypes {
     public static final String DEPLOYMENT = "deployment";
     public static final String JOB = "job";
     public static final String DOCKER_SECRET = "docker-secret";
+    public static final String SECRET = "secret";
+    public static final String SERVICE_INSTANCE = "service-instance";
 
 }
