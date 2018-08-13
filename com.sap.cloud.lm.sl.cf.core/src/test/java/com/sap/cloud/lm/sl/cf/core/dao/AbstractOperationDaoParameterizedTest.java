946e84805364e6194feed7c8021ea33ab7146466
Alexander Tsvetkov <alexander.tsvetkov.93@gmail.com>
2018-08-13 16:53:24 +0300

    Remove unnecessary parameterization from OperationDaoTest
    JIRA: LMCROSSITXSADEPLOY-1151

--- com.sap.cloud.lm.sl.cf.core/src/test/java/com/sap/cloud/lm/sl/cf/core/dao/AbstractOperationDaoParameterizedTest.java
+++ com.sap.cloud.lm.sl.cf.core/src/test/java/com/sap/cloud/lm/sl/cf/core/dao/AbstractOperationDaoParameterizedTest.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import javax.persistence.Persistence;
 
-import org.junit.After;
+import org.junit.jupiter.api.AfterEach;
 
 import com.sap.cloud.lm.sl.cf.core.helpers.OperationFactory;
 import com.sap.cloud.lm.sl.cf.web.api.model.Operation;
@@ -18,8 +18,8 @@ public abstract class AbstractOperationDaoParameterizedTest {
             dao.add(operation);
         }
     }
-    
-    @After
+
+    @AfterEach
     public void clearDatabase() {
         for (Operation operation : dao.findAll()) {
             dao.remove(operation.getProcessId());
--- com.sap.cloud.lm.sl.cf.core/src/test/java/com/sap/cloud/lm/sl/cf/core/dao/OperationDaoTest.java
+++ com.sap.cloud.lm.sl.cf.core/src/test/java/com/sap/cloud/lm/sl/cf/core/dao/OperationDaoTest.java
@@ -1,229 +1,111 @@
 package com.sap.cloud.lm.sl.cf.core.dao;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assume.assumeTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.text.MessageFormat;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-import org.junit.runners.Parameterized.Parameters;
+import org.junit.jupiter.api.Test;
 
-import com.google.gson.reflect.TypeToken;
-import com.sap.cloud.lm.sl.cf.core.dao.filters.OperationFilter;
+import com.sap.cloud.lm.sl.cf.core.message.Messages;
 import com.sap.cloud.lm.sl.cf.web.api.model.Operation;
-import com.sap.cloud.lm.sl.common.util.Callable;
-import com.sap.cloud.lm.sl.common.util.JsonUtil;
-import com.sap.cloud.lm.sl.common.util.Runnable;
-import com.sap.cloud.lm.sl.common.util.TestUtil;
+import com.sap.cloud.lm.sl.common.ConflictException;
+import com.sap.cloud.lm.sl.common.NotFoundException;
 
-@RunWith(Parameterized.class)
 public class OperationDaoTest extends AbstractOperationDaoParameterizedTest {
 
-    private static enum DaoOperation {
-        ADD, REMOVE, MERGE, FIND, FIND_ALL, FIND_ALL_IN_SPACE, FIND_LAST_IN_SPACE, FIND_ACTIVE_IN_SPACE, FIND_FINISHED_IN_SPACE,
-    }
+    private static final String OPERATION_1_ID = "1";
+    private static final String OPERATION_2_ID = "2";
+    private static final Operation OPERATION_1 = createOperation(OPERATION_1_ID);
+    private static final Operation OPERATION_2 = createOperation(OPERATION_2_ID);
 
-    private static final int LAST_OPERATIONS_COUNT = 3;
-    private static final String SPACE_ID = "1234";
+    @Test
+    public void testAdd() {
+        dao.add(OPERATION_1);
 
-    private String databaseContentResource;
-    private DaoOperation operation;
-    private String processId;
-    private String expected;
+        Set<Operation> operations = asSet(dao.findAll());
 
-    public OperationDaoTest(DaoOperation operation, String processId, String databaseContentResource, String expected) {
-        this.databaseContentResource = databaseContentResource;
-        this.operation = operation;
-        this.processId = processId;
-        this.expected = expected;
-    }
-
-    @Before
-    public void loadDatabaseContent() throws Exception {
-        String databaseContentJson = TestUtil.getResourceAsString(databaseContentResource, getClass());
-        List<Operation> databaseContent = JsonUtil.convertJsonToList(databaseContentJson, new TypeToken<List<Operation>>() {
-        }.getType());
-        addOperations(databaseContent);
-    }
-
-    @Parameters
-    public static Iterable<Object[]> getParameters() {
-        return Arrays.asList(new Object[][] {
-// @formatter:off
-            // (0) Find all ongoing deployments in non-empty database:
-            {
-                DaoOperation.FIND_ALL, null, "database-content-2.json", "R:database-content-2.json",
-            },
-            // (1) Find all ongoing deployments in empty database:
-            {
-                DaoOperation.FIND_ALL, null, "database-content-1.json", "R:database-content-1.json",
-            },
-            // (2) Add ongoing deployment to empty database:
-            {
-                DaoOperation.ADD, "1", "database-content-1.json", "",
-            },
-            // (3) Add ongoing deployment to non-empty database (conflict):
-            {
-                DaoOperation.ADD, "1", "database-content-2.json", "E:MTA operation with ID \"1\" already exists",
-            },
-            // (4) Add ongoing deployment to non-empty database:
-            {
-                DaoOperation.ADD, "3", "database-content-2.json", "",
-            },
-            // (5) Remove ongoing deployment from database (non-existing):
-            {
-                DaoOperation.REMOVE, "1", "database-content-1.json", "E:MTA operation with ID \"1\" does not exist",
-            },
-            // (6) Remove ongoing deployment from database:
-            {
-                DaoOperation.REMOVE, "1", "database-content-2.json", "",
-            },
-            // (7) Find ongoing deployment in database (non-existing):
-            {
-                DaoOperation.FIND, "1", "database-content-1.json", "E:MTA operation with ID \"1\" does not exist",
-            },
-            // (8) Find ongoing deployment in database:
-            {
-                DaoOperation.FIND, "2", "database-content-2.json", "",
-            },
-            // (9) Find all ongoing deployments in space in non-empty database:
-            {
-                DaoOperation.FIND_ALL_IN_SPACE, null, "database-content-3.json", "R:database-content-4.json",
-            },
-            // (10) Find last ongoing deployments in space in non-empty database:
-            {
-                DaoOperation.FIND_LAST_IN_SPACE, null, "database-content-5.json", "R:database-content-6.json",
-            },
-            // (11) Find active ongoing deployments in space in non-empty database:
-            {
-                DaoOperation.FIND_ACTIVE_IN_SPACE, null, "database-content-7.json", "R:database-content-8.json",
-            },
-            // (12) Find finished ongoing deployments in space in non-empty database:
-            {
-                DaoOperation.FIND_FINISHED_IN_SPACE, null, "database-content-7.json", "R:database-content-9.json",
-            },
-// @formatter:on
-        });
+        Set<Operation> expectedOperations = asSet(OPERATION_1);
+        assertEquals(expectedOperations, operations);
     }
 
     @Test
-    public void testAdd() throws Throwable {
-        assumeTrue(operation.equals(DaoOperation.ADD));
-        TestUtil.test(new Runnable() {
-            @Override
-            public void run() throws Exception {
-                int currentOperationsCnt = dao.findAll()
-                    .size();
-                Operation operation1 = new Operation().processId(processId)
-                    .acquiredLock(false);
-                dao.add(operation1);
+    public void testAddWithNonEmptyDatabase() {
+        dao.add(OPERATION_1);
+        dao.add(OPERATION_2);
 
-                assertEquals(currentOperationsCnt + 1, dao.findAll()
-                    .size());
+        Set<Operation> operations = asSet(dao.findAll());
 
-                Operation operation2 = dao.findRequired(processId);
-
-                assertEquals(operation1.getProcessId(), operation2.getProcessId());
-            }
-        }, expected);
+        Set<Operation> expectedOperations = asSet(OPERATION_1, OPERATION_2);
+        assertEquals(expectedOperations, operations);
     }
 
     @Test
-    public void testRemove() throws Throwable {
-        assumeTrue(operation.equals(DaoOperation.REMOVE));
+    public void testAddWithAlreadyExistingOperation() {
+        dao.add(OPERATION_1);
 
-        TestUtil.test(new Runnable() {
-            @Override
-            public void run() throws Exception {
-                int currentOperationsCnt = dao.findAll()
-                    .size();
-                dao.remove(processId);
+        Exception exception = assertThrows(ConflictException.class, () -> dao.add(OPERATION_1));
 
-                assertEquals(currentOperationsCnt - 1, dao.findAll()
-                    .size());
-            }
-        }, expected);
+        String expectedExceptionMessage = MessageFormat.format(Messages.OPERATION_ALREADY_EXISTS, OPERATION_1_ID);
+        assertEquals(expectedExceptionMessage, exception.getMessage());
     }
 
     @Test
-    public void testFind() throws Throwable {
-        assumeTrue(operation.equals(DaoOperation.FIND));
+    public void testRemove() {
+        dao.add(OPERATION_1);
+        assertEquals(1, dao.findAll()
+            .size());
 
-        TestUtil.test(new Runnable() {
-            @Override
-            public void run() throws Exception {
-                Operation operation1 = new Operation().processId(processId)
-                    .acquiredLock(false);
-                Operation operation2 = dao.findRequired(processId);
+        dao.remove(OPERATION_1_ID);
 
-                assertEquals(operation1.getProcessId(), operation2.getProcessId());
-            }
-        }, expected);
+        assertEquals(0, dao.findAll()
+            .size());
     }
 
     @Test
-    public void testFindAll() throws Throwable {
-        assumeTrue(operation.equals(DaoOperation.FIND_ALL));
+    public void testRemoveWithNonExistingOperation() {
+        Exception exception = assertThrows(NotFoundException.class, () -> dao.remove(OPERATION_1_ID));
 
-        TestUtil.test(new Callable<List<Operation>>() {
-            @Override
-            public List<Operation> call() throws Exception {
-                return dao.findAll();
-            }
-        }, expected, getClass());
+        String expectedExceptionMessage = MessageFormat.format(Messages.OPERATION_NOT_FOUND, OPERATION_1_ID);
+        assertEquals(expectedExceptionMessage, exception.getMessage());
     }
 
     @Test
-    public void testFindAllInSpace() throws Throwable {
-        assumeTrue(operation.equals(DaoOperation.FIND_ALL_IN_SPACE));
-
-        TestUtil.test(() -> {
-            OperationFilter filter = new OperationFilter.Builder().spaceId(SPACE_ID)
-                .build();
-            return dao.find(filter);
-        }, expected, getClass());
+    public void testFindRequired() {
+        dao.add(OPERATION_1);
+        Operation retrievedOperation = dao.findRequired(OPERATION_1_ID);
+        assertEquals(OPERATION_1, retrievedOperation);
     }
 
     @Test
-    public void testFindLastOperationsInSpace() {
-        assumeTrue(operation.equals(DaoOperation.FIND_LAST_IN_SPACE));
+    public void testFindRequiredWithNonExistingOperation() {
+        Exception exception = assertThrows(NotFoundException.class, () -> dao.findRequired(OPERATION_1_ID));
 
-        TestUtil.test(() -> {
-            OperationFilter filter = new OperationFilter.Builder().spaceId(SPACE_ID)
-                .maxResults(LAST_OPERATIONS_COUNT)
-                .orderByStartTime()
-                .descending()
-                .build();
-            return dao.find(filter);
-        }, expected, getClass());
+        String expectedExceptionMessage = MessageFormat.format(Messages.OPERATION_NOT_FOUND, OPERATION_1_ID);
+        assertEquals(expectedExceptionMessage, exception.getMessage());
     }
 
     @Test
-    public void testFindActiveOperationsInSpace() {
-        assumeTrue(operation.equals(DaoOperation.FIND_ACTIVE_IN_SPACE));
-
-        TestUtil.test(() -> {
-            OperationFilter filter = new OperationFilter.Builder().spaceId(SPACE_ID)
-                .inNonFinalState()
-                .build();
-            return dao.find(filter);
-        }, expected, getClass());
+    public void testFindWithNonExistingOperation() {
+        assertNull(dao.find(OPERATION_1_ID));
     }
 
-    @Test
-    public void testFindFinishedOperationsInSpace() {
-        assumeTrue(operation.equals(DaoOperation.FIND_FINISHED_IN_SPACE));
+    private static Set<Operation> asSet(Operation... operations) {
+        return asSet(Arrays.asList(operations));
+    }
 
-        TestUtil.test(() -> {
-            OperationFilter filter = new OperationFilter.Builder().spaceId(SPACE_ID)
-                .inFinalState()
-                .build();
-            return dao.find(filter);
-        }, expected, getClass());
+    private static Set<Operation> asSet(List<Operation> operations) {
+        return new HashSet<>(operations);
+    }
+
+    private static Operation createOperation(String id) {
+        return new Operation().processId(id)
+            .acquiredLock(false);
     }
 
 }
