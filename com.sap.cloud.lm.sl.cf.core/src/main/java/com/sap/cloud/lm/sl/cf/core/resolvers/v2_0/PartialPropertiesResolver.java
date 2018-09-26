3146091eae0c669a2d21423fd58118ed582289ad
Alexander Tsvetkov <alexander.tsvetkov.93@gmail.com>
2018-09-26 18:56:57 +0300

    Adopt version 1.63.2 of multiapps
    JIRA: LMCROSSITXSADEPLOY-918

--- com.sap.cloud.lm.sl.cf.core/src/main/java/com/sap/cloud/lm/sl/cf/core/resolvers/v2_0/PartialPropertiesResolver.java
+++ com.sap.cloud.lm.sl.cf.core/src/main/java/com/sap/cloud/lm/sl/cf/core/resolvers/v2_0/PartialPropertiesResolver.java
@@ -3,7 +3,6 @@ package com.sap.cloud.lm.sl.cf.core.resolvers.v2_0;
 import java.util.List;
 import java.util.Map;
 
-import com.sap.cloud.lm.sl.common.ContentException;
 import com.sap.cloud.lm.sl.mta.resolvers.PropertiesResolver;
 import com.sap.cloud.lm.sl.mta.resolvers.ProvidedValuesResolver;
 import com.sap.cloud.lm.sl.mta.resolvers.Reference;
@@ -13,18 +12,18 @@ public class PartialPropertiesResolver extends PropertiesResolver {
 
     private List<String> dependenciesToIgnore;
 
-    public PartialPropertiesResolver(Map<String, Object> properties, ProvidedValuesResolver<? extends ContentException> valuesResolver,
+    public PartialPropertiesResolver(Map<String, Object> properties, ProvidedValuesResolver valuesResolver,
         ReferencePattern referencePattern, String prefix, List<String> dependenciesToIgnore) {
         super(properties, valuesResolver, referencePattern, prefix);
         this.dependenciesToIgnore = dependenciesToIgnore;
     }
 
     @Override
-    protected Object resolveReference(Reference reference) {
+    protected Object resolveReferenceInContext(String key, Reference reference) {
         if (reference.getDependencyName() != null && dependenciesToIgnore.contains(reference.getDependencyName())) {
             return reference.getMatchedPattern();
         }
-        return super.resolveReference(reference);
+        return super.resolveReferenceInContext(key, reference);
     }
 
 }
