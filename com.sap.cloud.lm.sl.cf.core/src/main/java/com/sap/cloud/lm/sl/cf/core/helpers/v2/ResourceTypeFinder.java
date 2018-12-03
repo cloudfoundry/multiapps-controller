59979364e42bc121687e31e567c31b646a706d77
Alexander Tsvetkov <alexander.tsvetkov.93@gmail.com>
2018-12-03 09:43:26 +0200

    Use generic names for module and resource types
    JIRA: LMCROSSITXSADEPLOY-1230

--- com.sap.cloud.lm.sl.cf.core/src/main/java/com/sap/cloud/lm/sl/cf/core/helpers/v2/ResourceTypeFinder.java
+++ com.sap.cloud.lm.sl.cf.core/src/main/java/com/sap/cloud/lm/sl/cf/core/helpers/v2/ResourceTypeFinder.java
@@ -4,7 +4,7 @@ import java.util.Map;
 
 import com.sap.cloud.lm.sl.cf.core.model.SupportedParameters;
 import com.sap.cloud.lm.sl.mta.model.ElementContext;
-import com.sap.cloud.lm.sl.mta.model.v2.PlatformResourceType;
+import com.sap.cloud.lm.sl.mta.model.v2.ResourceType;
 
 public class ResourceTypeFinder extends com.sap.cloud.lm.sl.cf.core.helpers.v1.ResourceTypeFinder {
 
@@ -13,11 +13,11 @@ public class ResourceTypeFinder extends com.sap.cloud.lm.sl.cf.core.helpers.v1.R
     }
 
     @Override
-    public void visit(ElementContext context, com.sap.cloud.lm.sl.mta.model.v1.PlatformResourceType resourceType) {
-        visit(context, (com.sap.cloud.lm.sl.mta.model.v2.PlatformResourceType) resourceType);
+    public void visit(ElementContext context, com.sap.cloud.lm.sl.mta.model.v1.ResourceType resourceType) {
+        visit(context, (com.sap.cloud.lm.sl.mta.model.v2.ResourceType) resourceType);
     }
 
-    public void visit(ElementContext context, PlatformResourceType resourceType) {
+    public void visit(ElementContext context, ResourceType resourceType) {
         Map<String, Object> resourceTypeProperties = resourceType.getParameters();
         String typeProperty = (String) resourceTypeProperties.get(SupportedParameters.TYPE);
         if (typeProperty != null && typeProperty.equals(this.resourceType)) {
