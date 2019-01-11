b3c911c948924aa0273b735542f5b1cbf58fea3d
Valentin Velkov <valentin.velkov@sap.com>
2019-01-11 15:41:58 +0200

    Revert "Merge pull request #355 from IvanBorislavovDimitrov/parameters"
    This reverts commit 27051c528dfb3f1d369220cb8214fc4316bf49dc, reversing
    changes made to 81e08ab897fbf505d90b3ab3c48aa5517bfae77e.

--- com.sap.cloud.lm.sl.cf.process/src/main/java/com/sap/cloud/lm/sl/cf/process/steps/ProcessMtaArchiveStep.java
+++ com.sap.cloud.lm.sl.cf.process/src/main/java/com/sap/cloud/lm/sl/cf/process/steps/ProcessMtaArchiveStep.java
@@ -1,15 +1,11 @@
 package com.sap.cloud.lm.sl.cf.process.steps;
 
-import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
-import java.util.Collections;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.jar.Manifest;
 
 import javax.inject.Inject;
 
-import org.apache.commons.io.IOUtils;
 import org.flowable.engine.delegate.DelegateExecution;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Scope;
@@ -18,22 +14,17 @@ import org.springframework.stereotype.Component;
 import com.sap.cloud.lm.sl.cf.core.cf.HandlerFactory;
 import com.sap.cloud.lm.sl.cf.core.dao.OperationDao;
 import com.sap.cloud.lm.sl.cf.core.helpers.MtaArchiveHelper;
-import com.sap.cloud.lm.sl.cf.core.helpers.v2.PropertiesAccessor;
-import com.sap.cloud.lm.sl.cf.core.model.SupportedParameters;
 import com.sap.cloud.lm.sl.cf.core.util.ApplicationConfiguration;
 import com.sap.cloud.lm.sl.cf.persistence.processors.DefaultFileDownloadProcessor;
 import com.sap.cloud.lm.sl.cf.persistence.processors.FileDownloadProcessor;
-import com.sap.cloud.lm.sl.cf.persistence.services.FileContentProcessor;
 import com.sap.cloud.lm.sl.cf.persistence.services.FileStorageException;
 import com.sap.cloud.lm.sl.cf.process.Constants;
 import com.sap.cloud.lm.sl.cf.process.message.Messages;
 import com.sap.cloud.lm.sl.cf.process.util.ProcessConflictPreventer;
 import com.sap.cloud.lm.sl.common.SLException;
-import com.sap.cloud.lm.sl.common.util.JsonUtil;
 import com.sap.cloud.lm.sl.mta.handlers.ArchiveHandler;
 import com.sap.cloud.lm.sl.mta.handlers.DescriptorParserFacade;
 import com.sap.cloud.lm.sl.mta.model.v2.DeploymentDescriptor;
-import com.sap.cloud.lm.sl.mta.util.PropertiesUtil;
 
 @Component("processMtaArchiveStep")
 @Scope(BeanDefinition.SCOPE_PROTOTYPE)
@@ -103,62 +94,6 @@ public class ProcessMtaArchiveStep extends SyncFlowableStep {
                 getStepLogger().debug("MTA Archive Resources: {0}", mtaArchiveResources.keySet());
             });
         fileService.processFileContent(manifestProcessor);
-
-        setResourcesParameters(context, appArchiveId);
-    }
-
-    private void setResourcesParameters(DelegateExecution context, String appArchiveId) throws FileStorageException {
-        DeploymentDescriptor deploymentDescriptor = StepsUtil.getUnresolvedDeploymentDescriptor(context);
-
-        PropertiesAccessor propertiesAccessor = new HandlerFactory(getMajorVersion(deploymentDescriptor)).getPropertiesAccessor();
-
-        for (Resource resource : deploymentDescriptor.getResources2()) {
-            String resourceFileName = StepsUtil.getResourceFileName(context, resource.getName());
-            if (resourceFileName == null) {
-                continue;
-            }
-            Map<String, Object> parameters = propertiesAccessor.getParameters(resource);
-
-            Map<String, Object> contentConfigParameters = JsonUtil
-                .convertJsonToMap(getResourceFileContent(context, appArchiveId, resourceFileName));
-            Map<String, Object> mergedConfigParameters = PropertiesUtil.mergeExtensionProperties(contentConfigParameters,
-                getCurrentConfigParameters(resource, propertiesAccessor));
-            parameters.put(SupportedParameters.SERVICE_CONFIG, mergedConfigParameters);
-
-            propertiesAccessor.setParameters(resource, parameters);
-        }
-
-        StepsUtil.setUnresolvedDeploymentDescriptor(context, deploymentDescriptor);
-    }
-
-    private Map<String, Object> getCurrentConfigParameters(Resource resource, PropertiesAccessor propertiesAccessor) {
-        Object currentResourceConfigParameters = propertiesAccessor.getParameters(resource)
-            .get(SupportedParameters.SERVICE_CONFIG);
-
-        if (currentResourceConfigParameters == null) {
-            return Collections.emptyMap();
-        }
-
-        return (Map<String, Object>) currentResourceConfigParameters;
-    }
-
-    private int getMajorVersion(DeploymentDescriptor deploymentDescriptor) {
-        String schemaVersion = deploymentDescriptor.getSchemaVersion();
-
-        return Integer.valueOf(schemaVersion.split("\\.")[0]);
-    }
-
-    private String getResourceFileContent(DelegateExecution context, String appArchiveId, String fileName) throws FileStorageException {
-        StringBuilder fileContent = new StringBuilder();
-        FileContentProcessor parametersContentProcessor = appArchiveStream -> {
-            try (InputStream inputStream = ArchiveHandler.getInputStream(appArchiveStream, fileName, configuration.getMaxManifestSize())) {
-                fileContent.append(IOUtils.toString(inputStream, StandardCharsets.UTF_8));
-            }
-        };
-        fileService
-            .processFileContent(new DefaultFileDownloadProcessor(StepsUtil.getSpaceId(context), appArchiveId, parametersContentProcessor));
-
-        return fileContent.toString();
     }
 
     protected MtaArchiveHelper getHelper(Manifest manifest) {
--- com.sap.cloud.lm.sl.cf.process/src/test/java/com/sap/cloud/lm/sl/cf/process/steps/ProcessMtaArchiveStepTest.java
+++ com.sap.cloud.lm.sl.cf.process/src/test/java/com/sap/cloud/lm/sl/cf/process/steps/ProcessMtaArchiveStepTest.java
@@ -7,52 +7,51 @@ import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 import java.util.function.Function;
-import java.util.jar.Attributes;
 import java.util.jar.Manifest;
 import java.util.stream.Collectors;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipInputStream;
 
-import org.apache.commons.io.FileUtils;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 import org.mockito.Mockito;
 import org.mockito.invocation.InvocationOnMock;
 import org.mockito.stubbing.Answer;
 
 import com.sap.cloud.lm.sl.cf.core.helpers.MtaArchiveHelper;
-import com.sap.cloud.lm.sl.cf.core.helpers.v2.PropertiesAccessor;
-import com.sap.cloud.lm.sl.cf.core.model.SupportedParameters;
 import com.sap.cloud.lm.sl.cf.core.util.ApplicationConfiguration;
 import com.sap.cloud.lm.sl.cf.persistence.processors.FileDownloadProcessor;
 import com.sap.cloud.lm.sl.cf.process.util.ProcessConflictPreventer;
+import com.sap.cloud.lm.sl.common.ParsingException;
 import com.sap.cloud.lm.sl.common.SLException;
 import com.sap.cloud.lm.sl.common.util.JsonUtil;
 import com.sap.cloud.lm.sl.common.util.TestUtil;
-import com.sap.cloud.lm.sl.mta.model.v2.DeploymentDescriptor;
-import com.sap.cloud.lm.sl.mta.model.v2.Resource;
 
+@RunWith(Parameterized.class)
 public class ProcessMtaArchiveStepTest extends SyncFlowableStepTest<ProcessMtaArchiveStep> {
 
     private static final String SPACE_ID = "0";
 
     private static final String FILE_ID = "0";
 
-    private StepInput input;
+    private final StepInput input;
 
-    private Manifest outerManifest;
+    @Parameters
+    public static Iterable<Object[]> getParameters() {
+        return Arrays.asList(new Object[][] {
+            // (0)
+            { "process-mta-archive-step-1.json" } });
+    }
+
+    public ProcessMtaArchiveStepTest(String input) throws ParsingException, IOException {
+        this.input = JsonUtil.fromJson(TestUtil.getResourceAsString(input, ProcessMtaArchiveStepTest.class), StepInput.class);
+    }
 
     @Before
     public void setUp() throws Exception {
@@ -60,7 +59,6 @@ public class ProcessMtaArchiveStepTest extends SyncFlowableStepTest<ProcessMtaAr
         prepareFileService();
         when(configuration.getMaxMtaDescriptorSize()).thenReturn(ApplicationConfiguration.DEFAULT_MAX_MTA_DESCRIPTOR_SIZE);
         when(configuration.getMaxManifestSize()).thenReturn(ApplicationConfiguration.DEFAULT_MAX_MANIFEST_SIZE);
-
     }
 
     private void prepareContext() {
@@ -75,8 +73,10 @@ public class ProcessMtaArchiveStepTest extends SyncFlowableStepTest<ProcessMtaAr
             @Override
             public Void answer(InvocationOnMock invocation) throws Exception {
                 FileDownloadProcessor contentProcessor = (FileDownloadProcessor) invocation.getArguments()[0];
+                int fileId = Integer.parseInt(contentProcessor.getFileEntry()
+                    .getId());
 
-                contentProcessor.processContent(getClass().getResourceAsStream(input.archiveFileLocation));
+                contentProcessor.processContent(getClass().getResourceAsStream(input.archiveFileLocations.get(fileId)));
                 return null;
             }
 
@@ -85,59 +85,14 @@ public class ProcessMtaArchiveStepTest extends SyncFlowableStepTest<ProcessMtaAr
     }
 
     @Test
-    public void testModulesResourcesDependenciesCreatedSuccessfully() throws Exception {
-        input = JsonUtil.fromJson(TestUtil.getResourceAsString("process-mta-archive-step-1.json", ProcessMtaArchiveStepTest.class),
-            StepInput.class);
-
+    public void testExecute() throws Exception {
         step.execute(context);
+
         assertStepFinishedSuccessfully();
 
         testModules();
         testResources();
         testDependencies();
-    }
-
-    @Test
-    public void testResourceWithoutCurrentConfigParameters() throws Exception {
-        input = JsonUtil.fromJson(TestUtil.getResourceAsString("process-mta-archive-step-2.json", ProcessMtaArchiveStepTest.class),
-            StepInput.class);
-        step.execute(context);
-
-        testResources();
-    }
-
-    @Test
-    public void testConfigParametersFromTheDeploymentDescriptorWillOverrideParametersFromTheConfigFile() throws Exception {
-        input = JsonUtil.fromJson(TestUtil.getResourceAsString("process-mta-archive-step-3.json", ProcessMtaArchiveStepTest.class),
-            StepInput.class);
-        step.execute(context);
-
-        DeploymentDescriptor unresolvedDeploymentDescriptor = StepsUtil.getUnresolvedDeploymentDescriptor(context);
-        Resource resource = unresolvedDeploymentDescriptor.getResources2()
-            .get(0);
-        Map<String, Object> configResourceParams = (Map<String, Object>) new PropertiesAccessor().getParameters(resource)
-            .get(SupportedParameters.SERVICE_CONFIG);
-
-        String xsappname = "xsappname";
-        assertEquals(input.expectedResourceConfigParameters.get(xsappname), (String) configResourceParams.get(xsappname));
-    }
-
-    @Test
-    public void testAllConfigParametersAreAdded() {
-        input = JsonUtil.fromJson(TestUtil.getResourceAsString("process-mta-archive-step-4.json", ProcessMtaArchiveStepTest.class),
-            StepInput.class);
-        step.execute(context);
-        DeploymentDescriptor unresolvedDeploymentDescriptor = StepsUtil.getUnresolvedDeploymentDescriptor(context);
-        Resource resource = unresolvedDeploymentDescriptor.getResources2()
-            .get(0);
-        Map<String, Object> configResourceParams = (Map<String, Object>) new PropertiesAccessor().getParameters(resource)
-            .get(SupportedParameters.SERVICE_CONFIG);
-        String xsappname = "xsappname";
-        String anotherField = "anotherField";
-        String anotherField1 = "anotherField1";
-        assertEquals(input.expectedResourceConfigParameters.get(xsappname), (String) configResourceParams.get(xsappname));
-        assertEquals(input.expectedResourceConfigParameters.get(anotherField), (String) configResourceParams.get(anotherField));
-        assertEquals(input.expectedResourceConfigParameters.get(anotherField1), (String) configResourceParams.get(anotherField1));
     }
 
     private void testModules() throws Exception {
@@ -152,21 +107,9 @@ public class ProcessMtaArchiveStepTest extends SyncFlowableStepTest<ProcessMtaAr
     }
 
     private void testResources() throws Exception {
-        Map<String, Attributes> entries = outerManifest.getEntries();
-        DeploymentDescriptor unresolvedDeploymentDescriptor = StepsUtil.getUnresolvedDeploymentDescriptor(context);
-        for (String expectedResource : input.expectedResources) {
-            String resourceFileName = getResourceFileName(entries, expectedResource);
-            assertTrue(StepsUtil.getResourceFileName(context, expectedResource)
-                .equals(resourceFileName));
-            Resource resource = getResource(unresolvedDeploymentDescriptor, expectedResource);
-            Map<String, Object> configResourceParams = (Map<String, Object>) new PropertiesAccessor().getParameters(resource)
-                .get(SupportedParameters.SERVICE_CONFIG);
-            String fileContent = getFileContent(resourceFileName);
-            Map<String, Object> convertedJson = JsonUtil.convertJsonToMap(fileContent);
-            assertTrue(configResourceParams.entrySet()
-                .containsAll(convertedJson.entrySet()));
+        for (String expecedResource : input.expectedResources) {
+            assertTrue(StepsUtil.getResourceFileName(context, expecedResource) != null);
         }
-
     }
 
     private void testDependencies() throws Exception {
@@ -175,69 +118,27 @@ public class ProcessMtaArchiveStepTest extends SyncFlowableStepTest<ProcessMtaAr
         }
     }
 
-    private Resource getResource(DeploymentDescriptor deploymentDescriptor, String resourceName) {
-        return deploymentDescriptor.getResources2()
-            .stream()
-            .filter(r -> r.getName()
-                .equals(resourceName))
-            .findFirst()
-            .orElse(null);
-    }
-
-    private String getFileContent(String fileName) throws IOException {
-        File file = null;
-        try (ZipInputStream zipInputStream = new ZipInputStream(getClass().getResourceAsStream(input.archiveFileLocation))) {
-            ZipEntry zipEntry = zipInputStream.getNextEntry();
-            while (zipEntry != null) {
-                zipEntry = zipInputStream.getNextEntry();
-                if (fileName.equals(zipEntry.getName())) {
-                    file = new File(zipEntry.getName());
-                    Path filePath = Paths.get(zipEntry.getName());
-                    unzipFile(zipInputStream, filePath);
-                    return new String(Files.readAllBytes(filePath), StandardCharsets.UTF_8);
-                }
-            }
-        } finally {
-            FileUtils.deleteQuietly(file);
-        }
-
-        return null;
-    }
-
-    private void unzipFile(ZipInputStream zipInputStream, Path unzipFilePath) throws IOException {
-        try (BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(unzipFilePath.toAbsolutePath()
-            .toString()))) {
-
-            byte[] bytes = new byte[1024];
-            int read = 0;
-            while ((read = zipInputStream.read(bytes)) != -1) {
-                bos.write(bytes, 0, read);
-            }
-        }
-    }
-
     private static class StepInput {
 
-        String archiveFileLocation;
+        List<String> archiveFileLocations;
         Set<String> expectedModules;
         Set<String> expectedResources;
         Set<String> expectedRequiredDependencies;
-        Map<String, String> expectedResourceConfigParameters;
+
     }
 
     private class ProcessMtaArchiveStepMock extends ProcessMtaArchiveStep {
 
         @Override
         protected MtaArchiveHelper getHelper(Manifest manifest) {
-            Map<String, Attributes> entries = manifest.getEntries();
-            outerManifest = manifest;
             MtaArchiveHelper helper = Mockito.mock(MtaArchiveHelper.class);
             when(helper.getMtaArchiveModules()).thenReturn(input.expectedModules.stream()
-                .collect(Collectors.toMap(moduleName -> moduleName, Function.identity())));
+                .collect(Collectors.toMap(m -> m, Function.identity())));
             when(helper.getMtaArchiveResources()).thenReturn(input.expectedResources.stream()
-                .collect(Collectors.toMap(resourceName -> resourceName, resourceName -> getResourceFileName(entries, resourceName))));
+                .collect(Collectors.toMap(r -> r, Function.identity())));
             when(helper.getMtaRequiresDependencies()).thenReturn(input.expectedRequiredDependencies.stream()
-                .collect(Collectors.toMap(dependencyName -> dependencyName, Function.identity())));
+                .collect(Collectors.toMap(d -> d, Function.identity())));
+
             try {
                 doAnswer(a -> null).when(helper)
                     .init();
@@ -248,16 +149,6 @@ public class ProcessMtaArchiveStepTest extends SyncFlowableStepTest<ProcessMtaAr
             return helper;
         }
 
-    }
-
-    private String getResourceFileName(Map<String, Attributes> entries, String resourceName) {
-        return entries.entrySet()
-            .stream()
-            .filter(entry -> resourceName.equals(entry.getValue()
-                .getValue(MtaArchiveHelper.ATTR_MTA_RESOURCE)))
-            .findFirst()
-            .map(v -> v.getKey())
-            .orElse(null);
     }
 
     @Override
