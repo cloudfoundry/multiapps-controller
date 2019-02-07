128b5e9a523ffa1d74c02ceb1600b25cc902001b
Boyadzhiev, Petko <p.boyadzhiev@sap.com>
2019-02-07 16:48:51 +0200

    Fixing skipping the loading of configuration files on service update.

--- com.sap.cloud.lm.sl.cf.process/src/main/java/com/sap/cloud/lm/sl/cf/process/steps/CreateServiceStep.java
+++ com.sap.cloud.lm.sl.cf.process/src/main/java/com/sap/cloud/lm/sl/cf/process/steps/CreateServiceStep.java
@@ -1,11 +1,7 @@
 package com.sap.cloud.lm.sl.cf.process.steps;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 
 import javax.inject.Inject;
 
@@ -22,26 +18,15 @@ import com.sap.cloud.lm.sl.cf.core.cf.clients.ServiceWithAlternativesCreator;
 import com.sap.cloud.lm.sl.cf.core.cf.services.ServiceOperationType;
 import com.sap.cloud.lm.sl.cf.core.exec.MethodExecution;
 import com.sap.cloud.lm.sl.cf.core.exec.MethodExecution.ExecutionState;
-import com.sap.cloud.lm.sl.cf.core.helpers.MtaArchiveElements;
-import com.sap.cloud.lm.sl.cf.core.util.ApplicationConfiguration;
-import com.sap.cloud.lm.sl.cf.persistence.processors.DefaultFileDownloadProcessor;
-import com.sap.cloud.lm.sl.cf.persistence.services.FileContentProcessor;
 import com.sap.cloud.lm.sl.cf.persistence.services.FileStorageException;
-import com.sap.cloud.lm.sl.cf.process.Constants;
 import com.sap.cloud.lm.sl.cf.process.message.Messages;
 import com.sap.cloud.lm.sl.common.SLException;
-import com.sap.cloud.lm.sl.common.util.JsonUtil;
-import com.sap.cloud.lm.sl.mta.handlers.ArchiveHandler;
-import com.sap.cloud.lm.sl.mta.util.PropertiesUtil;
 
 @Component("createServiceStep")
 public class CreateServiceStep extends ServiceStep {
 
     @Inject
     private ServiceWithAlternativesCreator.Factory serviceCreatorFactory;
-
-    @Inject
-    private ApplicationConfiguration configuration;
 
     @Override
     protected MethodExecution<String> executeOperation(DelegateExecution execution, CloudControllerClient controllerClient,
@@ -67,7 +52,6 @@ public class CreateServiceStep extends ServiceStep {
 
     private MethodExecution<String> createCloudService(DelegateExecution context, CloudControllerClient client,
         CloudServiceExtended service) throws FileStorageException {
-        prepareServiceParameters(context, service);
         if (service.isUserProvided()) {
             client.createUserProvidedService(service, service.getCredentials());
             return new MethodExecution<String>(null, ExecutionState.FINISHED);
@@ -81,38 +65,6 @@ public class CreateServiceStep extends ServiceStep {
             .createService(client, service, StepsUtil.getSpaceId(context));
     }
 
-    private void prepareServiceParameters(DelegateExecution context, CloudServiceExtended service) throws FileStorageException {
-        MtaArchiveElements mtaArchiveElements = StepsUtil.getMtaArchiveElements(context);
-        String fileName = mtaArchiveElements.getResourceFileName(service.getResourceName());
-        if (fileName != null) {
-            getStepLogger().info(Messages.SETTING_SERVICE_PARAMETERS, service.getName(), fileName);
-            String appArchiveId = StepsUtil.getRequiredStringParameter(context, Constants.PARAM_APP_ARCHIVE_ID);
-            setServiceParameters(context, service, appArchiveId, fileName);
-        }
-    }
-
-    private void setServiceParameters(DelegateExecution context, CloudServiceExtended service, final String appArchiveId,
-        final String fileName) throws FileStorageException {
-        FileContentProcessor parametersFileProcessor = appArchiveStream -> {
-            try (InputStream is = ArchiveHandler.getInputStream(appArchiveStream, fileName, configuration.getMaxManifestSize())) {
-                mergeCredentials(service, is);
-            } catch (IOException e) {
-                throw new SLException(e, Messages.ERROR_RETRIEVING_MTA_RESOURCE_CONTENT, fileName);
-            }
-        };
-        fileService
-            .processFileContent(new DefaultFileDownloadProcessor(StepsUtil.getSpaceId(context), appArchiveId, parametersFileProcessor));
-    }
-
-    private void mergeCredentials(CloudServiceExtended service, InputStream credentialsJson) {
-        Map<String, Object> existingCredentials = service.getCredentials();
-        Map<String, Object> credentials = JsonUtil.convertJsonToMap(credentialsJson);
-        if (existingCredentials == null) {
-            existingCredentials = Collections.emptyMap();
-        }
-        Map<String, Object> result = PropertiesUtil.mergeExtensionProperties(credentials, existingCredentials);
-        service.setCredentials(result);
-    }
 
     private void processServiceCreationFailure(CloudServiceExtended service, CloudOperationException e) {
         if (!service.isOptional()) {
