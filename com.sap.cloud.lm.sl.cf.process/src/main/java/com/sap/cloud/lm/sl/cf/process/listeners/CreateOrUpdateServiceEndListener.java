d293363d25451064057b517fc19f2cd8026a2f46
Boyadzhiev, Petko <p.boyadzhiev@sap.com>
2019-02-14 14:51:36 +0200

    Fixing setting of isServiceUpdated in parent variables.

--- com.sap.cloud.lm.sl.cf.process/src/main/java/com/sap/cloud/lm/sl/cf/process/listeners/CreateOrUpdateServiceEndListener.java
+++ com.sap.cloud.lm.sl.cf.process/src/main/java/com/sap/cloud/lm/sl/cf/process/listeners/CreateOrUpdateServiceEndListener.java
@@ -2,22 +2,21 @@ package com.sap.cloud.lm.sl.cf.process.listeners;
 
 import org.flowable.engine.RuntimeService;
 import org.flowable.engine.delegate.DelegateExecution;
+import org.flowable.engine.delegate.ExecutionListener;
 import org.flowable.engine.impl.context.Context;
 import org.flowable.engine.runtime.Execution;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import com.sap.cloud.lm.sl.cf.process.Constants;
 import com.sap.cloud.lm.sl.cf.process.steps.StepsUtil;
 
 @Component("createOrUpdateServiceEndListener")
-public class CreateOrUpdateServiceEndListener extends AbstractProcessExecutionListener {
+public class CreateOrUpdateServiceEndListener implements ExecutionListener {
     
-    private static final Logger LOGGER = LoggerFactory.getLogger(CreateOrUpdateServiceEndListener.class);
-
+    private static final long serialVersionUID = 1L;
+    
     @Override
-    protected void notifyInternal(DelegateExecution context) throws Exception {
+    public void notify(DelegateExecution context) {
         boolean isServiceUpdated = StepsUtil.getIsServiceUpdated(context);
         String serviceName = StepsUtil.getServiceToProcessName(context);
         if(serviceName == null) {
@@ -32,11 +31,5 @@ public class CreateOrUpdateServiceEndListener extends AbstractProcessExecutionLi
         superExecutionId = superExecutionResult.getSuperExecutionId();
         
         runtimeService.setVariable(superExecutionId, exportedVariableName, isServiceUpdated);
-        
-    }
-
-    @Override
-    protected Logger getLogger() {
-        return LOGGER;
     }
 }
