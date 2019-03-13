54c41bd1788b6aa733aec63bbc8ce5e6a99b9dee
Ivanka Tashkova <ivanka.tashkova@sap.com>
2019-03-13 11:28:47 +0200

    Add missing FAILED value in Status enum

--- com.sap.cloud.lm.sl.cf.process/src/main/java/com/sap/cloud/lm/sl/cf/process/message/Messages.java
+++ com.sap.cloud.lm.sl.cf.process/src/main/java/com/sap/cloud/lm/sl/cf/process/message/Messages.java
@@ -103,6 +103,7 @@ public class Messages {
     public static final String ERROR_CREATING_APP = "Error creating application \"{0}\"";
     public static final String ERROR_UPDATING_APP = "Error updating application \"{0}\"";
     public static final String ERROR_UPLOADING_APP = "Error uploading application \"{0}\"";
+    public static final String ERROR_UPLOADING_APP_WITH_DETAILS = "Error uploading application \"{0}\". [{1}] \"{2}\"";
     public static final String ERROR_UPLOADING_APP_BECAUSE_OF = "Error uploading application \"{0}\": \"{1}\"";
     public static final String ERROR_RESERVING_ROUTES = "Error reserving routes for application \"{0}\"";
     public static final String ERROR_CHECKING_UPLOAD_APP_STATUS = "Error checking upload status of application \"{0}\"";
