ac75e6c3d91faa90136a3678f90f72ccfbe0c126
Dimitar Donchev <dimitar.donchev@sap.com>
2019-03-23 02:37:25 +0200

    NPE with existing service keys and v3 mta schema
    BUG: NGPBUG-81989

--- com.sap.cloud.lm.sl.cf.core/src/main/java/com/sap/cloud/lm/sl/cf/core/cf/v3/ApplicationCloudModelBuilder.java
+++ com.sap.cloud.lm.sl.cf.core/src/main/java/com/sap/cloud/lm/sl/cf/core/cf/v3/ApplicationCloudModelBuilder.java
@@ -5,7 +5,6 @@ import static com.sap.cloud.lm.sl.common.util.CommonUtil.cast;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 import org.apache.commons.collections4.CollectionUtils;
 
@@ -20,11 +19,9 @@ import com.sap.cloud.lm.sl.cf.core.model.SupportedParameters;
 import com.sap.cloud.lm.sl.cf.core.util.CloudModelBuilderUtil;
 import com.sap.cloud.lm.sl.cf.core.util.UserMessageLogger;
 import com.sap.cloud.lm.sl.mta.model.SystemParameters;
-import com.sap.cloud.lm.sl.mta.model.v2.Resource;
 import com.sap.cloud.lm.sl.mta.model.v3.DeploymentDescriptor;
 import com.sap.cloud.lm.sl.mta.model.v3.Module;
 import com.sap.cloud.lm.sl.mta.model.v3.RequiredDependency;
-import com.sap.cloud.lm.sl.mta.util.PropertiesUtil;
 
 public class ApplicationCloudModelBuilder extends com.sap.cloud.lm.sl.cf.core.cf.v2.ApplicationCloudModelBuilder {
 
@@ -69,33 +66,15 @@ public class ApplicationCloudModelBuilder extends com.sap.cloud.lm.sl.cf.core.cf
         List<ServiceKeyToInject> serviceKeysToInject = new ArrayList<>();
         for (RequiredDependency dependency : module.getRequiredDependencies3()) {
             ServiceKeyToInject serviceKey = getServiceKeyToInject(dependency);
-            if (isActiveServiceKey(serviceKey)) {
-                CollectionUtils.addIgnoreNull(serviceKeysToInject, serviceKey);
-            }
+            CollectionUtils.addIgnoreNull(serviceKeysToInject, serviceKey);
         }
         return serviceKeysToInject;
     }
 
-    private boolean isActiveServiceKey(ServiceKeyToInject serviceKeyToInject) {
-        if (serviceKeyToInject != null) {
-            Resource resource = getResource(serviceKeyToInject.getServiceName());
-            if (resource != null) {
-                return isActive(resource);
-            }
-        }
-        return false;
-    }
-
     protected ServiceKeyToInject getServiceKeyToInject(RequiredDependency dependency) {
         com.sap.cloud.lm.sl.mta.model.v3.Resource resource = (com.sap.cloud.lm.sl.mta.model.v3.Resource) getResource(dependency.getName());
-        if (resource != null && CloudModelBuilderUtil.isServiceKey(resource)
-            && CloudModelBuilderUtil.isActive(resource)) {
-            Map<String, Object> resourceParameters = resource.getParameters();
-            String serviceName = PropertiesUtil.getRequiredParameter(resourceParameters, SupportedParameters.SERVICE_NAME);
-            String serviceKeyName = (String) resourceParameters.getOrDefault(SupportedParameters.SERVICE_KEY_NAME, resource.getName());
-            String envVarName = (String) dependency.getParameters()
-                .getOrDefault(SupportedParameters.ENV_VAR_NAME, serviceKeyName);
-            return new ServiceKeyToInject(envVarName, serviceName, serviceKeyName);
+        if (resource != null && CloudModelBuilderUtil.isActive(resource)) {
+            return super.getServiceKeyToInject(dependency);
         }
         return null;
     }
