a717936fbf400448f848f426031290d86e7fb4b3
Ivanka Tashkova <ivanka.tashkova@sap.com>
2019-03-29 16:16:39 +0200

    Store process logs asynchronously

---
+++ com.sap.cloud.lm.sl.cf.web/src/main/java/com/sap/cloud/lm/sl/cf/web/configuration/AsyncProcessLoggerConfiguration.java
@@ -0,0 +1,28 @@
+package com.sap.cloud.lm.sl.cf.web.configuration;
+
+import java.util.concurrent.Executor;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import com.sap.cloud.lm.sl.cf.core.util.ApplicationConfiguration;
+
+@Configuration
+@EnableAsync
+public class AsyncProcessLoggerConfiguration {
+
+    ApplicationConfiguration configuration = new ApplicationConfiguration();
+
+    @Bean("asyncExecutor")
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(configuration.getFlowableJobExecutorCoreThreads());
+        executor.setMaxPoolSize(configuration.getFlowableJobExecutorMaxThreads());
+        executor.setQueueCapacity(configuration.getFlowableJobExecutorQueueCapacity());
+        executor.initialize();
+        return executor;
+    }
+
+}
