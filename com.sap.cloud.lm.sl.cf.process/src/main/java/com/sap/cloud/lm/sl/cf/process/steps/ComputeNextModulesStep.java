86c6eae90eba31c274efd83816d9984030f6d5e5
Ivan Kasarov <ivan.kasarov@sap.com>
2019-05-09 13:08:16 +0300

    fixed check for parallel deployment module dendecies with partial module selection

--- com.sap.cloud.lm.sl.cf.process/src/main/java/com/sap/cloud/lm/sl/cf/process/steps/ComputeNextModulesStep.java
+++ com.sap.cloud.lm.sl.cf.process/src/main/java/com/sap/cloud/lm/sl/cf/process/steps/ComputeNextModulesStep.java
@@ -1,18 +1,17 @@
 package com.sap.cloud.lm.sl.cf.process.steps;
 
 import java.util.List;
-import java.util.Objects;
 import java.util.stream.Collectors;
 
 import org.apache.commons.collections4.ListUtils;
-import org.cloudfoundry.client.lib.CloudControllerClient;
-import org.cloudfoundry.client.lib.domain.CloudApplication;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import com.sap.cloud.lm.sl.cf.core.security.serialization.SecureSerializationFacade;
 import com.sap.cloud.lm.sl.cf.process.message.Messages;
+import com.sap.cloud.lm.sl.cf.process.util.ModuleDependencyChecker;
+import com.sap.cloud.lm.sl.mta.model.DeploymentDescriptor;
 import com.sap.cloud.lm.sl.mta.model.Module;
 
 @Component("computeNextModulesStep")
@@ -25,47 +24,33 @@ public class ComputeNextModulesStep extends SyncFlowableStep {
     protected StepPhase executeStep(ExecutionWrapper execution) throws Exception {
         getStepLogger().debug(Messages.COMPUTING_NEXT_MODULES_FOR_PARALLEL_ITERATION);
         List<Module> allModulesToDeploy = StepsUtil.getModulesToDeploy(execution.getContext());
-        List<Module> completedApplications = StepsUtil.getIteratedModulesInParallel(execution.getContext());
-        List<String> completedModuleNames = completedApplications.stream()
-            .map(Module::getName)
-            .collect(Collectors.toList());
+        List<Module> completedModules = StepsUtil.getIteratedModulesInParallel(execution.getContext());
 
-        CloudControllerClient client = execution.getControllerClient();
+        DeploymentDescriptor descriptor = StepsUtil.getDeploymentDescriptor(execution.getContext());
+        ModuleDependencyChecker dependencyChecker = new ModuleDependencyChecker(execution.getControllerClient(), descriptor.getModules(),
+            allModulesToDeploy, completedModules);
+
+        getStepLogger().debug("Completed modules detected: " + dependencyChecker.getAlreadyDeployedModules());
+        getStepLogger().debug("All modules for deploy detected: " + dependencyChecker.getModulesForDeployment());
+        getStepLogger().debug("Modules not for deploy detected: " + dependencyChecker.getModulesNotForDeployment());
+
         // Set next iteration data
-        List<Module> modulesForNextIteration = computeApplicationsForNextIteration(client, allModulesToDeploy, completedModuleNames);
+        List<Module> modulesForNextIteration = computeApplicationsForNextIteration(allModulesToDeploy, dependencyChecker);
         StepsUtil.setModulesToIterateInParallel(execution.getContext(), modulesForNextIteration);
 
         // Mark next iteration data as computed
-        StepsUtil.setIteratedModulesInParallel(execution.getContext(), ListUtils.union(completedApplications, modulesForNextIteration));
+        StepsUtil.setIteratedModulesInParallel(execution.getContext(), ListUtils.union(completedModules, modulesForNextIteration));
 
         getStepLogger().debug(Messages.COMPUTED_NEXT_MODULES_FOR_PARALLEL_ITERATION, secureSerializer.toJson(modulesForNextIteration));
         return StepPhase.DONE;
     }
 
-    private List<Module> computeApplicationsForNextIteration(CloudControllerClient client, List<Module> allModulesToDeploy,
-        List<String> completedModules) {
-        allModulesToDeploy.removeIf(module -> completedModules.contains(module.getName()));
+    private List<Module> computeApplicationsForNextIteration(List<Module> allModulesToDeploy, ModuleDependencyChecker dependencyChecker) {
+        allModulesToDeploy.removeIf(module -> dependencyChecker.getAlreadyDeployedModules()
+            .contains(module.getName()));
+
         return allModulesToDeploy.stream()
-            .filter(module -> applicationHasAllDependenciesSatisfied(client, completedModules, module))
+            .filter(dependencyChecker::areAllDependenciesSatisfied)
             .collect(Collectors.toList());
     }
-
-    private boolean applicationHasAllDependenciesSatisfied(CloudControllerClient client, List<String> completedModules, Module module) {
-        if (module.getMajorSchemaVersion() < 3) {
-            return true;
-        }
-
-        return module.getDeployedAfter()
-            .isEmpty() || completedModules.containsAll(module.getDeployedAfter())
-            || areAllDependenciesAlreadyDeployed(client, module.getDeployedAfter());
-    }
-
-    private boolean areAllDependenciesAlreadyDeployed(CloudControllerClient client, List<String> deployedAfter) {
-        List<CloudApplication> nonDeployedApplications = deployedAfter.stream()
-            .map(deployAfterDependency -> client.getApplication(deployAfterDependency, false))
-            .filter(Objects::isNull)
-            .collect(Collectors.toList());
-        return nonDeployedApplications.isEmpty();
-    }
-
 }
---
+++ com.sap.cloud.lm.sl.cf.process/src/main/java/com/sap/cloud/lm/sl/cf/process/util/ModuleDependencyChecker.java
@@ -0,0 +1,84 @@
+package com.sap.cloud.lm.sl.cf.process.util;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.cloudfoundry.client.lib.CloudControllerClient;
+import org.cloudfoundry.client.lib.domain.CloudApplication;
+
+import com.sap.cloud.lm.sl.mta.model.Module;
+
+public class ModuleDependencyChecker {
+
+    private Set<String> modulesForDeployment;
+    private Set<String> modulesNotForDeployment;
+    private Set<String> modulesAlreadyDeployed;
+    private CloudControllerClient client;
+
+    public ModuleDependencyChecker(CloudControllerClient client, List<Module> allModulesInDescriptor, List<Module> allModulesToDeploy,
+        List<Module> completedModules) {
+        this.client = client;
+
+        modulesForDeployment = computeModulesForDeployment(allModulesToDeploy);
+        modulesAlreadyDeployed = computeAlreadyDeployedModules(completedModules);
+        modulesNotForDeployment = computeModuleNotForDeployment(allModulesInDescriptor);
+    }
+
+    private Set<String> computeModulesForDeployment(List<Module> allModulesToDeploy) {
+        return allModulesToDeploy.stream()
+            .map(Module::getName)
+            .collect(Collectors.toSet());
+    }
+
+    private Set<String> computeAlreadyDeployedModules(List<Module> completedModules) {
+        return completedModules.stream()
+            .map(Module::getName)
+            .collect(Collectors.toSet());
+    }
+
+    private Set<String> computeModuleNotForDeployment(List<Module> allModules) {
+        return allModules.stream()
+            .map(Module::getName)
+            .filter(module -> !modulesForDeployment.contains(module))
+            .collect(Collectors.toSet());
+    }
+
+    public boolean areAllDependenciesSatisfied(Module module) {
+        if (module.getMajorSchemaVersion() < 3) {
+            return true;
+        }
+
+        return module.getDeployedAfter()
+            .isEmpty() || modulesAlreadyDeployed.containsAll(module.getDeployedAfter())
+            || areAllDependenciesAlreadyPresent(module.getDeployedAfter());
+    }
+
+    private boolean areAllDependenciesAlreadyPresent(List<String> deployedAfter) {
+        List<CloudApplication> modulesNotFoundInSpace = deployedAfter.stream()
+            .filter(modulesNotForDeployment::contains)
+            .map(deployAfterDependency -> client.getApplication(deployAfterDependency, false))
+            .filter(Objects::isNull)
+            .collect(Collectors.toList());
+
+        List<String> modulesNotYetDeployed = deployedAfter.stream()
+            .filter(modulesForDeployment::contains)
+            .filter(module -> !modulesAlreadyDeployed.contains(module))
+            .collect(Collectors.toList());
+
+        return modulesNotFoundInSpace.isEmpty() && modulesNotYetDeployed.isEmpty();
+    }
+
+    public Set<String> getModulesForDeployment() {
+        return modulesForDeployment;
+    }
+
+    public Set<String> getModulesNotForDeployment() {
+        return modulesNotForDeployment;
+    }
+
+    public Set<String> getAlreadyDeployedModules() {
+        return modulesAlreadyDeployed;
+    }
+}
