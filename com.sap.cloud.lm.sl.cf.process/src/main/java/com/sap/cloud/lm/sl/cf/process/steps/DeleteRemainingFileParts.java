b6aac1871ff2ac8cae31f7a25c852c7740ee15d2
IvanBorislavovDimitrov <starstrucks1997@gmail.com>
2019-06-07 11:54:18 +0300

    Make ValidateDeployParamtersStep idempotent

---
+++ com.sap.cloud.lm.sl.cf.process/src/main/java/com/sap/cloud/lm/sl/cf/process/steps/DeleteRemainingFileParts.java
@@ -0,0 +1,38 @@
+package com.sap.cloud.lm.sl.cf.process.steps;
+
+import java.util.List;
+
+import org.flowable.engine.delegate.DelegateExecution;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import com.sap.cloud.lm.sl.cf.persistence.model.FileEntry;
+import com.sap.cloud.lm.sl.cf.persistence.services.FileStorageException;
+import com.sap.cloud.lm.sl.cf.process.Constants;
+import com.sap.cloud.lm.sl.cf.process.message.Messages;
+
+@Component("deleteRemainingFileParts")
+@Scope(BeanDefinition.SCOPE_PROTOTYPE)
+public class DeleteRemainingFileParts extends SyncFlowableStep {
+
+    @Override
+    protected StepPhase executeStep(ExecutionWrapper execution) throws Exception {
+        List<FileEntry> filesToRemove = StepsUtil.getFromJsonBinaries(execution.getContext(), Constants.VAR_FILE_ENTRIES, FileEntry.class);
+        filesToRemove.forEach(this::attemptToDeleteFilePart);
+        return StepPhase.DONE;
+    }
+
+    private void attemptToDeleteFilePart(FileEntry fileEntry) {
+        try {
+            fileService.deleteFile(fileEntry.getSpace(), fileEntry.getId());
+        } catch (FileStorageException e) {
+            logger.warn(Messages.ERROR_DELETING_ARCHIVE_PARTS_CONTENT, e);
+        }
+    }
+
+    @Override
+    protected String getStepErrorMessage(DelegateExecution context) {
+        return Messages.ERROR_DELETING_REMAINING_FILE_PARTS;
+    }
+}
