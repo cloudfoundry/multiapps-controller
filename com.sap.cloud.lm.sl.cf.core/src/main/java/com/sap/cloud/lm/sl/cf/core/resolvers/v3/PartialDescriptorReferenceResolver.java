700b965f7d09eb93c99fd3710e74a78440173194
theghost5800 <kristian0001@gmail.com>
2019-06-11 17:11:32 +0300

    Add PartialModuleReferenceResolver to support hooks customization
    This change is necessary to add reference and placeholder resolvers for
    hooks parameters
    
    JIRA:LMCROSSITXSADEPLOY-1556 Support hooks customization in extension
    descriptors

--- com.sap.cloud.lm.sl.cf.core/src/main/java/com/sap/cloud/lm/sl/cf/core/resolvers/v3/PartialDescriptorReferenceResolver.java
+++ com.sap.cloud.lm.sl.cf.core/src/main/java/com/sap/cloud/lm/sl/cf/core/resolvers/v3/PartialDescriptorReferenceResolver.java
@@ -2,13 +2,12 @@ package com.sap.cloud.lm.sl.cf.core.resolvers.v3;
 
 import java.util.List;
 
-import com.sap.cloud.lm.sl.cf.core.resolvers.v2.PartialModuleReferenceResolver;
 import com.sap.cloud.lm.sl.mta.model.DeploymentDescriptor;
 import com.sap.cloud.lm.sl.mta.model.Module;
 import com.sap.cloud.lm.sl.mta.model.Resource;
 import com.sap.cloud.lm.sl.mta.resolvers.ResolverBuilder;
-import com.sap.cloud.lm.sl.mta.resolvers.v2.ModuleReferenceResolver;
 import com.sap.cloud.lm.sl.mta.resolvers.v3.DescriptorReferenceResolver;
+import com.sap.cloud.lm.sl.mta.resolvers.v3.ModuleReferenceResolver;
 import com.sap.cloud.lm.sl.mta.resolvers.v3.ResourceReferenceResolver;
 
 public class PartialDescriptorReferenceResolver extends DescriptorReferenceResolver {
