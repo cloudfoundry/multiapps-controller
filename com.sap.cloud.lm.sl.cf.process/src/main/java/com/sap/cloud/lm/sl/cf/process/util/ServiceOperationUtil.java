87b343f36ca976cfcd9109ccdbafaf4db340096c
Encho Belezirev <encho.belezirev@gmail.com>
2019-07-30 16:21:16 +0300

    Minor changes
    Change-Id: I1e304e19314a63bffb157b2e4282750926e64671

--- com.sap.cloud.lm.sl.cf.process/src/main/java/com/sap/cloud/lm/sl/cf/process/util/ServiceOperationUtil.java
+++ com.sap.cloud.lm.sl.cf.process/src/main/java/com/sap/cloud/lm/sl/cf/process/util/ServiceOperationUtil.java
@@ -10,15 +10,14 @@ import com.sap.cloud.lm.sl.cf.process.message.Messages;
 import com.sap.cloud.lm.sl.common.SLException;
 
 public class ServiceOperationUtil {
-    
+
     public static Map<String, Object> getServiceKeyCredentials(CloudControllerClient client, String serviceName, String serviceKeyName) {
         List<CloudServiceKey> existingServiceKeys = client.getServiceKeys(serviceName);
-        for (CloudServiceKey existingServiceKey : existingServiceKeys) {
-            if (existingServiceKey.getName()
-                .equals(serviceKeyName)) {
-                return existingServiceKey.getCredentials();
-            }
-        }
-        throw new SLException(Messages.ERROR_RETRIEVING_REQUIRED_SERVICE_KEY_ELEMENT, serviceKeyName, serviceName);
+        CloudServiceKey foundServiceKey = existingServiceKeys.stream()
+            .filter(existingServiceKey -> existingServiceKey.getName()
+                .equals(serviceKeyName))
+            .findFirst()
+            .orElseThrow(() -> new SLException(Messages.ERROR_RETRIEVING_REQUIRED_SERVICE_KEY_ELEMENT, serviceKeyName, serviceName));
+        return foundServiceKey.getCredentials();
     }
 }
