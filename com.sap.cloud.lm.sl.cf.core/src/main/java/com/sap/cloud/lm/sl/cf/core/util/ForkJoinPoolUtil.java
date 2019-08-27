ba019097a3656598f41a8a903738ffc6dd70092e
Vasil Bozhurski <vasil.bozhurski@sap.com>
2019-08-27 14:27:43 +0300

    add ForkJoinPool util
    adds a helpful util when wanting to use parallel streams with custom
    thread forkjoin thread pools
    
    Bug: LMCROSSITXSADEPLOY-1642

---
+++ com.sap.cloud.lm.sl.cf.core/src/main/java/com/sap/cloud/lm/sl/cf/core/util/ForkJoinPoolUtil.java
@@ -0,0 +1,27 @@
+package com.sap.cloud.lm.sl.cf.core.util;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ForkJoinPool;
+
+public class ForkJoinPoolUtil {
+
+    public static <T> T execute(int threads, Callable<T> callable) {
+        ForkJoinPool customThreadPool = new ForkJoinPool(threads);
+        try {
+            return customThreadPool.submit(callable)
+                                   .join();
+        } finally {
+            customThreadPool.shutdown();
+        }
+    }
+
+    public static void execute(int threads, Runnable runnable) {
+        ForkJoinPool customThreadPool = new ForkJoinPool(threads);
+        try {
+            customThreadPool.submit(runnable)
+                            .join();
+        } finally {
+            customThreadPool.shutdown();
+        }
+    }
+}
