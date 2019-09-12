ce109754bab5f10ca9a2b8fe1c105cf2c8708ba7
Velkov <valentin.velkov@sap.com>
2019-09-12 16:06:19 +0300

    Add tests for services and queries

---
+++ com.sap.cloud.lm.sl.cf.core/src/test/java/com/sap/cloud/lm/sl/cf/core/persistence/service/ConfigurationEntryServiceTest.java
@@ -0,0 +1,161 @@
+package com.sap.cloud.lm.sl.cf.core.persistence.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.text.MessageFormat;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import com.sap.cloud.lm.sl.cf.core.message.Messages;
+import com.sap.cloud.lm.sl.cf.core.model.CloudTarget;
+import com.sap.cloud.lm.sl.cf.core.model.ConfigurationEntry;
+import com.sap.cloud.lm.sl.cf.core.persistence.query.ConfigurationEntryQuery;
+import com.sap.cloud.lm.sl.cf.core.persistence.service.ConfigurationEntryService.ConfigurationEntryMapper;
+import com.sap.cloud.lm.sl.common.ConflictException;
+import com.sap.cloud.lm.sl.mta.model.Version;
+
+public class ConfigurationEntryServiceTest {
+
+    private static final ConfigurationEntry CONFIGURATION_ENTRY_1 = createConfigurationEntry(1l, "providerNid", "providerId", "2.0", "org",
+                                                                                             "space", "content");
+    private static final ConfigurationEntry CONFIGURATION_ENTRY_2 = createConfigurationEntry(2l, "providerNid1", "providerId1", "3.1",
+                                                                                             "org1", "space1", "content1");
+    private ConfigurationEntryService configurationEntryService = createConfigurationEntryService();
+
+    @AfterEach
+    public void cleanUp() {
+        configurationEntryService.createQuery()
+                                 .delete();
+    }
+
+    @Test
+    public void testAdd() {
+        configurationEntryService.add(CONFIGURATION_ENTRY_1);
+        assertEquals(1, configurationEntryService.createQuery()
+                                                 .list()
+                                                 .size());
+        assertEquals(CONFIGURATION_ENTRY_1.getId(), configurationEntryService.createQuery()
+                                                                             .id(CONFIGURATION_ENTRY_1.getId())
+                                                                             .singleResult()
+                                                                             .getId());
+    }
+
+    @Test
+    public void testAddWithNonEmptyDatabase() {
+        addConfigurationEntries(Arrays.asList(CONFIGURATION_ENTRY_1, CONFIGURATION_ENTRY_2));
+
+        assertConfigurationEntryExists(CONFIGURATION_ENTRY_1.getId());
+        assertConfigurationEntryExists(CONFIGURATION_ENTRY_2.getId());
+
+        assertEquals(2, configurationEntryService.createQuery()
+                                                 .list()
+                                                 .size());
+    }
+
+    @Test
+    public void testAddWithAlreadyExistingEntry() {
+        configurationEntryService.add(CONFIGURATION_ENTRY_1);
+        Exception exception = assertThrows(ConflictException.class, () -> configurationEntryService.add(CONFIGURATION_ENTRY_1));
+        String expectedExceptionMessage = MessageFormat.format(Messages.CONFIGURATION_ENTRY_ALREADY_EXISTS,
+                                                               CONFIGURATION_ENTRY_1.getProviderNid(),
+                                                               CONFIGURATION_ENTRY_1.getProviderId(),
+                                                               CONFIGURATION_ENTRY_1.getProviderVersion()
+                                                                                    .toString(),
+                                                               CONFIGURATION_ENTRY_1.getTargetSpace()
+                                                                                    .getOrg(),
+                                                               CONFIGURATION_ENTRY_1.getTargetSpace()
+                                                                                    .getSpace());
+        assertEquals(expectedExceptionMessage, exception.getMessage());
+    }
+
+    @Test
+    public void testQueryById() {
+        testQueryByCriteria((query, entry) -> query.id(entry.getId()));
+    }
+
+    @Test
+    public void testQueryByProviderNid() {
+        testQueryByCriteria((query, entry) -> query.providerNid(entry.getProviderNid()));
+    }
+
+    @Test
+    public void testQueryByProviderId() {
+        testQueryByCriteria((query, entry) -> query.providerId(entry.getProviderId()));
+    }
+
+    @Test
+    public void testQueryByTarget() {
+        testQueryByCriteria((query, entry) -> query.target(entry.getTargetSpace()));
+    }
+
+    @Test
+    public void testQueryBySpaceId() {
+        testQueryByCriteria((query, entry) -> query.spaceId(entry.getSpaceId()));
+    }
+
+    @Test
+    public void testQueryByVersion() {
+        addConfigurationEntries(Arrays.asList(CONFIGURATION_ENTRY_1, CONFIGURATION_ENTRY_2));
+
+        String version = ">3.0.0";
+        assertEquals(1, configurationEntryService.createQuery()
+                                                 .version(version)
+                                                 .list()
+                                                 .size());
+    }
+
+    private void testQueryByCriteria(ConfigurationEntryQueryBuilder configurationEntryQueryBuilder) {
+        addConfigurationEntries(Arrays.asList(CONFIGURATION_ENTRY_1, CONFIGURATION_ENTRY_2));
+        assertEquals(1, configurationEntryQueryBuilder.build(configurationEntryService.createQuery(), CONFIGURATION_ENTRY_1)
+                                                      .list()
+                                                      .size());
+        assertEquals(1, configurationEntryQueryBuilder.build(configurationEntryService.createQuery(), CONFIGURATION_ENTRY_1)
+                                                      .delete());
+        assertConfigurationEntryExists(CONFIGURATION_ENTRY_2.getId());
+    }
+
+    private interface ConfigurationEntryQueryBuilder {
+
+        ConfigurationEntryQuery build(ConfigurationEntryQuery entryQuery, ConfigurationEntry testedEntry);
+    }
+
+    private void addConfigurationEntries(List<ConfigurationEntry> entries) {
+        entries.forEach(configurationEntryService::add);
+    }
+
+    private void assertConfigurationEntryExists(Long id) {
+        // If does not exist, will throw NoResultException
+        configurationEntryService.createQuery()
+                                 .id(id)
+                                 .singleResult();
+    }
+
+    private static ConfigurationEntry createConfigurationEntry(long id, String providerNid, String providerId, String version, String org,
+                                                               String space, String content) {
+        return new ConfigurationEntry(id,
+                                      providerNid,
+                                      providerId,
+                                      Version.parseVersion(version),
+                                      new CloudTarget(org, space),
+                                      content,
+                                      Collections.emptyList(),
+                                      space);
+    }
+
+    private ConfigurationEntryService createConfigurationEntryService() {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("TestDefault");
+        ConfigurationEntryService configurationEntryService = new ConfigurationEntryService(entityManagerFactory);
+        ConfigurationEntryMapper configurationEntryFactory = new ConfigurationEntryMapper();
+        configurationEntryService.entryMapper = configurationEntryFactory;
+        return configurationEntryService;
+    }
+
+}
---
+++ com.sap.cloud.lm.sl.cf.core/src/test/java/com/sap/cloud/lm/sl/cf/core/persistence/service/ConfigurationSubscriptionServiceTest.java
@@ -0,0 +1,185 @@
+package com.sap.cloud.lm.sl.cf.core.persistence.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.text.MessageFormat;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import com.sap.cloud.lm.sl.cf.core.message.Messages;
+import com.sap.cloud.lm.sl.cf.core.model.ConfigurationEntry;
+import com.sap.cloud.lm.sl.cf.core.model.ConfigurationFilter;
+import com.sap.cloud.lm.sl.cf.core.model.ConfigurationSubscription;
+import com.sap.cloud.lm.sl.cf.core.model.ConfigurationSubscription.ModuleDto;
+import com.sap.cloud.lm.sl.cf.core.model.ConfigurationSubscription.ResourceDto;
+import com.sap.cloud.lm.sl.cf.core.persistence.query.ConfigurationSubscriptionQuery;
+import com.sap.cloud.lm.sl.cf.core.persistence.service.ConfigurationSubscriptionService.ConfigurationSubscriptionMapper;
+import com.sap.cloud.lm.sl.common.ConflictException;
+import com.sap.cloud.lm.sl.mta.model.Version;
+
+public class ConfigurationSubscriptionServiceTest {
+
+    private static final ConfigurationSubscription CONFIGURATION_SUBSCRIPTION_1 = createConfigurationSubscription(1l, "mta", "space", "app",
+                                                                                                                  createConfigurationFilter("3.1"),
+                                                                                                                  createModuleDto("moduleName"),
+                                                                                                                  createResourceDto("resourceName"));
+
+    private static final ConfigurationSubscription CONFIGURATION_SUBSCRIPTION_2 = createConfigurationSubscription(2l, "mta1", "space1",
+                                                                                                                  "app1",
+                                                                                                                  createConfigurationFilter("2.0"),
+                                                                                                                  createModuleDto("moduleName1"),
+                                                                                                                  createResourceDto("resourceName1"));
+
+    private ConfigurationSubscriptionService configurationSubscriptionService = createConfigurationSubscriptionService();
+
+    @AfterEach
+    public void cleanUp() {
+        configurationSubscriptionService.createQuery()
+                                        .delete();
+    }
+
+    @Test
+    public void testAdd() {
+        configurationSubscriptionService.add(CONFIGURATION_SUBSCRIPTION_1);
+        assertEquals(1, configurationSubscriptionService.createQuery()
+                                                        .list()
+                                                        .size());
+        assertEquals(CONFIGURATION_SUBSCRIPTION_1.getId(), configurationSubscriptionService.createQuery()
+                                                                                           .id(CONFIGURATION_SUBSCRIPTION_1.getId())
+                                                                                           .singleResult()
+                                                                                           .getId());
+    }
+
+    @Test
+    public void testAddWithNonEmptyDatabase() {
+        addConfigurationSubscriptions(Arrays.asList(CONFIGURATION_SUBSCRIPTION_1, CONFIGURATION_SUBSCRIPTION_2));
+
+        assertConfigurationSubscriptionExists(CONFIGURATION_SUBSCRIPTION_1.getId());
+        assertConfigurationSubscriptionExists(CONFIGURATION_SUBSCRIPTION_2.getId());
+
+        assertEquals(2, configurationSubscriptionService.createQuery()
+                                                        .list()
+                                                        .size());
+    }
+
+    @Test
+    public void testAddWithAlreadyExistingSubscription() {
+        configurationSubscriptionService.add(CONFIGURATION_SUBSCRIPTION_1);
+        Exception exception = assertThrows(ConflictException.class,
+                                           () -> configurationSubscriptionService.add(CONFIGURATION_SUBSCRIPTION_1));
+        String expectedExceptionMessage = MessageFormat.format(Messages.CONFIGURATION_SUBSCRIPTION_ALREADY_EXISTS,
+                                                               CONFIGURATION_SUBSCRIPTION_1.getMtaId(),
+                                                               CONFIGURATION_SUBSCRIPTION_1.getAppName(),
+                                                               CONFIGURATION_SUBSCRIPTION_1.getResourceDto()
+                                                                                           .getName(),
+                                                               CONFIGURATION_SUBSCRIPTION_1.getSpaceId());
+        assertEquals(expectedExceptionMessage, exception.getMessage());
+    }
+
+    @Test
+    public void testQueryById() {
+        testQueryByCriteria((query, subscription) -> query.id(subscription.getId()));
+    }
+
+    @Test
+    public void testQueryByMtaId() {
+        testQueryByCriteria((query, subscription) -> query.mtaId(subscription.getMtaId()));
+    }
+
+    @Test
+    public void testQueryBySpaceId() {
+        testQueryByCriteria((query, subscription) -> query.spaceId(subscription.getSpaceId()));
+    }
+
+    @Test
+    public void testQueryByAppName() {
+        testQueryByCriteria((query, subscription) -> query.appName(subscription.getAppName()));
+    }
+
+    @Test
+    public void testQueryByResourceName() {
+        testQueryByCriteria((query, subscription) -> query.resourceName(subscription.getResourceDto()
+                                                                                    .getName()));
+    }
+
+    @Test
+    public void testQueryByFilterMatching() {
+        addConfigurationSubscriptions(Arrays.asList(CONFIGURATION_SUBSCRIPTION_1, CONFIGURATION_SUBSCRIPTION_2));
+
+        int foundSubscriptions = configurationSubscriptionService.createQuery()
+                                                                 .onSelectMatching(Arrays.asList(new ConfigurationEntry(null,
+                                                                                                                        null,
+                                                                                                                        Version.parseVersion("3.1"),
+                                                                                                                        null,
+                                                                                                                        null,
+                                                                                                                        null,
+                                                                                                                        null)))
+                                                                 .list()
+                                                                 .size();
+        assertEquals(1, foundSubscriptions);
+    }
+
+    private void testQueryByCriteria(ConfigurationSubscriptionQueryBuilder configurationSubscriptionQueryBuilder) {
+        addConfigurationSubscriptions(Arrays.asList(CONFIGURATION_SUBSCRIPTION_1, CONFIGURATION_SUBSCRIPTION_2));
+        assertEquals(1,
+                     configurationSubscriptionQueryBuilder.build(configurationSubscriptionService.createQuery(),
+                                                                 CONFIGURATION_SUBSCRIPTION_1)
+                                                          .list()
+                                                          .size());
+        assertEquals(1,
+                     configurationSubscriptionQueryBuilder.build(configurationSubscriptionService.createQuery(),
+                                                                 CONFIGURATION_SUBSCRIPTION_1)
+                                                          .delete());
+        assertConfigurationSubscriptionExists(CONFIGURATION_SUBSCRIPTION_2.getId());
+    }
+
+    private interface ConfigurationSubscriptionQueryBuilder {
+
+        ConfigurationSubscriptionQuery build(ConfigurationSubscriptionQuery entryQuery, ConfigurationSubscription testedSubscription);
+    }
+
+    private void addConfigurationSubscriptions(List<ConfigurationSubscription> subscriptions) {
+        subscriptions.forEach(configurationSubscriptionService::add);
+    }
+
+    private static ConfigurationFilter createConfigurationFilter(String providerVersion) {
+        return new ConfigurationFilter(null, null, providerVersion, null, null);
+    }
+
+    private static ModuleDto createModuleDto(String name) {
+        return new ModuleDto(name, null, null, null);
+    }
+
+    private static ResourceDto createResourceDto(String name) {
+        return new ResourceDto(name, null);
+    }
+
+    private void assertConfigurationSubscriptionExists(Long id) {
+        // If does not exist, will throw NoResultException
+        configurationSubscriptionService.createQuery()
+                                        .id(id)
+                                        .singleResult();
+    }
+
+    private static ConfigurationSubscription createConfigurationSubscription(long id, String mtaId, String spaceId, String appName,
+                                                                             ConfigurationFilter filter, ModuleDto moduleDto,
+                                                                             ResourceDto resourceDto) {
+        return new ConfigurationSubscription(id, mtaId, spaceId, appName, filter, moduleDto, resourceDto);
+    }
+
+    private ConfigurationSubscriptionService createConfigurationSubscriptionService() {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("TestDefault");
+        ConfigurationSubscriptionService configurationSubscriptionService = new ConfigurationSubscriptionService(entityManagerFactory);
+        ConfigurationSubscriptionMapper configurationSubscriptionFactory = new ConfigurationSubscriptionMapper();
+        configurationSubscriptionService.subscriptionMapper = configurationSubscriptionFactory;
+        return configurationSubscriptionService;
+    }
+
+}
