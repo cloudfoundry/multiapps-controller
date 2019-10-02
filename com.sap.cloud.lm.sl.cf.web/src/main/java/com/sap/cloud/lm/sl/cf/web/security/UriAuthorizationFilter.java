e1c108141c95deda9b60143910d1fde71d4d88cd
Alexander Tsvetkov <alexander.tsvetkov.93@gmail.com>
2019-10-02 13:39:09 +0300

    Return 404 instead of 500 for unsupported endpoints

--- com.sap.cloud.lm.sl.cf.web/src/main/java/com/sap/cloud/lm/sl/cf/web/security/UriAuthorizationFilter.java
+++ com.sap.cloud.lm.sl.cf.web/src/main/java/com/sap/cloud/lm/sl/cf/web/security/UriAuthorizationFilter.java
@@ -2,10 +2,16 @@ package com.sap.cloud.lm.sl.cf.web.security;
 
 import java.io.IOException;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 public interface UriAuthorizationFilter {
 
     String getUriRegex();
 
-    void ensureUserIsAuthorized(HttpCommunication communication) throws IOException;
+    /**
+     * @return Whether or not the request should be forwarded to the rest of the filter chain and eventually to the appropriate handler.
+     */
+    boolean ensureUserIsAuthorized(HttpServletRequest request, HttpServletResponse response) throws IOException;
 
 }
