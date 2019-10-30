7488cb8f54109d94916e1f60f06e630dd534d101
Alexander Tsvetkov <alexander.tsvetkov.93@gmail.com>
2019-10-30 13:03:12 +0200

    Generate ApplicationShutdown class

---
+++ com.sap.cloud.lm.sl.cf.core/src/main/java/com/sap/cloud/lm/sl/cf/core/shutdown/model/ApplicationShutdown.java
@@ -0,0 +1,28 @@
+package com.sap.cloud.lm.sl.cf.core.shutdown.model;
+
+import org.immutables.value.Value;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
+@Value.Immutable
+@JsonSerialize(as = ImmutableApplicationShutdown.class)
+@JsonDeserialize(as = ImmutableApplicationShutdown.class)
+public interface ApplicationShutdown {
+
+    enum Status {
+        FINISHED, RUNNING
+    }
+
+    String getApplicationId();
+
+    String getApplicationInstanceId();
+
+    int getApplicationInstanceIndex();
+
+    @Value.Default
+    default Status getStatus() {
+        return Status.RUNNING;
+    }
+
+}
