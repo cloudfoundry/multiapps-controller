68ad7df92bca741346f4e5a1f802385b868140ec
Alexander Tsvetkov <alexander.tsvetkov.93@gmail.com>
2019-11-04 12:26:49 +0200

    Fix authorization for space name-based APIs
    The authorization didn't work for organizations/spaces with whitespace
    characters in their names (or other characters that require URL
    encoding).
    
    JIRA: LMCROSSITXSADEPLOY-1768

---
+++ com.sap.cloud.lm.sl.cf.web/src/main/java/com/sap/cloud/lm/sl/cf/web/util/ServletUtils.java
@@ -0,0 +1,23 @@
+package com.sap.cloud.lm.sl.cf.web.util;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.text.MessageFormat;
+
+import javax.servlet.http.HttpServletRequest;
+
+import com.sap.cloud.lm.sl.cf.web.message.Messages;
+
+public class ServletUtils {
+
+    public static String getDecodedURI(HttpServletRequest request) {
+        String uri = request.getRequestURI();
+        try {
+            return URLDecoder.decode(uri, StandardCharsets.UTF_8.name());
+        } catch (UnsupportedEncodingException e) {
+            throw new IllegalStateException(MessageFormat.format(Messages.COULD_NOT_DECODE_URI_0, uri), e);
+        }
+    }
+
+}
