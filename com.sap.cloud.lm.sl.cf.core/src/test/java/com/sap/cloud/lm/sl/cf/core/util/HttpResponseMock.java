611009990a1586146891ee66206a4803e6aa3b15
Alexander Tsvetkov <alexander.tsvetkov.93@gmail.com>
2019-11-06 15:17:03 +0200

    CSRF Client: Fix false-negative test
    The test for retries of 403 responses reported positive results even
    without 5ad449362196460a6f69e48c0645bb06878a3383.
    
    JIRA: LMCROSSITXSADEPLOY-1763

---
+++ com.sap.cloud.lm.sl.cf.core/src/test/java/com/sap/cloud/lm/sl/cf/core/util/HttpResponseMock.java
@@ -0,0 +1,60 @@
+package com.sap.cloud.lm.sl.cf.core.util;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.apache.http.Header;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.ProtocolVersion;
+import org.apache.http.StatusLine;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.message.BasicStatusLine;
+import org.immutables.value.Value;
+import org.mockito.Mockito;
+
+@Value.Immutable
+public abstract class HttpResponseMock {
+
+    abstract int getStatusCode();
+
+    abstract Map<String, String> getHeaders();
+
+    abstract String getBody();
+
+    @Value.Derived
+    public CloseableHttpResponse getMock() {
+        CloseableHttpResponse response = Mockito.mock(CloseableHttpResponse.class);
+        Mockito.when(response.getStatusLine())
+               .thenReturn(createStatusLine(getStatusCode()));
+        Mockito.when(response.getEntity())
+               .thenReturn(createHttpEntity(getBody()));
+        mockHeaders(response);
+        return response;
+    }
+
+    private static StatusLine createStatusLine(int statusCode) {
+        return new BasicStatusLine(new ProtocolVersion("HTTP", 1, 1), statusCode, null);
+    }
+
+    private static HttpEntity createHttpEntity(String body) {
+        return new StringEntity(body, StandardCharsets.UTF_8);
+    }
+
+    private void mockHeaders(HttpResponse response) {
+        for (Entry<String, String> header : getHeaders().entrySet()) {
+            Mockito.when(response.containsHeader(header.getKey()))
+                   .thenReturn(true);
+            Mockito.when(response.getFirstHeader(header.getKey()))
+                   .thenReturn(createHeader(header));
+        }
+    }
+
+    private static Header createHeader(Entry<String, String> header) {
+        return new BasicHeader(header.getKey(), header.getValue());
+    }
+
+}
