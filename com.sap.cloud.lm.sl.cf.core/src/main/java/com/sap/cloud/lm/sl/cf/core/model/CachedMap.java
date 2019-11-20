9a77bf84d5138742bfee4f1db0b7984f3246adb7
Alexander Tsvetkov <alexander.tsvetkov.93@gmail.com>
2019-11-20 11:43:52 +0200

    Fix poor performance of CachedMap

--- com.sap.cloud.lm.sl.cf.core/src/main/java/com/sap/cloud/lm/sl/cf/core/model/CachedMap.java
+++ com.sap.cloud.lm.sl.cf.core/src/main/java/com/sap/cloud/lm/sl/cf/core/model/CachedMap.java
@@ -1,7 +1,7 @@
 package com.sap.cloud.lm.sl.cf.core.model;
 
-import java.util.Collections;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.LongSupplier;
 import java.util.function.Supplier;
 
@@ -13,19 +13,19 @@ public class CachedMap<K, V> {
     private final long expirationTimeInSeconds;
     private final LongSupplier currentTimeSupplier = System::currentTimeMillis;
 
-    private final Map<K, CachedObject<V>> referenceMap = Collections.synchronizedMap(new ReferenceMap<>(ReferenceStrength.HARD,
-                                                                                                        ReferenceStrength.SOFT));
+    private final Map<K, CachedObject<V>> referenceMap = new ConcurrentHashMap<>(new ReferenceMap<>(ReferenceStrength.HARD,
+                                                                                                    ReferenceStrength.SOFT));
 
     public CachedMap(long expirationTimeInSeconds) {
         this.expirationTimeInSeconds = expirationTimeInSeconds;
     }
 
-    public synchronized V get(K key, Supplier<V> refreshFunction) {
+    public V get(K key, Supplier<V> refreshFunction) {
         CachedObject<V> value = referenceMap.computeIfAbsent(key, k -> new CachedObject<>(expirationTimeInSeconds, currentTimeSupplier));
         return value.get(refreshFunction);
     }
 
-    public synchronized V forceRefresh(K key, Supplier<V> refreshFunction) {
+    public V forceRefresh(K key, Supplier<V> refreshFunction) {
         CachedObject<V> value = referenceMap.computeIfAbsent(key, k -> new CachedObject<>(expirationTimeInSeconds, currentTimeSupplier));
         return value.forceRefresh(refreshFunction);
     }
