49b5b26cedd25d665b9907286c1063c04de4d7b7
Alexander Tsvetkov <alexander.tsvetkov.93@gmail.com>
2019-12-12 13:13:58 +0200

    Decrease default CF controller connection timeout to 2m
    JIRA: LMCROSSITXSADEPLOY-1831

--- com.sap.cloud.lm.sl.cf.core/src/main/java/com/sap/cloud/lm/sl/cf/core/util/ApplicationConfiguration.java
+++ com.sap.cloud.lm.sl.cf.core/src/main/java/com/sap/cloud/lm/sl/cf/core/util/ApplicationConfiguration.java
@@ -128,7 +128,7 @@ public class ApplicationConfiguration {
     public static final Integer DEFAULT_THREAD_MONITOR_CACHE_UPDATE_IN_SECONDS = 1;
     public static final Integer DEFAULT_SPACE_DEVELOPER_CACHE_TIME_IN_SECONDS = 20;
     // We've experimented with much smaller values (5, 15 seconds), but these lead to connection timeouts.
-    public static final int DEFAULT_CONTROLLER_CLIENT_CONNECT_TIMEOUT_IN_SECONDS = (int) TimeUnit.MINUTES.toSeconds(10);
+    public static final int DEFAULT_CONTROLLER_CLIENT_CONNECT_TIMEOUT_IN_SECONDS = (int) TimeUnit.MINUTES.toSeconds(2);
     public static final int DEFAULT_CONTROLLER_CLIENT_CONNECTION_POOL_SIZE = 192;
     public static final int DEFAULT_CONTROLLER_CLIENT_THREAD_POOL_SIZE = 64;
     public static final Boolean DEFAULT_SAP_INTERNAL_DELIVERY = false;
