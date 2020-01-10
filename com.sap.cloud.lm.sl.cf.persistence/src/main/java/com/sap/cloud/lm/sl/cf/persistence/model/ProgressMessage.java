ba0e1df10a29118883fcb87cfaa6a12cbd6ee94a
IvanBorislavovDimitrov <starstrucks1997@gmail.com>
2020-01-10 12:03:24 +0200

    Fix missing CTS logs

--- com.sap.cloud.lm.sl.cf.persistence/src/main/java/com/sap/cloud/lm/sl/cf/persistence/model/ProgressMessage.java
+++ com.sap.cloud.lm.sl.cf.persistence/src/main/java/com/sap/cloud/lm/sl/cf/persistence/model/ProgressMessage.java
@@ -3,10 +3,14 @@ package com.sap.cloud.lm.sl.cf.persistence.model;
 import java.sql.Timestamp;
 import java.util.Date;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import org.immutables.value.Value;
 import org.immutables.value.Value.Immutable;
 
 @Immutable
+@JsonSerialize(as = ImmutableProgressMessage.class)
+@JsonDeserialize(as = ImmutableProgressMessage.class)
 public interface ProgressMessage {
 
     @Value.Default
