bd7a04090bbbd3c5e00190f18084d6253c76d82c
Rangel Ivanov <rangel.ivanov@sap.com>
2020-01-20 18:15:08 +0200

    Update ConfigurationSubsciptions in bg-deploy
    Update the subscribers for the given mta when removing the application
    name suffix '-new'
    
    LMCROSSITXSADEPLOY-1159

--- com.sap.cloud.lm.sl.cf.process/src/test/java/com/sap/cloud/lm/sl/cf/process/steps/RemoveNewApplicationsSuffixStepTest.java
+++ com.sap.cloud.lm.sl.cf.process/src/test/java/com/sap/cloud/lm/sl/cf/process/steps/RemoveNewApplicationsSuffixStepTest.java
@@ -1,8 +1,12 @@
 package com.sap.cloud.lm.sl.cf.process.steps;
 
+import com.sap.cloud.lm.sl.cf.core.model.ConfigurationSubscription;
+import com.sap.cloud.lm.sl.cf.core.persistence.query.ConfigurationSubscriptionQuery;
+import com.sap.cloud.lm.sl.cf.core.persistence.service.ConfigurationSubscriptionService;
 import com.sap.cloud.lm.sl.cf.process.Constants;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 
 import java.util.Arrays;
@@ -11,9 +15,20 @@ import java.util.List;
 
 public class RemoveNewApplicationsSuffixStepTest extends SyncFlowableStepTest<RemoveNewApplicationsSuffixStep> {
 
+    @Mock
+    private ConfigurationSubscriptionService subscriptionService;
+
     @BeforeEach
     public void setUp() {
         context.setVariable(Constants.PARAM_KEEP_ORIGINAL_APP_NAMES_AFTER_DEPLOY, true);
+        context.setVariable(Constants.PARAM_MTA_ID, "");
+        ConfigurationSubscriptionQuery query = Mockito.mock(ConfigurationSubscriptionQuery.class);
+        Mockito.when(query.mtaId(""))
+               .thenReturn(query);
+        Mockito.when(query.list())
+               .thenReturn(Collections.emptyList());
+        Mockito.when(subscriptionService.createQuery())
+               .thenReturn(query);
     }
 
     @Test
@@ -39,6 +54,25 @@ public class RemoveNewApplicationsSuffixStepTest extends SyncFlowableStepTest<Re
             Mockito.verify(client)
                    .rename(app, removeAppNameSuffix(app));
         }
+    }
+
+    @Test
+    public void testUpdatingOfConfigurationSubscriptions() {
+        ConfigurationSubscriptionQuery query = Mockito.mock(ConfigurationSubscriptionQuery.class);
+        Mockito.when(query.mtaId(""))
+               .thenReturn(query);
+        Mockito.when(query.list())
+               .thenReturn(Collections.singletonList(new ConfigurationSubscription(0, "", "", "a-new", null, null, null)));
+        Mockito.when(subscriptionService.createQuery())
+               .thenReturn(query);
+
+        StepsUtil.setAppsToDeploy(context, Collections.singletonList("a-new"));
+
+        step.execute(context);
+        assertStepFinishedSuccessfully();
+
+        Mockito.verify(subscriptionService)
+               .update(0L, new ConfigurationSubscription(0, "", "", "a", null, null, null));
     }
 
     private static String removeAppNameSuffix(String appName) {
