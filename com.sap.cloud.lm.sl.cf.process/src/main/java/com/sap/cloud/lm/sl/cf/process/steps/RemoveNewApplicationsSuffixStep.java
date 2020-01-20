602032aaeab4c4edccfa56f79b09040aec21dcd5
Rangel Ivanov <rangel.ivanov@sap.com>
2020-01-20 18:15:08 +0200

    Update ConfigurationSubsciptions in bg-deploy
    Update the subscribers for the given mta when removing the application
    name suffix '-new'
    
    LMCROSSITXSADEPLOY-1159

--- com.sap.cloud.lm.sl.cf.process/src/main/java/com/sap/cloud/lm/sl/cf/process/steps/RemoveNewApplicationsSuffixStep.java
+++ com.sap.cloud.lm.sl.cf.process/src/main/java/com/sap/cloud/lm/sl/cf/process/steps/RemoveNewApplicationsSuffixStep.java
@@ -1,18 +1,25 @@
 package com.sap.cloud.lm.sl.cf.process.steps;
 
 import com.sap.cloud.lm.sl.cf.core.model.BlueGreenApplicationNameSuffix;
+import com.sap.cloud.lm.sl.cf.core.model.ConfigurationSubscription;
+import com.sap.cloud.lm.sl.cf.core.persistence.service.ConfigurationSubscriptionService;
+import com.sap.cloud.lm.sl.cf.process.Constants;
 import com.sap.cloud.lm.sl.cf.process.message.Messages;
 import org.cloudfoundry.client.lib.CloudControllerClient;
 import org.flowable.engine.delegate.DelegateExecution;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Scope;
 
+import javax.inject.Inject;
 import javax.inject.Named;
 import java.util.List;
 
 @Named("removeNewApplicationsSuffixStep")
 @Scope(BeanDefinition.SCOPE_PROTOTYPE)
 public class RemoveNewApplicationsSuffixStep extends SyncFlowableStep {
+
+    @Inject
+    private ConfigurationSubscriptionService subscriptionService;
 
     @Override
     protected StepPhase executeStep(ExecutionWrapper execution) {
@@ -28,7 +35,37 @@ public class RemoveNewApplicationsSuffixStep extends SyncFlowableStep {
             getStepLogger().debug(Messages.RENAMING_APPLICATION, appName);
             client.rename(appName, BlueGreenApplicationNameSuffix.removeSuffix(appName));
         }
+
+        String mtaId = (String) execution.getContext()
+                                         .getVariable(Constants.PARAM_MTA_ID);
+        updateConfigurationSubscribers(appsToProcess, mtaId);
+
         return StepPhase.DONE;
+    }
+
+    private void updateConfigurationSubscribers(List<String> appsToProcess, String mtaId) {
+        List<ConfigurationSubscription> subscriptions = subscriptionService.createQuery()
+                                                                           .mtaId(mtaId)
+                                                                           .list();
+        for (ConfigurationSubscription subscription : subscriptions) {
+            if (appsToProcess.contains(subscription.getAppName())) {
+                updateConfigurationSubscription(subscription);
+            }
+        }
+    }
+
+    private void updateConfigurationSubscription(ConfigurationSubscription subscription) {
+        subscriptionService.update(subscription.getId(), createNewSubscription(subscription));
+    }
+
+    private ConfigurationSubscription createNewSubscription(ConfigurationSubscription subscription) {
+        return new ConfigurationSubscription(subscription.getId(),
+                                             subscription.getMtaId(),
+                                             subscription.getSpaceId(),
+                                             BlueGreenApplicationNameSuffix.removeSuffix(subscription.getAppName()),
+                                             subscription.getFilter(),
+                                             subscription.getModuleDto(),
+                                             subscription.getResourceDto());
     }
 
     @Override
