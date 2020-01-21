9792d4503ddf7a13c5ccdad53643f2bd891b45d0
Vasil Bozhurski <vasil.bozhurski@sap.com>
2020-01-21 16:07:53 +0200

    Revert "Skip upload of unchanged application content"
    This reverts commit 08119ac68407aeebe74168638673ad9ed1a2164e.

--- com.sap.cloud.lm.sl.cf.process/src/main/java/com/sap/cloud/lm/sl/cf/process/message/Messages.java
+++ com.sap.cloud.lm.sl.cf.process/src/main/java/com/sap/cloud/lm/sl/cf/process/message/Messages.java
@@ -417,8 +417,7 @@ public class Messages {
     public static final String ARCHIVE_IS_VERIFIED = "Archive signature is verified";
     public static final String CHECKING_FOR_OVERWRITING_READ_ONLY_PARAMETERS = "Checking for overwriting read-only parameters for mta with id: \"{0}\"";
     public static final String NO_READ_ONLY_PARAMETERS_ARE_OVERWRITTEN = "No read-only parameters are overwritten";
-    public static final String CONTENT_OF_APPLICATION_0_IS_NOT_CHANGED = "Content of application \"{0}\" is not changed - upload will be skipped.";
-    
+
     protected Messages() {
     }
 }
