7ebf0a587ea6ea6f50da392cf14f2c465f2e3c60
Velkov <valentin.velkov@sap.com>
2020-02-05 15:07:51 +0200

    [Metadata] Update metadata messages

--- com.sap.cloud.lm.sl.cf.core/src/main/java/com/sap/cloud/lm/sl/cf/core/message/Messages.java
+++ com.sap.cloud.lm.sl.cf.core/src/main/java/com/sap/cloud/lm/sl/cf/core/message/Messages.java
@@ -64,9 +64,12 @@ public final class Messages {
     public static final String UNABLE_TO_PARSE_MEMORY_STRING_0 = "Unable to parse memory string \"{0}\"";
     public static final String CANT_CREATE_SERVICE_NOT_MATCHING_OFFERINGS_OR_PLAN = "Service \"{0}\" could not be created because none of the service offering(s) \"{1}\" match with existing service offerings or provide service plan \"{2}\"";
     public static final String CANT_CREATE_SERVICE = "Service \"{0}\" could not be created because all attempt(s) to use service offerings \"{1}\" failed";
-    public static final String CANT_PARSE_MTA_METADATA_FOR_APP_0 = "Cannot parse MTA metadata for application \"{0}\". This indicates that MTA reserved variables in the application''s environment were modified manually. Either revert the changes or delete the application.";
-    public static final String MTA_METADATA_FOR_APP_0_IS_INCOMPLETE = "MTA metadata for application \"{0}\" is incomplete";
-    public static final String ENV_OF_APP_0_CONTAINS_INVALID_VALUE_FOR_1 = "The environment of application \"{0}\" contains an invalid value for \"{0}\"";
+    public static final String CANT_PARSE_MTA_METADATA_VERSION_FOR_0 = "Cannot parse version from MTA metadata for \"{0}\". This indicates that MTA reserved variables in the entity's metadata were modified manually. Either revert the changes or delete the entity.";
+    public static final String CANT_PARSE_MTA_ENV_METADATA_VERSION_FOR_APP_0 = "Cannot parse version from MTA metadata for application \"{0}\". This indicates that MTA reserved variables in the application's environment were modified manually. Either revert the changes or delete the application.";
+    public static final String MTA_METADATA_FOR_APP_0_IS_INCOMPLETE = "MTA metadata for application \"{0}\" is incomplete. This indicates that MTA reserved variables in the application's environment were modified manually. Either revert the changes or delete the application.";
+    public static final String MTA_METADATA_FOR_0_IS_INCOMPLETE = "MTA metadata for entity \"{0}\" is incomplete. This indicates that MTA reserved variables in the entity's metadata were modified manually. Either revert the changes or delete the entity.";
+    public static final String ENV_OF_APP_0_CONTAINS_INVALID_VALUE_FOR_1 = "The environment of application \"{0}\" contains an invalid value for \"{0}\". This indicates that MTA reserved variables in the application's environment were modified manually. Either revert the changes or delete the application.";
+    public static final String METADATA_OF_0_CONTAINS_INVALID_VALUE_FOR_1 = "The metadata of \"{0}\" contains an invalid value for \"{0}\". This indicates that MTA reserved variables in the entity's metadata were modified manually. Either revert the changes or delete the entity.";
     public static final String COULD_NOT_DELETE_SPACE_LEFTOVERS = "Could not delete space leftovers";
     public static final String SERVICE_MISSING_REQUIRED_PARAMETER = "Service \"{0}\" has missing required parameter: {1}";
     public static final String CONTROLLER_URL_NOT_SPECIFIED = "Controller URL is not specified in the environment.";
