9b0ae7bbdb21efd7cb6cc8e9fdd3047a46795e76
Alexander Tsvetkov <alexander.tsvetkov.93@gmail.com>
2020-02-10 12:48:01 +0200

    Try to create DB indexes only if they don't exist

--- com.sap.cloud.lm.sl.cf.persistence/src/main/java/com/sap/cloud/lm/sl/cf/persistence/changes/IndexProcessIdsOfProgressMessagesChange.java
+++ com.sap.cloud.lm.sl.cf.persistence/src/main/java/com/sap/cloud/lm/sl/cf/persistence/changes/IndexProcessIdsOfProgressMessagesChange.java
@@ -4,7 +4,7 @@ public class IndexProcessIdsOfProgressMessagesChange extends AbstractIndexSQLCha
 
     @Override
     protected String getQuery() {
-        return "CREATE INDEX CONCURRENTLY IDX_PROGRESS_MESSAGE_PROCESS_ID ON PROGRESS_MESSAGE(PROCESS_ID)";
+        return "CREATE INDEX CONCURRENTLY IF NOT EXISTS IDX_PROGRESS_MESSAGE_PROCESS_ID ON PROGRESS_MESSAGE(PROCESS_ID)";
     }
 
     @Override
