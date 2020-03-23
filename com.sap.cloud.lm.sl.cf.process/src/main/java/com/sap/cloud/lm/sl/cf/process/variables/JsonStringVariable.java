b6ec5116a3a0644ecd17e29b7d3773bc1eabe9c3
Alexander Tsvetkov <alexander.tsvetkov.93@gmail.com>
2020-03-23 11:36:12 +0200

    Add support for enum process variables
    JIRA: LMCROSSITXSADEPLOY-2001

--- com.sap.cloud.lm.sl.cf.process/src/main/java/com/sap/cloud/lm/sl/cf/process/variables/JsonStringVariable.java
+++ com.sap.cloud.lm.sl.cf.process/src/main/java/com/sap/cloud/lm/sl/cf/process/variables/JsonStringVariable.java
@@ -2,10 +2,13 @@ package com.sap.cloud.lm.sl.cf.process.variables;
 
 import org.immutables.value.Value;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.sap.cloud.lm.sl.common.util.JsonUtil;
 
 @Value.Immutable
-public abstract class JsonStringVariable<T> implements TypedVariable<T> {
+public abstract class JsonStringVariable<T> implements Variable<T> {
+
+    public abstract TypeReference<T> getType();
 
     @Override
     public Serializer<T> getSerializer() {
