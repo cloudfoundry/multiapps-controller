13ca50b133343f224ff68a5cc80377279677fb91
Alexander Tsvetkov <alexander.tsvetkov.93@gmail.com>
2020-04-06 12:35:41 +0300

    Add support for JSON list process variables
    JIRA: LMCROSSITXSADEPLOY-2001

---
+++ com.sap.cloud.lm.sl.cf.process/src/main/java/com/sap/cloud/lm/sl/cf/process/variables/JsonStringListVariable.java
@@ -0,0 +1,39 @@
+package com.sap.cloud.lm.sl.cf.process.variables;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.immutables.value.Value;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.sap.cloud.lm.sl.common.util.JsonUtil;
+
+@Value.Immutable
+public abstract class JsonStringListVariable<T> implements ListVariable<T, List<T>> {
+
+    public abstract TypeReference<T> getType();
+
+    @Override
+    public Serializer<List<T>> getSerializer() {
+        return new Serializer<List<T>>() {
+
+            @Override
+            public Object serialize(List<T> values) {
+                return values.stream()
+                             .map(JsonUtil::toJson)
+                             .collect(Collectors.toList());
+            }
+
+            @SuppressWarnings("unchecked")
+            @Override
+            public List<T> deserialize(Object serializedValue) {
+                List<String> serializedValues = (List<String>) serializedValue;
+                return serializedValues.stream()
+                                       .map(value -> JsonUtil.fromJson(value, getType()))
+                                       .collect(Collectors.toList());
+            }
+
+        };
+    }
+
+}
