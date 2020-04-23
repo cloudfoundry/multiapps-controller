a1f616bc03acba68464e5a74e06ffa7282ac74ad
Alexander Tsvetkov <alexander.tsvetkov.93@gmail.com>
2020-04-23 15:48:27 +0300

    Remove dead code

--- com.sap.cloud.lm.sl.cf.process/src/main/java/com/sap/cloud/lm/sl/cf/process/steps/TaskIdProvider.java
+++
@@ -1,9 +0,0 @@
-package com.sap.cloud.lm.sl.cf.process.steps;
-
-import org.flowable.engine.delegate.DelegateExecution;
-
-public interface TaskIdProvider {
-
-    String getTaskId(DelegateExecution execution);
-
-}
