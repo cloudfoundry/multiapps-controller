a2f5445ec1ec387b3bf9c6f1ea26b3178e3fbe30
Vasil Bozhurski <vasil.bozhurski@sap.com>
2020-04-24 17:03:58 +0300

    fix SLP issues
    -Fix issue that the SLP wrapper exception used was not part of the
    ExceptionMapper which resulted in all error cases (bad content, not
    found, etc) to always be mapped to 500 (internal server error)
    -Fix issue with SLP URLs ending in known file types (for instance .log)
    resulting in Spring throwing an error because it could not convert to
    said type.

--- com.sap.cloud.lm.sl.cf.web/src/test/java/com/sap/cloud/lm/sl/cf/web/resources/CFExceptionMapperTest.java
+++ com.sap.cloud.lm.sl.cf.web/src/test/java/com/sap/cloud/lm/sl/cf/web/resources/CFExceptionMapperTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.sap.cloud.lm.sl.common.ConflictException;
 import com.sap.cloud.lm.sl.common.ContentException;
@@ -37,6 +38,9 @@ public class CFExceptionMapperTest {
             Arguments.of(new ParsingException("Bad request"), new RestResponse(400, "Bad request")),
             Arguments.of(new ContentException("Bad request"), new RestResponse(400, "Bad request")),
             Arguments.of(new SLException("SL exception"), new RestResponse(500, "SL exception")),
+            Arguments.of(new ResponseStatusException(HttpStatus.NOT_FOUND, "Not found"), new RestResponse(404, "Not found")),
+            Arguments.of(new ResponseStatusException(HttpStatus.BAD_REQUEST, "Bad request"), new RestResponse(400, "Bad request")),
+            Arguments.of(new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Something went wrong"), new RestResponse(500, "Something went wrong")),
             Arguments.of(new CloudOperationException(HttpStatus.TOO_MANY_REQUESTS, HttpStatus.TOO_MANY_REQUESTS.getReasonPhrase(), "Rate limit exceeded"), new RestResponse(502, "429 Too Many Requests: Rate limit exceeded"))
         // @formatter:on
         );
