d3bd5df630f78831d2dd8456e0a8bb2202e52ac6
Alexander Tsvetkov <alexander.tsvetkov.93@gmail.com>
2020-05-22 10:34:09 +0300

    Simplify implementation of secure serialization
    JIRA: LMCROSSITXSADEPLOY-1881

--- com.sap.cloud.lm.sl.cf.core/src/main/java/com/sap/cloud/lm/sl/cf/core/security/serialization/CompositeElement.java
+++
@@ -1,9 +0,0 @@
-package com.sap.cloud.lm.sl.cf.core.security.serialization;
-
-import java.util.Collection;
-
-public interface CompositeElement extends Element {
-
-    Collection<Element> getMembers();
-
-}
--- com.sap.cloud.lm.sl.cf.core/src/main/java/com/sap/cloud/lm/sl/cf/core/security/serialization/MapElement.java
+++
@@ -1,9 +0,0 @@
-package com.sap.cloud.lm.sl.cf.core.security.serialization;
-
-public interface MapElement extends CompositeElement {
-
-    void remove(String memberName);
-
-    void add(String memberName, Object memberValue);
-
-}
