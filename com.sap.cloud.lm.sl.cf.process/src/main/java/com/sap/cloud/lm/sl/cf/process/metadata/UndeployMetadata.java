64c3372951576a058d61dd73173f483dfa303b0e
Alexander Tsvetkov <alexander.tsvetkov.93@gmail.com>
2020-05-27 17:07:25 +0300

    Remove static initializers from operation metadata

--- com.sap.cloud.lm.sl.cf.process/src/main/java/com/sap/cloud/lm/sl/cf/process/metadata/UndeployMetadata.java
+++ com.sap.cloud.lm.sl.cf.process/src/main/java/com/sap/cloud/lm/sl/cf/process/metadata/UndeployMetadata.java
@@ -1,8 +1,5 @@
 package com.sap.cloud.lm.sl.cf.process.metadata;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import com.sap.cloud.lm.sl.cf.process.Constants;
 import com.sap.cloud.lm.sl.cf.process.variables.Variables;
 import com.sap.cloud.lm.sl.cf.web.api.model.ImmutableOperationMetadata;
@@ -12,60 +9,54 @@ import com.sap.cloud.lm.sl.cf.web.api.model.ParameterMetadata.ParameterType;
 
 public class UndeployMetadata {
 
-    private static final Set<ImmutableParameterMetadata> PARAMS = new HashSet<>();
-
-    static {
-        PARAMS.add(ImmutableParameterMetadata.builder()
-                                             .id(Variables.DELETE_SERVICES.getName())
-                                             .type(ParameterType.BOOLEAN)
-                                             .defaultValue(Variables.DELETE_SERVICES.getDefaultValue())
-                                             .build());
-        PARAMS.add(ImmutableParameterMetadata.builder()
-                                             .id(Variables.DELETE_SERVICE_KEYS.getName())
-                                             .type(ParameterType.BOOLEAN)
-                                             .defaultValue(Variables.DELETE_SERVICE_KEYS.getDefaultValue())
-                                             .build());
-        PARAMS.add(ImmutableParameterMetadata.builder()
-                                             .id(Variables.DELETE_SERVICE_BROKERS.getName())
-                                             .type(ParameterType.BOOLEAN)
-                                             .defaultValue(Variables.DELETE_SERVICE_BROKERS.getDefaultValue())
-                                             .build());
-        PARAMS.add(ImmutableParameterMetadata.builder()
-                                             .id(Variables.MTA_ID.getName())
-                                             .required(true)
-                                             .type(ParameterType.STRING)
-                                             .build());
-        PARAMS.add(ImmutableParameterMetadata.builder()
-                                             .id(Variables.NO_RESTART_SUBSCRIBED_APPS.getName())
-                                             .defaultValue(Variables.NO_RESTART_SUBSCRIBED_APPS.getDefaultValue())
-                                             .type(ParameterType.BOOLEAN)
-                                             .build());
-        PARAMS.add(ImmutableParameterMetadata.builder()
-                                             .id(Variables.NO_FAIL_ON_MISSING_PERMISSIONS.getName())
-                                             .defaultValue(Variables.NO_FAIL_ON_MISSING_PERMISSIONS.getDefaultValue())
-                                             .type(ParameterType.BOOLEAN)
-                                             .build());
-        PARAMS.add(ImmutableParameterMetadata.builder()
-                                             .id(Variables.ABORT_ON_ERROR.getName())
-                                             .type(ParameterType.BOOLEAN)
-                                             .defaultValue(Variables.ABORT_ON_ERROR.getDefaultValue())
-                                             .build());
-        PARAMS.add(ImmutableParameterMetadata.builder()
-                                             .id(Variables.ENABLE_ENV_DETECTION.getName())
-                                             .type(ParameterType.BOOLEAN)
-                                             .defaultValue(Variables.ENABLE_ENV_DETECTION.getDefaultValue())
-                                             .required(false)
-                                             .build());
-    }
-
     private UndeployMetadata() {
     }
 
     public static OperationMetadata getMetadata() {
         return ImmutableOperationMetadata.builder()
-                                         .parameters(PARAMS)
                                          .diagramId(Constants.UNDEPLOY_SERVICE_ID)
                                          .addVersions(Constants.SERVICE_VERSION_1_0)
+                                         .addParameter(ImmutableParameterMetadata.builder()
+                                                                                 .id(Variables.DELETE_SERVICES.getName())
+                                                                                 .type(ParameterType.BOOLEAN)
+                                                                                 .defaultValue(Variables.DELETE_SERVICES.getDefaultValue())
+                                                                                 .build())
+                                         .addParameter(ImmutableParameterMetadata.builder()
+                                                                                 .id(Variables.DELETE_SERVICE_KEYS.getName())
+                                                                                 .type(ParameterType.BOOLEAN)
+                                                                                 .defaultValue(Variables.DELETE_SERVICE_KEYS.getDefaultValue())
+                                                                                 .build())
+                                         .addParameter(ImmutableParameterMetadata.builder()
+                                                                                 .id(Variables.DELETE_SERVICE_BROKERS.getName())
+                                                                                 .type(ParameterType.BOOLEAN)
+                                                                                 .defaultValue(Variables.DELETE_SERVICE_BROKERS.getDefaultValue())
+                                                                                 .build())
+                                         .addParameter(ImmutableParameterMetadata.builder()
+                                                                                 .id(Variables.MTA_ID.getName())
+                                                                                 .required(true)
+                                                                                 .type(ParameterType.STRING)
+                                                                                 .build())
+                                         .addParameter(ImmutableParameterMetadata.builder()
+                                                                                 .id(Variables.NO_RESTART_SUBSCRIBED_APPS.getName())
+                                                                                 .defaultValue(Variables.NO_RESTART_SUBSCRIBED_APPS.getDefaultValue())
+                                                                                 .type(ParameterType.BOOLEAN)
+                                                                                 .build())
+                                         .addParameter(ImmutableParameterMetadata.builder()
+                                                                                 .id(Variables.NO_FAIL_ON_MISSING_PERMISSIONS.getName())
+                                                                                 .defaultValue(Variables.NO_FAIL_ON_MISSING_PERMISSIONS.getDefaultValue())
+                                                                                 .type(ParameterType.BOOLEAN)
+                                                                                 .build())
+                                         .addParameter(ImmutableParameterMetadata.builder()
+                                                                                 .id(Variables.ABORT_ON_ERROR.getName())
+                                                                                 .type(ParameterType.BOOLEAN)
+                                                                                 .defaultValue(Variables.ABORT_ON_ERROR.getDefaultValue())
+                                                                                 .build())
+                                         .addParameter(ImmutableParameterMetadata.builder()
+                                                                                 .id(Variables.ENABLE_ENV_DETECTION.getName())
+                                                                                 .type(ParameterType.BOOLEAN)
+                                                                                 .defaultValue(Variables.ENABLE_ENV_DETECTION.getDefaultValue())
+                                                                                 .required(false)
+                                                                                 .build())
                                          .build();
     }
 }
