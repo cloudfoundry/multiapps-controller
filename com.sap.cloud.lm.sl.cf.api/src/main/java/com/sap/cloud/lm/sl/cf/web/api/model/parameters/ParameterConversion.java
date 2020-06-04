19424db2adae7a679cb22b6c6412aef626e3c3ab
Alexander Tsvetkov <alexander.tsvetkov.93@gmail.com>
2020-06-04 12:31:12 +0300

    Facilitate processing of operation parameters
    JIRA: LMCROSSITXSADEPLOY-2069

--- com.sap.cloud.lm.sl.cf.api/src/main/java/com/sap/cloud/lm/sl/cf/web/api/model/parameters/ParameterConversion.java
+++ com.sap.cloud.lm.sl.cf.api/src/main/java/com/sap/cloud/lm/sl/cf/web/api/model/parameters/ParameterConversion.java
@@ -13,17 +13,18 @@ public final class ParameterConversion {
     }
 
     public static Map<String, Object> toFlowableVariables(Set<ParameterMetadata> parametersMetadata, Map<String, Object> parameters) {
+        // Do not refactor this loop into a stream. Collectors.toMap has a known bug (https://bugs.openjdk.java.net/browse/JDK-8148463) and
+        // throws NullPointerExceptions when trying to insert null values in the map.
         Map<String, Object> variables = new HashMap<>();
         for (ParameterMetadata parameterMetadata : parametersMetadata) {
-            if (!parameters.containsKey(parameterMetadata.getId())) {
-                continue;
-            }
-            Object value = parameters.get(parameterMetadata.getId());
-
-            ParameterConverter parameterConverter = getParameterConverter(parameterMetadata);
-            variables.put(parameterMetadata.getId(), parameterConverter.convert(value));
+            variables.put(parameterMetadata.getId(), toFlowableVariable(parameterMetadata, parameters));
         }
         return variables;
+    }
+
+    private static Object toFlowableVariable(ParameterMetadata parameterMetadata, Map<String, Object> parameters) {
+        Object value = parameters.computeIfAbsent(parameterMetadata.getId(), key -> parameterMetadata.getDefaultValue());
+        return value == null ? null : getParameterConverter(parameterMetadata).convert(value);
     }
 
     private static ParameterConverter getParameterConverter(ParameterMetadata parameterMetadata) {
