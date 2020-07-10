2d7b9104dff02ebcd1c0b42be3605227def3014a
Boris Iliev <boris.iliev@sap.com>
2020-07-10 14:39:47 +0300

    Adds test for DatabaseSequenceMigrationExecutor

---
+++ com.sap.cloud.lm.sl.cf.database/src/test/java/com/sap/cloud/lm/sl/cf/database/migration/executor/DatabaseSequenceMigrationExecutorTest.java
@@ -0,0 +1,69 @@
+package com.sap.cloud.lm.sl.cf.database.migration.executor;
+
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import com.sap.cloud.lm.sl.cf.database.migration.client.DatabaseQueryClient;
+
+class DatabaseSequenceMigrationExecutorTest {
+
+    private static final String TEST_SEQUENCE_NAME = "test";
+
+    @Mock
+    private DataSource mockSourceDataSource;
+    @Mock
+    private DataSource mockTargetDataSource;
+    @Mock
+    private DatabaseQueryClient mockSourceDatabaseQueryClient;
+    @Mock
+    private DatabaseQueryClient mockTargetDatabaseQueryClient;
+
+    private ImmutableDatabaseSequenceMigrationExecutor databaseSequenceMigrationExecutor;
+
+    @BeforeEach
+    public void initialiseDatabaseTableMigrationExecutorWithMocks() {
+        MockitoAnnotations.initMocks(this);
+        databaseSequenceMigrationExecutor = ImmutableDatabaseSequenceMigrationExecutor.builder()
+                                                                                      .sourceDataSource(mockSourceDataSource)
+                                                                                      .targetDataSource(mockTargetDataSource)
+                                                                                      .sourceDatabaseQueryClient(mockSourceDatabaseQueryClient)
+                                                                                      .targetDatabaseQueryClient(mockTargetDatabaseQueryClient)
+                                                                                      .build();
+    }
+
+    @Test
+    public void testExecuteMigrationInternalWhenLastSequenceValueIsZero() throws SQLException {
+        Mockito.when(mockSourceDatabaseQueryClient.getLastSequenceValue(TEST_SEQUENCE_NAME))
+               .thenReturn(0L);
+
+        Assertions.assertThrows(IllegalStateException.class, () -> databaseSequenceMigrationExecutor.executeMigration(TEST_SEQUENCE_NAME));
+    }
+
+    @Test
+    public void testExecuteMigrationInternalWhenLastSequenceValueIsNegative() throws SQLException {
+        Mockito.when(mockSourceDatabaseQueryClient.getLastSequenceValue(TEST_SEQUENCE_NAME))
+               .thenReturn(-1L);
+
+        Assertions.assertThrows(IllegalStateException.class, () -> databaseSequenceMigrationExecutor.executeMigration(TEST_SEQUENCE_NAME));
+    }
+
+    @Test
+    public void testExecuteMigrationInternalWhenLastSequenceValueIsPositive() throws SQLException {
+        Mockito.when(mockSourceDatabaseQueryClient.getLastSequenceValue(TEST_SEQUENCE_NAME))
+               .thenReturn(1L);
+
+        databaseSequenceMigrationExecutor.executeMigration(TEST_SEQUENCE_NAME);
+
+        Mockito.verify(mockTargetDatabaseQueryClient)
+               .updateSequence(TEST_SEQUENCE_NAME, 1L);
+    }
+
+}
