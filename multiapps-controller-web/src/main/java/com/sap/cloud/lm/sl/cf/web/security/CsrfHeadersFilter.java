5bfeb39a580caecde7be5fe7a34a96bfbae4bc13
Alexander Tsvetkov <alexander.tsvetkov.93@gmail.com>
2020-07-24 09:18:37 +0300

    Use more OSS-friendly group/artifact IDs (Part 1)
    Split up into several commits to preserve Git history (rename detection
    fails with too many files in a single commit).

---
+++ multiapps-controller-web/src/main/java/com/sap/cloud/lm/sl/cf/web/security/CsrfHeadersFilter.java
@@ -0,0 +1,42 @@
+package com.sap.cloud.lm.sl.cf.web.security;
+
+import java.io.IOException;
+
+import javax.inject.Named;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.sap.cloud.lm.sl.cf.web.Constants;
+
+/**
+ * A filter that is added in the Spring filter chain after the CSRF_FILTER, reads the CSRF from the session and exposes its information in
+ * response headers
+ * 
+ * @author i031908
+ */
+@Named("csrfHeadersFilter")
+public class CsrfHeadersFilter extends OncePerRequestFilter {
+
+    private static final String SPRING_SECURITY_CSRF_SESSION_ATTRIBUTE = "_csrf";
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+        throws ServletException, IOException {
+        filterChain.doFilter(request, response);
+        // Do not move filterChain.doFilter after the header copy!
+        // The order is deliberately set to copy the csrf token on the way back, after the whole filter chain has passed.
+        // This is because user authentication may force session & token recreation after this filter.
+        CsrfToken token = (CsrfToken) request.getAttribute(SPRING_SECURITY_CSRF_SESSION_ATTRIBUTE);
+        if (token != null) {
+            response.setHeader(Constants.CSRF_HEADER_NAME, token.getHeaderName());
+            response.setHeader(Constants.CSRF_PARAM_NAME, token.getParameterName());
+            response.setHeader(Constants.CSRF_TOKEN, token.getToken());
+        }
+    }
+
+}
---
+++ multiapps-controller-web/src/main/java/com/sap/cloud/lm/sl/cf/web/security/CustomAuthenticationProvider.java
@@ -0,0 +1,107 @@
+package com.sap.cloud.lm.sl.cf.web.security;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.cloudfoundry.client.lib.CloudOperationException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.AuthenticationServiceException;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.InsufficientAuthenticationException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+
+import com.sap.cloud.lm.sl.cf.client.TokenProvider;
+import com.sap.cloud.lm.sl.cf.client.util.TokenFactory;
+import com.sap.cloud.lm.sl.cf.core.auditlogging.AuditLoggingProvider;
+import com.sap.cloud.lm.sl.cf.core.cf.TokenProviderFactory;
+import com.sap.cloud.lm.sl.cf.core.security.token.parsers.TokenParserChain;
+import com.sap.cloud.lm.sl.cf.core.util.ApplicationConfiguration;
+import com.sap.cloud.lm.sl.cf.core.util.SecurityUtil;
+import com.sap.cloud.lm.sl.cf.web.Messages;
+
+@Named("customAuthenticationProvider")
+public class CustomAuthenticationProvider implements AuthenticationProvider {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CustomAuthenticationProvider.class);
+
+    @Inject
+    @Qualifier("tokenStore")
+    TokenStore tokenStore;
+
+    @Inject
+    @Qualifier("tokenProviderFactory")
+    TokenProviderFactory cloudFoundryTokenProviderFactory;
+
+    @Inject
+    ApplicationConfiguration configuration;
+
+    @Inject
+    TokenFactory tokenFactory;
+
+    @Inject
+    TokenParserChain tokenParserChain;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) {
+        if (!configuration.isBasicAuthEnabled())
+            throw new InsufficientAuthenticationException("Basic authentication is not enabled, use OAuth2");
+
+        try {
+            UsernamePasswordAuthenticationToken auth = (UsernamePasswordAuthenticationToken) authentication;
+            String userName = (String) auth.getPrincipal();
+            String password = (String) auth.getCredentials();
+
+            TokenProvider tokenProvider = cloudFoundryTokenProviderFactory.createTokenProvider(userName, password);
+
+            // Get a valid token from the client
+            // If this works, consider the request authenticated
+            OAuth2AccessToken token = (tokenProvider != null) ? tokenProvider.getToken() : null;
+
+            if (token == null) {
+                String message = "Null access token returned by cloud controller";
+                AuditLoggingProvider.getFacade()
+                                    .logSecurityIncident(message);
+                throw new AuthenticationServiceException(message);
+            }
+
+            // Check if an authentication for this token already exists in the token store
+            OAuth2Authentication auth2 = tokenStore.readAuthentication(token);
+            if (auth2 == null) {
+                token = tokenParserChain.parse(token.getValue());
+
+                // Create an authentication for the token and store it in the token store
+                auth2 = SecurityUtil.createAuthentication(SecurityUtil.CLIENT_ID, token.getScope(), SecurityUtil.getTokenUserInfo(token));
+                storeAccessToken(token, auth2);
+            }
+
+            return auth2;
+        } catch (CloudOperationException e) {
+            String message = Messages.CANNOT_AUTHENTICATE_WITH_CLOUD_CONTROLLER;
+            AuditLoggingProvider.getFacade()
+                                .logSecurityIncident(message);
+            throw new BadCredentialsException(message, e);
+        }
+    }
+
+    private void storeAccessToken(OAuth2AccessToken token, OAuth2Authentication auth2) {
+        try {
+            tokenStore.storeAccessToken(token, auth2);
+        } catch (DataIntegrityViolationException e) {
+            LOGGER.debug(Messages.ERROR_STORING_TOKEN_DUE_TO_INTEGRITY_VIOLATION, e);
+            // Ignoring the exception as the token and authentication are already persisted by another client.
+        }
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return authentication.equals(UsernamePasswordAuthenticationToken.class);
+    }
+}
