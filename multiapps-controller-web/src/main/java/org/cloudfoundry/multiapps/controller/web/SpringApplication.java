e89fc38015a34541b0144611b556adc9e8a4ad44
Alexander Tsvetkov <alexander.tsvetkov.93@gmail.com>
2020-10-01 17:09:23 +0300

    Make Spring configuration resistant to renames
    This commit makes sure we don't hardcode any package/class names in our
    Spring configuration.
    
    JIRA: LMCROSSITXSADEPLOY-2208

---
+++ multiapps-controller-web/src/main/java/org/cloudfoundry/multiapps/controller/web/SpringApplication.java
@@ -0,0 +1,10 @@
+package org.cloudfoundry.multiapps.controller.web;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
+
+@ComponentScan(basePackageClasses = org.cloudfoundry.multiapps.controller.PackageMarker.class)
+@ImportResource("classpath:security-context.xml")
+public class SpringApplication {
+
+}
