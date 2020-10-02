3bee50c55434cf3ac3ba4c90cb6396c965f67207
Alexander Tsvetkov <alexander.tsvetkov.93@gmail.com>
2020-10-02 19:58:33 +0300

    Fix database connection pool exhaustion

--- multiapps-controller-persistence/src/main/java/org/cloudfoundry/multiapps/controller/persistence/util/DataSourceFactory.java
+++ multiapps-controller-persistence/src/main/java/org/cloudfoundry/multiapps/controller/persistence/util/DataSourceFactory.java
@@ -24,6 +24,7 @@ public class DataSourceFactory {
         hikariConfig.setUsername(service.getUsername());
         hikariConfig.setPassword(service.getPassword());
         hikariConfig.setJdbcUrl(service.getJdbcUrl());
+        hikariConfig.setMaxLifetime(0);
         hikariConfig.setConnectionTimeout(60000);
         if (maximumPoolSize != null) {
             hikariConfig.setMaximumPoolSize(maximumPoolSize);
