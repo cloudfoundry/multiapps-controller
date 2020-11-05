92979fdf3710a28d6692cb039555635245d26c3e
Velinov <boyan.velinov@sap.com>
2020-11-05 16:11:00 +0200

    Increaseb WebClient maxInMemorySize to 1MB
    Jira: LMCROSSITXSADEPLOY-2247

--- multiapps-controller-core/src/main/java/org/cloudfoundry/multiapps/controller/core/cf/clients/WebClientFactory.java
+++ multiapps-controller-core/src/main/java/org/cloudfoundry/multiapps/controller/core/cf/clients/WebClientFactory.java
@@ -2,6 +2,7 @@ package org.cloudfoundry.multiapps.controller.core.cf.clients;
 
 import javax.inject.Named;
 
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import com.sap.cloudfoundry.client.facade.CloudControllerClient;
@@ -10,11 +11,18 @@ import com.sap.cloudfoundry.client.facade.util.RestUtil;
 @Named
 public class WebClientFactory {
 
+    private static final int MAX_IN_MEMORY_SIZE = 1 * 1024 * 1024; // 1MB
+    
     public WebClient getWebClient(CloudControllerClient client) {
         WebClient.Builder webClientBuilder = new RestUtil().createWebClient(false)
                                                            .mutate()
                                                            .baseUrl(client.getCloudControllerUrl()
                                                                           .toString());
+        webClientBuilder.exchangeStrategies(ExchangeStrategies.builder()
+                                            .codecs(configurer -> configurer
+                                                    .defaultCodecs()
+                                                    .maxInMemorySize(MAX_IN_MEMORY_SIZE))
+                                                  .build());
         webClientBuilder.defaultHeaders(httpHeaders -> httpHeaders.setBearerAuth(computeAuthorizationToken(client)));
         return webClientBuilder.build();
     }
