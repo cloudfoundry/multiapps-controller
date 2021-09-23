428f363a51d00db27df2fe6659570c36017ff0a5
Rangel Ivanov <rangel.ivanov@sap.com>
2021-09-23 14:24:56 +0300

    Fix some sonar issues

--- multiapps-controller-process/src/main/java/org/cloudfoundry/multiapps/controller/process/util/StagingApplicationAttributeUpdater.java
+++ multiapps-controller-process/src/main/java/org/cloudfoundry/multiapps/controller/process/util/StagingApplicationAttributeUpdater.java
@@ -6,7 +6,6 @@ import java.util.Objects;
 import org.cloudfoundry.multiapps.controller.process.util.ElementUpdater.UpdateStrategy;
 
 import com.sap.cloudfoundry.client.facade.domain.CloudApplication;
-import com.sap.cloudfoundry.client.facade.domain.DockerInfo;
 import com.sap.cloudfoundry.client.facade.domain.Staging;
 
 public class StagingApplicationAttributeUpdater extends ApplicationAttributeUpdater {
@@ -31,19 +30,15 @@ public class StagingApplicationAttributeUpdater extends ApplicationAttributeUpda
         String healthCheckType = staging.getHealthCheckType();
         String healthCheckHttpEndpoint = staging.getHealthCheckHttpEndpoint();
         Boolean sshEnabled = staging.isSshEnabled();
-        return (buildpacks != null && !buildpacks.equals(existingStaging.getBuildpacks()))
-            || (command != null && !command.equals(existingStaging.getCommand()))
-            || (stackName != null && !stackName.equals(existingStaging.getStackName()))
-            || (healthCheckTimeout != null && !healthCheckTimeout.equals(existingStaging.getHealthCheckTimeout()))
-            || (healthCheckType != null && !healthCheckType.equals(existingStaging.getHealthCheckType()))
-            || (healthCheckInvocationTimeout != null && !healthCheckInvocationTimeout.equals(existingStaging.getInvocationTimeout()))
-            || (healthCheckHttpEndpoint != null && !healthCheckHttpEndpoint.equals(existingStaging.getHealthCheckHttpEndpoint()))
-            || (sshEnabled != null && !sshEnabled.equals(existingStaging.isSshEnabled())
-                || isDockerInfoModified(existingStaging.getDockerInfo(), staging.getDockerInfo()));
-    }
-
-    private boolean isDockerInfoModified(DockerInfo existingDockerInfo, DockerInfo newDockerInfo) {
-        return !Objects.equals(existingDockerInfo, newDockerInfo);
+        return !Objects.equals(buildpacks, existingStaging.getBuildpacks())
+            || !Objects.equals(command, existingStaging.getCommand())
+            || !Objects.equals(stackName, existingStaging.getStackName())
+            || !Objects.equals(healthCheckTimeout, existingStaging.getHealthCheckTimeout())
+            || !Objects.equals(healthCheckType, existingStaging.getHealthCheckType())
+            || !Objects.equals(healthCheckInvocationTimeout, existingStaging.getInvocationTimeout())
+            || !Objects.equals(healthCheckHttpEndpoint, existingStaging.getHealthCheckHttpEndpoint())
+            || !Objects.equals(sshEnabled, existingStaging.isSshEnabled())
+            || !Objects.equals(staging.getDockerInfo(), existingStaging.getDockerInfo());
     }
 
     @Override
