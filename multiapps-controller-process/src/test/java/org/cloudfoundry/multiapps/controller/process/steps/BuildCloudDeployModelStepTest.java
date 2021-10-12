2f0c2725ec68011d1c569243d08008ad4d4911f4
abdermendz <abil.dermendzhiev@sap.com>
2021-10-12 10:24:48 +0300

    Add unit test for extracting services step

--- multiapps-controller-process/src/test/java/org/cloudfoundry/multiapps/controller/process/steps/BuildCloudDeployModelStepTest.java
+++ multiapps-controller-process/src/test/java/org/cloudfoundry/multiapps/controller/process/steps/BuildCloudDeployModelStepTest.java
@@ -1,24 +1,13 @@
 package org.cloudfoundry.multiapps.controller.process.steps;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
-
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.stream.Stream;
-
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.sap.cloudfoundry.client.facade.domain.CloudServiceKey;
 import org.cloudfoundry.multiapps.common.test.TestUtil;
 import org.cloudfoundry.multiapps.common.test.Tester.Expectation;
 import org.cloudfoundry.multiapps.common.util.JsonUtil;
-import org.cloudfoundry.multiapps.controller.client.lib.domain.CloudServiceInstanceExtended;
 import org.cloudfoundry.multiapps.controller.core.cf.util.ModulesCloudModelBuilderContentCalculator;
 import org.cloudfoundry.multiapps.controller.core.cf.v2.ApplicationCloudModelBuilder;
 import org.cloudfoundry.multiapps.controller.core.cf.v2.ServiceKeysCloudModelBuilder;
-import org.cloudfoundry.multiapps.controller.core.cf.v2.ServicesCloudModelBuilder;
 import org.cloudfoundry.multiapps.controller.core.helpers.ModuleToDeployHelper;
 import org.cloudfoundry.multiapps.controller.core.model.DeployedMta;
 import org.cloudfoundry.multiapps.controller.core.test.DescriptorTestUtil;
@@ -30,8 +19,16 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.Mock;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.sap.cloudfoundry.client.facade.domain.CloudServiceKey;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 class BuildCloudDeployModelStepTest extends SyncFlowableStepTest<BuildCloudDeployModelStep> {
 
@@ -42,7 +39,6 @@ class BuildCloudDeployModelStepTest extends SyncFlowableStepTest<BuildCloudDeplo
 
     protected List<Module> modulesToDeploy;
     protected DeployedMta deployedMta;
-    protected List<CloudServiceInstanceExtended> servicesToBind;
     protected Map<String, List<CloudServiceKey>> serviceKeys;
 
     @Mock
@@ -52,15 +48,13 @@ class BuildCloudDeployModelStepTest extends SyncFlowableStepTest<BuildCloudDeplo
     @Mock
     protected ServiceKeysCloudModelBuilder serviceKeysCloudModelBuilder;
     @Mock
-    protected ServicesCloudModelBuilder servicesCloudModelBuilder;
-    @Mock
     protected ModuleToDeployHelper moduleToDeployHelper;
 
     public static Stream<Arguments> testExecute() {
         return Stream.of(
 // @formatter:off
-                Arguments.of(new StepInput("modules-to-deploy-01.json", "services-to-bind-01.json", "services-to-create-01.json", "service-keys-01.json", List.of("api.cf.neo.ondemand.com"), "deployed-mta-12.json")),
-                Arguments.of(new StepInput("modules-to-deploy-01.json", "services-to-bind-01.json", "services-to-create-01.json", "service-keys-01.json", List.of("api.cf.neo.ondemand.com"), null))
+                Arguments.of(new StepInput("modules-to-deploy-01.json", "services-to-create-01.json", "service-keys-01.json", List.of("api.cf.neo.ondemand.com"), "deployed-mta-12.json")),
+                Arguments.of(new StepInput("modules-to-deploy-01.json", "services-to-create-01.json", "service-keys-01.json", List.of("api.cf.neo.ondemand.com"), null))
 // @formatter:on
         );
     }
@@ -74,8 +68,6 @@ class BuildCloudDeployModelStepTest extends SyncFlowableStepTest<BuildCloudDeplo
 
         assertStepFinishedSuccessfully();
 
-        tester.test(() -> context.getVariable(Variables.SERVICES_TO_BIND),
-                    new Expectation(Expectation.Type.JSON, input.servicesToBindLocation));
         tester.test(() -> context.getVariable(Variables.SERVICE_KEYS_TO_CREATE),
                     new Expectation(Expectation.Type.JSON, input.serviceKeysLocation));
         tester.test(() -> context.getVariable(Variables.MODULES_TO_DEPLOY),
@@ -91,10 +83,6 @@ class BuildCloudDeployModelStepTest extends SyncFlowableStepTest<BuildCloudDeplo
         modulesToDeploy = JsonUtil.fromJson(modulesToDeployString, new TypeReference<>() {
         });
 
-        String servicesToBindString = TestUtil.getResourceAsString(input.servicesToBindLocation, getClass());
-        servicesToBind = JsonUtil.fromJson(servicesToBindString, new TypeReference<>() {
-        });
-
         String serviceKeysString = TestUtil.getResourceAsString(input.serviceKeysLocation, getClass());
         serviceKeys = JsonUtil.fromJson(serviceKeysString, new TypeReference<>() {
         });
@@ -106,7 +94,6 @@ class BuildCloudDeployModelStepTest extends SyncFlowableStepTest<BuildCloudDeplo
         when(moduleToDeployHelper.isApplication(any())).thenReturn(true);
         when(modulesCloudModelBuilderContentCalculator.calculateContentForBuilding(any())).thenReturn(modulesToDeploy);
         when(applicationCloudModelBuilder.getApplicationDomains(any(), any())).thenReturn(input.customDomains);
-        when(servicesCloudModelBuilder.build(any())).thenReturn(servicesToBind);
         when(serviceKeysCloudModelBuilder.build()).thenReturn(serviceKeys);
         context.setVariable(Variables.DEPLOYED_MTA, deployedMta);
     }
@@ -126,16 +113,14 @@ class BuildCloudDeployModelStepTest extends SyncFlowableStepTest<BuildCloudDeplo
     protected static class StepInput {
 
         public final String servicesToBindLocation;
-        public final String servicesToCreateLocation;
         public final String deployedMtaLocation;
         public final String serviceKeysLocation;
         public final String modulesToDeployLocation;
         public final List<String> customDomains;
 
-        public StepInput(String modulesToDeployLocation, String servicesToBindLocation, String servicesToCreateLocation,
-                         String serviceKeysLocation, List<String> customDomains, String deployedMtaLocation) {
+        public StepInput(String modulesToDeployLocation, String servicesToBindLocation, String serviceKeysLocation,
+                         List<String> customDomains, String deployedMtaLocation) {
             this.servicesToBindLocation = servicesToBindLocation;
-            this.servicesToCreateLocation = servicesToCreateLocation;
             this.deployedMtaLocation = deployedMtaLocation;
             this.serviceKeysLocation = serviceKeysLocation;
             this.modulesToDeployLocation = modulesToDeployLocation;
@@ -155,11 +140,6 @@ class BuildCloudDeployModelStepTest extends SyncFlowableStepTest<BuildCloudDeplo
                   getModulesContentCalculator(ProcessContext context, List<Module> mtaDescriptorModules, Set<String> mtaManifestModuleNames,
                                               Set<String> deployedModuleNames, Set<String> allMtaModuleNames) {
             return modulesCloudModelBuilderContentCalculator;
-        }
-
-        @Override
-        protected ServicesCloudModelBuilder getServicesCloudModelBuilder(ProcessContext context) {
-            return servicesCloudModelBuilder;
         }
 
         @Override
---
+++ multiapps-controller-process/src/test/java/org/cloudfoundry/multiapps/controller/process/steps/ExtractServicesDeploymentStepTest.java
@@ -0,0 +1,89 @@
+package org.cloudfoundry.multiapps.controller.process.steps;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import org.cloudfoundry.multiapps.common.test.TestUtil;
+import org.cloudfoundry.multiapps.common.test.Tester;
+import org.cloudfoundry.multiapps.common.util.JsonUtil;
+import org.cloudfoundry.multiapps.controller.client.lib.domain.CloudServiceInstanceExtended;
+import org.cloudfoundry.multiapps.controller.core.cf.v2.ServicesCloudModelBuilder;
+import org.cloudfoundry.multiapps.controller.process.variables.Variables;
+import org.cloudfoundry.multiapps.mta.model.Resource;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.Mock;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+class ExtractServicesDeploymentStepTest extends SyncFlowableStepTest<ExtractServicesDeploymentStep> {
+
+    private List<Resource> batchOfResources;
+    private List<CloudServiceInstanceExtended> servicesToBind;
+
+    @Mock
+    protected ServicesCloudModelBuilder servicesCloudModelBuilder;
+
+    public static Stream<Arguments> testExecute() {
+        return Stream.of(
+// @formatter:off
+                Arguments.of(new ExtractServicesDeploymentStepTest.StepInput("services-to-create-01.json","batches-to-process-01.json"),
+                             new ExtractServicesDeploymentStepTest.StepInput("services-to-create-02.json","batches-to-process-02.json"))
+// @formatter:on
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void testExecute(ExtractServicesDeploymentStepTest.StepInput input) {
+        loadParameters(input);
+        step.execute(execution);
+        assertStepFinishedSuccessfully();
+
+        tester.test(() -> context.getVariable(Variables.BATCH_TO_PROCESS),
+                    new Tester.Expectation(Tester.Expectation.Type.JSON, input.batchToProcessLocation));
+        tester.test(() -> context.getVariable(Variables.SERVICES_TO_BIND),
+                    new Tester.Expectation(Tester.Expectation.Type.JSON, input.servicesToBindLocation));
+        assertEquals(batchOfResources.size(), context.getVariable(Variables.SERVICES_TO_CREATE_COUNT));
+    }
+
+    private void loadParameters(StepInput input) {
+        String batchToProcessString = TestUtil.getResourceAsString(input.batchToProcessLocation, getClass());
+        batchOfResources = JsonUtil.fromJson(batchToProcessString, new TypeReference<>() {
+        });
+
+        String servicesToBindString = TestUtil.getResourceAsString(input.servicesToBindLocation, getClass());
+        servicesToBind = JsonUtil.fromJson(servicesToBindString, new TypeReference<>() {
+        });
+
+        context.setVariable(Variables.BATCH_TO_PROCESS, batchOfResources);
+        context.setVariable(Variables.SERVICES_TO_BIND, servicesToBind);
+        when(servicesCloudModelBuilder.build(any())).thenReturn(servicesToBind);
+    }
+
+    @Override
+    protected ExtractServicesDeploymentStep createStep() {
+        return new ExtractServicesDeploymentStepMock();
+    }
+
+    protected static class StepInput {
+        public final String servicesToBindLocation;
+        public final String batchToProcessLocation;
+
+        public StepInput(String servicesToBindLocation, String batchToProcessLocation) {
+            this.servicesToBindLocation = servicesToBindLocation;
+            this.batchToProcessLocation = batchToProcessLocation;
+        }
+    }
+
+    private class ExtractServicesDeploymentStepMock extends ExtractServicesDeploymentStep {
+        @Override
+        protected ServicesCloudModelBuilder getServicesCloudModelBuilder(ProcessContext context) {
+            return servicesCloudModelBuilder;
+        }
+    }
+}
