e104c361c1959fba3c5abb377108284064eff528
IvanBorislavovDimitrov <starstrucks1997@gmail.com>
2022-04-08 17:21:28 +0300

    Add locking on clean-up job

--- com.sap.cloud.lm.sl.cf.web/src/main/java/com/sap/cloud/lm/sl/cf/web/configuration/SpringSchedulingConfiguration.java
+++ com.sap.cloud.lm.sl.cf.web/src/main/java/com/sap/cloud/lm/sl/cf/web/configuration/SpringSchedulingConfiguration.java
@@ -1,9 +1,11 @@
 package com.sap.cloud.lm.sl.cf.web.configuration;
 
+import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
 @EnableScheduling
+@EnableSchedulerLock(defaultLockAtMostFor = "1h")
 public class SpringSchedulingConfiguration {
 }
