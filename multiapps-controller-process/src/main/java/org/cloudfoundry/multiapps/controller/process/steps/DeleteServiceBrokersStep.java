3daa21ad2d41ca0c8129a38342164df6ddfe21d9
theghost5800 <kristian0001@gmail.com>
2022-04-27 12:19:27 +0300

    Undo Revert "Add polling of async operation"
    This reverts commit 18186414b51a3e2d2c25fc80b85d5be1480dc4a6.

--- multiapps-controller-process/src/main/java/org/cloudfoundry/multiapps/controller/process/steps/DeleteServiceBrokersStep.java
+++ multiapps-controller-process/src/main/java/org/cloudfoundry/multiapps/controller/process/steps/DeleteServiceBrokersStep.java
@@ -1,7 +1,12 @@
 package org.cloudfoundry.multiapps.controller.process.steps;
 
 import java.text.MessageFormat;
+import java.time.Duration;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Optional;
 
 import javax.inject.Named;
 
@@ -22,22 +27,30 @@ import com.sap.cloudfoundry.client.facade.domain.CloudServiceBroker;
 
 @Named("deleteServiceBrokersStep")
 @Scope(BeanDefinition.SCOPE_PROTOTYPE)
-public class DeleteServiceBrokersStep extends SyncFlowableStep {
+public class DeleteServiceBrokersStep extends TimeoutAsyncFlowableStep {
+
+    private static final Duration ASYNC_JOB_POLLING_TIMEOUT = Duration.ofMinutes(30);
 
     @Override
-    protected StepPhase executeStep(ProcessContext context) {
+    protected StepPhase executeAsyncStep(ProcessContext context) {
         getStepLogger().debug(Messages.DELETING_SERVICE_BROKERS);
 
         List<CloudApplication> appsToUndeploy = context.getVariable(Variables.APPS_TO_UNDEPLOY);
         CloudControllerClient client = context.getControllerClient();
         List<String> createdOrUpdatedServiceBrokers = getCreatedOrUpdatedServiceBrokerNames(context);
 
-        for (CloudApplication app : appsToUndeploy) {
-            deleteServiceBrokerIfNecessary(context, app, createdOrUpdatedServiceBrokers, client);
+        Map<String, CloudServiceBroker> serviceBrokersToDelete = getServiceBrokersToDelete(appsToUndeploy, createdOrUpdatedServiceBrokers,
+                                                                                           client);
+
+        Map<String, String> serviceBrokerNamesJobIds = deleteServiceBrokers(context, serviceBrokersToDelete, client);
+
+        if (serviceBrokerNamesJobIds.isEmpty()) {
+            getStepLogger().debug(Messages.SERVICE_BROKERS_DELETED);
+            return StepPhase.DONE;
         }
 
-        getStepLogger().debug(Messages.SERVICE_BROKERS_DELETED);
-        return StepPhase.DONE;
+        context.setVariable(Variables.SERVICE_BROKER_NAMES_JOB_IDS, serviceBrokerNamesJobIds);
+        return StepPhase.POLL;
     }
 
     @Override
@@ -51,43 +64,82 @@ public class DeleteServiceBrokersStep extends SyncFlowableStep {
         return ExceptionMessageTailMapper.map(configuration, CloudComponents.SERVICE_BROKERS, offering);
     }
 
+    @Override
+    public Duration getTimeout(ProcessContext context) {
+        return ASYNC_JOB_POLLING_TIMEOUT;
+    }
+
+    @Override
+    protected List<AsyncExecution> getAsyncStepExecutions(ProcessContext context) {
+        return List.of(new PollServiceBrokersOperationsExecution());
+    }
+
     protected List<String> getCreatedOrUpdatedServiceBrokerNames(ProcessContext context) {
         return StepsUtil.getCreatedOrUpdatedServiceBrokerNames(context);
     }
 
-    private void deleteServiceBrokerIfNecessary(ProcessContext context, CloudApplication app, List<String> createdOrUpdatedServiceBrokers,
-                                                CloudControllerClient client) {
+    private Map<String, CloudServiceBroker> getServiceBrokersToDelete(List<CloudApplication> appsToUndeploy,
+                                                                      List<String> createdOrUpdatedServiceBrokers,
+                                                                      CloudControllerClient client) {
+        Map<String, CloudServiceBroker> serviceBrokersToDelete = new HashMap<>();
+        for (CloudApplication app : appsToUndeploy) {
+            Optional<CloudServiceBroker> optionalServiceBroker = getServiceBrokerToDelete(app, createdOrUpdatedServiceBrokers, client);
+            if (optionalServiceBroker.isPresent()) {
+                serviceBrokersToDelete.put(app.getName(), optionalServiceBroker.get());
+            }
+        }
+        return serviceBrokersToDelete;
+    }
+
+    private Optional<CloudServiceBroker> getServiceBrokerToDelete(CloudApplication app, List<String> createdOrUpdatedServiceBrokers,
+                                                                  CloudControllerClient client) {
         ApplicationAttributes appAttributes = ApplicationAttributes.fromApplication(app);
         if (!appAttributes.get(SupportedParameters.CREATE_SERVICE_BROKER, Boolean.class, false)) {
-            return;
+            return Optional.empty();
         }
-        String name = appAttributes.get(SupportedParameters.SERVICE_BROKER_NAME, String.class, app.getName());
 
-        CloudServiceBroker serviceBroker = client.getServiceBroker(name, false);
-        if (serviceBroker != null && !createdOrUpdatedServiceBrokers.contains(name)) {
+        String serviceBrokerName = appAttributes.get(SupportedParameters.SERVICE_BROKER_NAME, String.class, app.getName());
+        return Optional.ofNullable(client.getServiceBroker(serviceBrokerName, false))
+                       .filter(serviceBroker -> !createdOrUpdatedServiceBrokers.contains(serviceBroker.getName()));
+    }
+
+    private Map<String, String> deleteServiceBrokers(ProcessContext context, Map<String, CloudServiceBroker> serviceBrokersToDelete,
+                                                     CloudControllerClient client) {
+        Map<String, String> serviceBrokerNamesJobIds = new HashMap<>();
+        for (Entry<String, CloudServiceBroker> appNameServiceBroker : serviceBrokersToDelete.entrySet()) {
+            String appName = appNameServiceBroker.getKey();
+            CloudServiceBroker serviceBroker = appNameServiceBroker.getValue();
             try {
-                getStepLogger().info(MessageFormat.format(Messages.DELETING_SERVICE_BROKER, name, app.getName()));
-                client.deleteServiceBroker(name);
-                getStepLogger().debug(MessageFormat.format(Messages.DELETED_SERVICE_BROKER, name, app.getName()));
-            } catch (CloudOperationException e) {
-                switch (e.getStatusCode()) {
-                    case FORBIDDEN:
-                        if (shouldSucceed(context)) {
-                            getStepLogger().warn(Messages.DELETE_OF_SERVICE_BROKERS_FAILED_403, name);
-                            return;
-                        }
-                        context.setVariable(Variables.SERVICE_OFFERING, name);
-                        throw new CloudServiceBrokerException(e);
-                    case BAD_GATEWAY:
-                        context.setVariable(Variables.SERVICE_OFFERING, name);
-                        throw new CloudServiceBrokerException(e);
-                    case CONFLICT:
-                        getStepLogger().warn(Messages.DELETE_OF_SERVICE_BROKERS_FAILED_409, name);
-                        throw new CloudServiceBrokerException(e);
-                    default:
-                        throw e;
+                getStepLogger().info(MessageFormat.format(Messages.DELETING_SERVICE_BROKER, serviceBroker.getName(), appName));
+                String jobId = client.deleteServiceBroker(serviceBroker.getName());
+                getStepLogger().debug(MessageFormat.format(Messages.DELETE_SERVICE_BROKER_TRIGERRED, serviceBroker.getName(), appName));
+                if (jobId != null) {
+                    serviceBrokerNamesJobIds.put(serviceBroker.getName(), jobId);
                 }
+            } catch (CloudOperationException e) {
+                handleCloudOperationException(context, serviceBroker, e);
             }
+        }
+        return serviceBrokerNamesJobIds;
+    }
+
+    private void handleCloudOperationException(ProcessContext context, CloudServiceBroker serviceBroker, CloudOperationException e) {
+        switch (e.getStatusCode()) {
+            case FORBIDDEN:
+                if (shouldSucceed(context)) {
+                    getStepLogger().warn(Messages.DELETE_OF_SERVICE_BROKERS_FAILED_403, serviceBroker.getName());
+                    return;
+                }
+                context.setVariable(Variables.SERVICE_OFFERING, serviceBroker.getName());
+                throw new CloudServiceBrokerException(e);
+            case BAD_GATEWAY:
+                context.setVariable(Variables.SERVICE_OFFERING, serviceBroker.getName());
+                throw new CloudServiceBrokerException(e);
+            case CONFLICT:
+                getStepLogger().warn(Messages.DELETE_OF_SERVICE_BROKERS_FAILED_409, serviceBroker.getName());
+                throw new CloudServiceBrokerException(e);
+            default:
+                throw e;
         }
     }
 
