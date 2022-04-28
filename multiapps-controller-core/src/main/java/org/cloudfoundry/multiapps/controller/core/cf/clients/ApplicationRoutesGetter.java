32eaf13759e2e08afe19684d3425f0468e9c5fae
Rangel Ivanov <rangel.ivanov@sap.com>
2022-04-28 11:05:15 +0300

    Add batching to service route bindings request

--- multiapps-controller-core/src/main/java/org/cloudfoundry/multiapps/controller/core/cf/clients/ApplicationRoutesGetter.java
+++ multiapps-controller-core/src/main/java/org/cloudfoundry/multiapps/controller/core/cf/clients/ApplicationRoutesGetter.java
@@ -1,6 +1,7 @@
 package org.cloudfoundry.multiapps.controller.core.cf.clients;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
@@ -11,6 +12,8 @@ import com.sap.cloudfoundry.client.facade.CloudControllerClient;
 import org.cloudfoundry.multiapps.controller.client.lib.domain.CloudRouteExtended;
 
 public class ApplicationRoutesGetter extends CustomControllerClient {
+
+    private static final int MAX_CHAR_LENGTH_FOR_PARAMS_IN_REQUEST = 4000;
 
     private final CloudEntityResourceMapper resourceMapper = new CloudEntityResourceMapper();
 
@@ -25,9 +28,8 @@ public class ApplicationRoutesGetter extends CustomControllerClient {
     private List<CloudRouteExtended> getRoutesInternal(String appName) {
         String appRoutesUrl = getAppRoutesUrl(client.getApplicationGuid(appName));
         var resourcesWithIncluded = getAllResourcesWithIncluded(appRoutesUrl);
-        String routeGuids = getRouteGuidsAsString(resourcesWithIncluded.getResources());
-        String serviceRouteBindingsUrl = getServiceRouteBindingsUrl(routeGuids);
-        var serviceRouteBindingResources = getAllResources(serviceRouteBindingsUrl);
+        var batchedRouteGuids = getBatchedRouteGuids(resourcesWithIncluded.getResources());
+        var serviceRouteBindingResources = getAllServiceRouteBindingResources(batchedRouteGuids);
         var serviceRouteBindingsByRouteGuid = groupByRouteGuid(serviceRouteBindingResources);
 
         return toCloudRoutes(resourcesWithIncluded, serviceRouteBindingsByRouteGuid);
@@ -37,14 +39,36 @@ public class ApplicationRoutesGetter extends CustomControllerClient {
         return "/v3/routes?include=domain&app_guids=" + appGuid.toString();
     }
 
-    private String getServiceRouteBindingsUrl(String routeGuids) {
-        return "/v3/service_route_bindings?route_guids=" + routeGuids;
+    private String getServiceRouteBindingsUrl(Collection<String> routeGuids) {
+        return "/v3/service_route_bindings?route_guids=" + String.join(",", routeGuids);
     }
 
-    private String getRouteGuidsAsString(List<Map<String, Object>> routeResources) {
-        return routeResources.stream()
-                             .map(routeResource -> (String) routeResource.get("guid"))
-                             .collect(Collectors.joining(","));
+    private List<List<String>> getBatchedRouteGuids(List<Map<String, Object>> routeResources) {
+        List<List<String>> batches = new ArrayList<>();
+        int currentBatchLength = 0, currentBatchIndex = 0;
+        batches.add(new ArrayList<>());
+
+        for (Map<String, Object> routeResource : routeResources) {
+            String routeGuid = (String) routeResource.get("guid");
+            int elementLength = routeGuid.length();
+            if (elementLength + currentBatchLength >= MAX_CHAR_LENGTH_FOR_PARAMS_IN_REQUEST) {
+                batches.add(new ArrayList<>());
+                currentBatchIndex++;
+                currentBatchLength = 0;
+            }
+            batches.get(currentBatchIndex)
+                   .add(routeGuid);
+            currentBatchLength += elementLength;
+        }
+        return batches;
+    }
+
+    private List<Map<String, Object>> getAllServiceRouteBindingResources(List<List<String>> batchedRouteGuids) {
+        return batchedRouteGuids.stream()
+                                .map(this::getServiceRouteBindingsUrl)
+                                .map(this::getAllResources)
+                                .flatMap(List::stream)
+                                .collect(Collectors.toList());
     }
 
     private Map<String, List<Map<String, Object>>> groupByRouteGuid(List<Map<String, Object>> serviceRouteBindings) {
