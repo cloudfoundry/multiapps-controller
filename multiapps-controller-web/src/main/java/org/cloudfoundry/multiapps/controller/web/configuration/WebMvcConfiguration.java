4bded5b1a3e96d0f8876e74b68c793fb9c563071
IvanBorislavovDimitrov <starstrucks1997@gmail.com>
2022-09-14 14:17:14 +0300

    Use default serilization strategy where needed

--- multiapps-controller-web/src/main/java/org/cloudfoundry/multiapps/controller/web/configuration/WebMvcConfiguration.java
+++ multiapps-controller-web/src/main/java/org/cloudfoundry/multiapps/controller/web/configuration/WebMvcConfiguration.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import javax.inject.Inject;
 
+import org.cloudfoundry.multiapps.common.util.JsonSerializationStrategy;
 import org.cloudfoundry.multiapps.common.util.JsonUtil;
 import org.cloudfoundry.multiapps.controller.web.interceptors.CustomHandlerInterceptor;
 import org.cloudfoundry.multiapps.controller.web.util.XmlNamespaceIgnoringHttpMessageConverter;
@@ -59,7 +60,7 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
     }
 
     private MappingJackson2HttpMessageConverter createJsonHttpMessageConverter() {
-        MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter = new MappingJackson2HttpMessageConverter(JsonUtil.getObjectMapper());
+        MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter = new MappingJackson2HttpMessageConverter(JsonUtil.getObjectMapper(JsonSerializationStrategy.DEFAULT));
         mappingJackson2HttpMessageConverter.setSupportedMediaTypes(buildSupportedMediaTypes(mappingJackson2HttpMessageConverter));
         return mappingJackson2HttpMessageConverter;
     }
