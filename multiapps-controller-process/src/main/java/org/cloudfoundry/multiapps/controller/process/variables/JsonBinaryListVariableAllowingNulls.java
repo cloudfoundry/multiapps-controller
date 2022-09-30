d5a2f1166a815e1c75b8b9240307c42845dfa32a
IvanBorislavovDimitrov <starstrucks1997@gmail.com>
2022-09-30 13:47:49 +0300

    Revert "Revert "Replace null values comming from extension descriptors""
    This reverts commit a04fcd9c7d53fac20c3e17d096ce1be7b1fdd928.

---
+++ multiapps-controller-process/src/main/java/org/cloudfoundry/multiapps/controller/process/variables/JsonBinaryListVariableAllowingNulls.java
@@ -0,0 +1,40 @@
+package org.cloudfoundry.multiapps.controller.process.variables;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.cloudfoundry.multiapps.common.util.JsonSerializationStrategy;
+import org.cloudfoundry.multiapps.common.util.JsonUtil;
+import org.immutables.value.Value;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+
+@Value.Immutable
+public abstract class JsonBinaryListVariableAllowingNulls<T> implements ListVariable<T, List<T>> {
+
+    public abstract TypeReference<T> getType();
+
+    @Override
+    public Serializer<List<T>> getSerializer() {
+        return new Serializer<>() {
+
+            @Override
+            public Object serialize(List<T> values) {
+                return values.stream()
+                             .map(value -> JsonUtil.toJsonBinary(value, JsonSerializationStrategy.ALLOW_NULLS))
+                             .collect(Collectors.toList());
+            }
+
+            @SuppressWarnings("unchecked")
+            @Override
+            public List<T> deserialize(Object serializedValue) {
+                List<byte[]> serializedValues = (List<byte[]>) serializedValue;
+                return serializedValues.stream()
+                                       .map(value -> JsonUtil.fromJsonBinary(value, JsonSerializationStrategy.ALLOW_NULLS, getType()))
+                                       .collect(Collectors.toList());
+            }
+
+        };
+    }
+
+}
