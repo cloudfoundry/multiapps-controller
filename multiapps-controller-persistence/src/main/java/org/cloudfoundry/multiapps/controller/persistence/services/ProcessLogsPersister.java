e7076fc210231a9cd4c9bc66b47069755f909f7d
IvanBorislavovDimitrov <starstrucks1997@gmail.com>
2022-09-30 11:28:20 +0300

    Fix memory leak during file logging

--- multiapps-controller-persistence/src/main/java/org/cloudfoundry/multiapps/controller/persistence/services/ProcessLogsPersister.java
+++ multiapps-controller-persistence/src/main/java/org/cloudfoundry/multiapps/controller/persistence/services/ProcessLogsPersister.java
@@ -3,6 +3,7 @@ package org.cloudfoundry.multiapps.controller.persistence.services;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import org.cloudfoundry.multiapps.common.SLException;
 import org.springframework.scheduling.annotation.Async;
 
 @Named("processLogsPersister")
@@ -16,10 +17,19 @@ public class ProcessLogsPersister {
     @Async("asyncExecutor")
     public void persistLogs(String correlationId, String taskId) {
         for (ProcessLogger processLogger : processLoggerProvider.getExistingLoggers(correlationId, taskId)) {
+            persistLogsInDatabase(processLogger);
+        }
+    }
+
+    private void persistLogsInDatabase(ProcessLogger processLogger) {
+        try {
             processLogger.persistLogFile(processLogsPersistenceService);
+        } catch (Exception e) {
+            throw new SLException(e, e.getMessage());
+        } finally {
             processLoggerProvider.removeLoggersCache(processLogger);
-            processLogger.closeLoggerContext();
             processLogger.deleteLogFile();
+            processLogger.closeLoggerContext();
         }
     }
 }
