0eb19db58ef8a91d87357eb179af6922d9e8a522
IvanBorislavovDimitrov <starstrucks1997@gmail.com>
2022-10-04 15:18:52 +0300

    Revert "Revert "Use system default timezones""
    This reverts commit 51cab27580a25003443e60eaf3079aa06cc9ba9d.

--- com.sap.cloud.lm.sl.cf.web/src/main/java/com/sap/cloud/lm/sl/cf/web/util/TokenGenerator.java
+++ com.sap.cloud.lm.sl.cf.web/src/main/java/com/sap/cloud/lm/sl/cf/web/util/TokenGenerator.java
@@ -7,7 +7,7 @@ import java.nio.charset.StandardCharsets;
 import java.text.MessageFormat;
 import java.time.Instant;
 import java.time.LocalDateTime;
-import java.time.ZoneOffset;
+import java.time.ZoneId;
 
 import org.apache.commons.collections4.MapUtils;
 import org.cloudfoundry.client.lib.oauth2.OAuth2AccessTokenWithAdditionalInfo;
@@ -57,7 +57,7 @@ public abstract class TokenGenerator {
         long expirationInSeconds = ((Number) oAuth2AccessTokenWithAdditionalInfo.getAdditionalInfo()
                                                                                 .get(EXPIRES_AT_KEY)).longValue();
         return Instant.ofEpochSecond(expirationInSeconds)
-                      .atZone(ZoneOffset.UTC)
+                      .atZone(ZoneId.systemDefault())
                       .toLocalDateTime();
     }
 }
