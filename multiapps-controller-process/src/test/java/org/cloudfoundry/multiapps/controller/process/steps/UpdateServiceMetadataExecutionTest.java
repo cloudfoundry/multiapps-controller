77cedd7b9fe48af6c3483acc19240e14c6033864
IvanBorislavovDimitrov <starstrucks1997@gmail.com>
2022-11-14 09:48:28 +0200

    Make UpdateServiceMetadataStep fail-safe for optional services

--- multiapps-controller-process/src/test/java/org/cloudfoundry/multiapps/controller/process/steps/UpdateServiceMetadataExecutionTest.java
+++ multiapps-controller-process/src/test/java/org/cloudfoundry/multiapps/controller/process/steps/UpdateServiceMetadataExecutionTest.java
@@ -1,9 +1,11 @@
 package org.cloudfoundry.multiapps.controller.process.steps;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.mockito.ArgumentMatchers.any;
+
 import java.util.UUID;
 
 import org.cloudfoundry.client.v3.Metadata;
-import org.cloudfoundry.multiapps.controller.client.lib.domain.CloudServiceInstanceExtended;
 import org.cloudfoundry.multiapps.controller.client.lib.domain.ImmutableCloudServiceInstanceExtended;
 import org.cloudfoundry.multiapps.controller.process.util.StepLogger;
 import org.cloudfoundry.multiapps.controller.process.variables.Variables;
@@ -12,8 +14,10 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.springframework.http.HttpStatus;
 
 import com.sap.cloudfoundry.client.facade.CloudControllerClient;
+import com.sap.cloudfoundry.client.facade.CloudOperationException;
 
 class UpdateServiceMetadataExecutionTest {
 
@@ -40,27 +44,46 @@ class UpdateServiceMetadataExecutionTest {
     }
 
     @Test
-    void testExecute() {
-        Metadata v3Metadata = Metadata.builder()
-                                      .annotation("a", "b")
-                                      .annotation("c", "d")
-                                      .build();
-        CloudServiceInstanceExtended serviceInstanceToCreate = ImmutableCloudServiceInstanceExtended.builder()
-                                                                                                    .name(SERVICE_INSTANCE_NAME)
-                                                                                                    .plan(SERVICE_INSTANCE_PLAN)
-                                                                                                    .label(SERVICE_INSTANCE_LABEL)
-                                                                                                    .v3Metadata(v3Metadata)
-                                                                                                    .build();
+    void testUpdateMetadata() {
+        var v3Metadata = buildGenericMetadata();
+        var serviceInstanceToCreate = buildServiceInstance(v3Metadata);
         Mockito.when(processContext.getVariable(Variables.SERVICE_TO_PROCESS))
                .thenReturn(serviceInstanceToCreate);
         Mockito.when(controllerClient.getRequiredServiceInstanceGuid(SERVICE_INSTANCE_NAME))
                .thenReturn(SERVICE_INSTANCE_GUID);
-
-        AsyncExecution asyncExecution = new UpdateServiceMetadataExecution();
+        var asyncExecution = new UpdateServiceMetadataExecution();
         asyncExecution.execute(processContext);
-
         Mockito.verify(controllerClient)
                .updateServiceInstanceMetadata(SERVICE_INSTANCE_GUID, v3Metadata);
+    }
+
+    @Test
+    void testUpdateMetadataOnOptionalService() {
+        var v3Metadata = buildGenericMetadata();
+        var serviceInstanceToCreate = ImmutableCloudServiceInstanceExtended.copyOf(buildServiceInstance(v3Metadata))
+                                                                           .withIsOptional(true);
+        Mockito.when(processContext.getVariable(Variables.SERVICE_TO_PROCESS))
+               .thenReturn(serviceInstanceToCreate);
+        Mockito.when(controllerClient.getRequiredServiceInstanceGuid(any()))
+               .thenThrow(new CloudOperationException(HttpStatus.NOT_FOUND));
+        var asyncExecution = new UpdateServiceMetadataExecution();
+        assertDoesNotThrow(() -> asyncExecution.execute(processContext));
+    }
+
+    private static Metadata buildGenericMetadata() {
+        return Metadata.builder()
+                       .annotation("a", "b")
+                       .annotation("c", "d")
+                       .build();
+    }
+
+    private static ImmutableCloudServiceInstanceExtended buildServiceInstance(Metadata v3Metadata) {
+        return ImmutableCloudServiceInstanceExtended.builder()
+                                                    .name(SERVICE_INSTANCE_NAME)
+                                                    .plan(SERVICE_INSTANCE_PLAN)
+                                                    .label(SERVICE_INSTANCE_LABEL)
+                                                    .v3Metadata(v3Metadata)
+                                                    .build();
     }
 
 }
