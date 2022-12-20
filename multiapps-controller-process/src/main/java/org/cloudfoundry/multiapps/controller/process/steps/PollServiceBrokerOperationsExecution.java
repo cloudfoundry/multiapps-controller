242d7631d49177c0ca268d66bbac4a0699eae04f
IvanBorislavovDimitrov <starstrucks1997@gmail.com>
2022-12-20 15:19:27 +0200

    Reuse polling adaptor for service brokers

--- multiapps-controller-process/src/main/java/org/cloudfoundry/multiapps/controller/process/steps/PollServiceBrokerOperationsExecution.java
+++ multiapps-controller-process/src/main/java/org/cloudfoundry/multiapps/controller/process/steps/PollServiceBrokerOperationsExecution.java
@@ -1,47 +1,50 @@
 package org.cloudfoundry.multiapps.controller.process.steps;
 
 import java.text.MessageFormat;
+import java.util.function.Consumer;
 
-import org.cloudfoundry.client.v3.jobs.JobState;
 import org.cloudfoundry.multiapps.controller.process.Messages;
 import org.cloudfoundry.multiapps.controller.process.variables.Variables;
 
-import com.sap.cloudfoundry.client.facade.CloudControllerClient;
 import com.sap.cloudfoundry.client.facade.domain.CloudAsyncJob;
 import com.sap.cloudfoundry.client.facade.domain.CloudServiceBroker;
 
-public class PollServiceBrokerOperationsExecution implements AsyncExecution {
+public class PollServiceBrokerOperationsExecution extends PollOperationBaseExecution {
 
     @Override
     public AsyncExecutionState execute(ProcessContext context) {
         CloudServiceBroker broker = context.getVariable(Variables.CREATED_OR_UPDATED_SERVICE_BROKER);
-        String jobId = context.getVariable(Variables.SERVICE_BROKER_ASYNC_JOB_ID);
-
         context.getStepLogger()
                .debug(Messages.POLLING_ASYNC_OPERATION_SERVICE_BROKER, broker.getName());
-
-        return checkAsyncJobStatus(context, broker.getName(), jobId);
+        return super.execute(context);
     }
 
-    protected AsyncExecutionState checkAsyncJobStatus(ProcessContext context, String serviceBrokerName, String jobId) {
-        CloudControllerClient client = context.getControllerClient();
-        CloudAsyncJob job = client.getAsyncJob(jobId);
+    @Override
+    protected String getAsyncJobId(ProcessContext context) {
+        return context.getVariable(Variables.SERVICE_BROKER_ASYNC_JOB_ID);
+    }
 
-        if (job.getState() == JobState.COMPLETE) {
-            context.getStepLogger()
-                   .info(Messages.ASYNC_OPERATION_FOR_SERVICE_BROKER_FINISHED, serviceBrokerName);
-            return AsyncExecutionState.FINISHED;
-        }
+    @Override
+    protected Consumer<CloudAsyncJob> getInProgressHandler(ProcessContext context) {
+        CloudServiceBroker broker = context.getVariable(Variables.CREATED_OR_UPDATED_SERVICE_BROKER);
+        return serviceBrokerJob -> context.getStepLogger()
+                                          .debug(Messages.ASYNC_OPERATION_SERVICE_BROKER_IN_STATE_WITH_WARNINGS, broker.getName(),
+                                                 serviceBrokerJob.getState(), serviceBrokerJob.getWarnings());
+    }
 
-        if (job.getState() == JobState.FAILED) {
-            context.getStepLogger()
-                   .error(Messages.ASYNC_OPERATION_FOR_SERVICE_BROKER_FAILED_WITH, serviceBrokerName, job.getErrors());
-            return AsyncExecutionState.ERROR;
-        }
+    @Override
+    protected Consumer<CloudAsyncJob> getOnCompleteHandler(ProcessContext context) {
+        CloudServiceBroker broker = context.getVariable(Variables.CREATED_OR_UPDATED_SERVICE_BROKER);
+        return serviceBrokerJob -> context.getStepLogger()
+                                          .info(Messages.ASYNC_OPERATION_FOR_SERVICE_BROKER_FINISHED, broker.getName());
+    }
 
-        context.getStepLogger()
-               .debug(Messages.ASYNC_OPERATION_SERVICE_BROKER_IN_STATE_WITH_WARNINGS, serviceBrokerName, job.getState(), job.getWarnings());
-        return AsyncExecutionState.RUNNING;
+    @Override
+    protected Consumer<CloudAsyncJob> getOnErrorHandler(ProcessContext context) {
+        CloudServiceBroker broker = context.getVariable(Variables.CREATED_OR_UPDATED_SERVICE_BROKER);
+        return serviceBrokerJob -> context.getStepLogger()
+                                          .error(Messages.ASYNC_OPERATION_FOR_SERVICE_BROKER_FAILED_WITH, broker.getName(),
+                                                 serviceBrokerJob.getErrors());
     }
 
     @Override
--- multiapps-controller-process/src/main/java/org/cloudfoundry/multiapps/controller/process/steps/PollServiceBrokersOperationsExecution.java
+++ multiapps-controller-process/src/main/java/org/cloudfoundry/multiapps/controller/process/steps/PollServiceBrokersOperationsExecution.java
@@ -2,22 +2,29 @@ package org.cloudfoundry.multiapps.controller.process.steps;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Consumer;
 
 import org.cloudfoundry.multiapps.controller.process.Messages;
+import org.cloudfoundry.multiapps.controller.process.util.AsyncJobToAsyncExecutionStateAdapter;
+import org.cloudfoundry.multiapps.controller.process.util.ImmutableAsyncJobToAsyncExecutionStateAdapter;
 import org.cloudfoundry.multiapps.controller.process.variables.Variables;
 
-public class PollServiceBrokersOperationsExecution extends PollServiceBrokerOperationsExecution {
+import com.sap.cloudfoundry.client.facade.CloudControllerClient;
+import com.sap.cloudfoundry.client.facade.domain.CloudAsyncJob;
+
+public class PollServiceBrokersOperationsExecution implements AsyncExecution {
 
     @Override
     public AsyncExecutionState execute(ProcessContext context) {
         Map<String, String> serviceBrokerNamesJobIds = context.getVariable(Variables.SERVICE_BROKER_NAMES_JOB_IDS);
 
         Map<String, String> serviceBrokerStillInProgress = new HashMap<>();
-        for (var entry : serviceBrokerNamesJobIds.entrySet()) {
-            String serviceBrokerName = entry.getKey();
-            String jobId = entry.getValue();
-
-            AsyncExecutionState asyncJobStatus = checkAsyncJobStatus(context, serviceBrokerName, jobId);
+        for (var brokerNameWithJobId : serviceBrokerNamesJobIds.entrySet()) {
+            String serviceBrokerName = brokerNameWithJobId.getKey();
+            String jobId = brokerNameWithJobId.getValue();
+            CloudControllerClient controllerClient = context.getControllerClient();
+            AsyncExecutionState asyncJobStatus = createAsyncJobAdapter(context,
+                                                                       serviceBrokerName).evaluateState(controllerClient.getAsyncJob(jobId));
             if (asyncJobStatus == AsyncExecutionState.ERROR) {
                 return AsyncExecutionState.ERROR;
             }
@@ -32,6 +39,39 @@ public class PollServiceBrokersOperationsExecution extends PollServiceBrokerOper
 
         context.setVariable(Variables.SERVICE_BROKER_NAMES_JOB_IDS, serviceBrokerStillInProgress);
         return AsyncExecutionState.RUNNING;
+    }
+
+    private AsyncJobToAsyncExecutionStateAdapter createAsyncJobAdapter(ProcessContext context, String serviceBrokerName) {
+        return ImmutableAsyncJobToAsyncExecutionStateAdapter.builder()
+                                                            .inProgressHandler(getInProgressHandler(context, serviceBrokerName))
+                                                            .onCompleteHandler(getOnCompleteHandler(context, serviceBrokerName))
+                                                            .onErrorHandler(getOnErrorHandler(context, serviceBrokerName))
+                                                            .onErrorHandlerForOptionalResource(getOnErrorHandlerForOptionalResource(context,
+                                                                                                                                    serviceBrokerName))
+                                                            .build();
+    }
+
+    private Consumer<CloudAsyncJob> getInProgressHandler(ProcessContext context, String serviceBrokerName) {
+        return serviceBrokerJob -> context.getStepLogger()
+                                          .debug(Messages.ASYNC_OPERATION_SERVICE_BROKER_IN_STATE_WITH_WARNINGS, serviceBrokerName,
+                                                 serviceBrokerJob.getState(), serviceBrokerJob.getWarnings());
+
+    }
+
+    private Consumer<CloudAsyncJob> getOnCompleteHandler(ProcessContext context, String serviceBrokerName) {
+        return serviceBrokerJob -> context.getStepLogger()
+                                          .info(Messages.ASYNC_OPERATION_FOR_SERVICE_BROKER_FINISHED, serviceBrokerName);
+
+    }
+
+    private Consumer<CloudAsyncJob> getOnErrorHandler(ProcessContext context, String serviceBrokerName) {
+        return serviceBrokerJob -> context.getStepLogger()
+                                          .error(Messages.ASYNC_OPERATION_FOR_SERVICE_BROKER_FAILED_WITH, serviceBrokerName,
+                                                 serviceBrokerJob.getErrors());
+    }
+
+    private Consumer<CloudAsyncJob> getOnErrorHandlerForOptionalResource(ProcessContext context, String serviceBrokerName) {
+        return getOnErrorHandler(context, serviceBrokerName);
     }
 
     @Override
