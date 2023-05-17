d753f1167035b48523ae16401d82c3b1d607ada7
Tina Ilieva <tina.ilieva@sap.com>
2023-05-17 14:19:12 +0300

    Remove objectstore implementation
    Objectstore is not used in on-premise delivery
    
    Jira: LMCROSSITXSADEPLOY-2736

--- com.sap.cloud.lm.sl.cf.web/src/main/java/com/sap/cloud/lm/sl/cf/web/configuration/FileStorageConfiguration.java
+++ com.sap.cloud.lm.sl.cf.web/src/main/java/com/sap/cloud/lm/sl/cf/web/configuration/FileStorageConfiguration.java
@@ -2,25 +2,17 @@ package com.sap.cloud.lm.sl.cf.web.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 import com.sap.cloud.lm.sl.cf.web.configuration.bean.factory.FileSystemFileStorageFactoryBean;
-import com.sap.cloud.lm.sl.cf.web.configuration.bean.factory.ObjectStoreFileStorageFactoryBean;
 
 @Configuration
 public class FileStorageConfiguration {
 
     private static final String FS_STORAGE_SERVICE_NAME = "deploy-service-fss";
-    private static final String OBJECT_STORE_SERVICE_NAME = "deploy-service-os";
 
     @Bean
     public FileSystemFileStorageFactoryBean fileSystemFileStorage() {
         return new FileSystemFileStorageFactoryBean(FS_STORAGE_SERVICE_NAME);
     }
 
-    @Bean
-    @Profile("cf")
-    public ObjectStoreFileStorageFactoryBean objectStoreFileStorage() {
-        return new ObjectStoreFileStorageFactoryBean(OBJECT_STORE_SERVICE_NAME);
-    }
 }
