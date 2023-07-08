9063e8886ea86b50f28b3e0d75c4604b67ef4085
Velizar Kalapov <velizar.kalapov@sap.com>
2023-07-08 09:50:46 +0300

    Fix service detachment/deletion

--- multiapps-controller-process/src/main/java/org/cloudfoundry/multiapps/controller/process/util/ResourcesGetter.java
+++ multiapps-controller-process/src/main/java/org/cloudfoundry/multiapps/controller/process/util/ResourcesGetter.java
@@ -3,8 +3,10 @@ package org.cloudfoundry.multiapps.controller.process.util;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.cloudfoundry.multiapps.controller.core.util.CloudModelBuilderUtil;
 import org.cloudfoundry.multiapps.controller.process.variables.VariableHandling;
 import org.cloudfoundry.multiapps.controller.process.variables.Variables;
+import org.cloudfoundry.multiapps.mta.model.DeploymentDescriptor;
 import org.cloudfoundry.multiapps.mta.model.Resource;
 import org.flowable.engine.delegate.DelegateExecution;
 
@@ -17,9 +19,23 @@ public class ResourcesGetter {
     }
 
     public List<Resource> getResources() {
-        return VariableHandling.get(execution, Variables.BATCHES_TO_PROCESS)
-                               .stream()
-                               .flatMap(List::stream)
-                               .collect(Collectors.toList());
+        DeploymentDescriptor deploymentDescriptor = VariableHandling.get(execution, Variables.COMPLETE_DEPLOYMENT_DESCRIPTOR);
+        List<String> resourcesSpecifiedForDeployment = VariableHandling.get(execution, Variables.RESOURCES_FOR_DEPLOYMENT);
+
+        return deploymentDescriptor.getResources()
+                                   .stream()
+                                   .filter(this::isActive)
+                                   .filter(r -> isResourceSpecifiedForDeployment(r, resourcesSpecifiedForDeployment))
+                                   .filter(CloudModelBuilderUtil::isService)
+                                   .collect(Collectors.toList());
     }
+
+    private boolean isActive(Resource resource) {
+        return resource.getMajorSchemaVersion() < 3 || resource.isActive();
+    }
+
+    private boolean isResourceSpecifiedForDeployment(Resource resource, List<String> resourcesSpecifiedForDeployment) {
+        return resourcesSpecifiedForDeployment == null || resourcesSpecifiedForDeployment.contains(resource.getName());
+    }
+
 }
--- multiapps-controller-process/src/main/java/org/cloudfoundry/multiapps/controller/process/variables/Variables.java
+++ multiapps-controller-process/src/main/java/org/cloudfoundry/multiapps/controller/process/variables/Variables.java
@@ -33,7 +33,6 @@ import org.cloudfoundry.multiapps.mta.model.DeploymentDescriptor;
 import org.cloudfoundry.multiapps.mta.model.ExtensionDescriptor;
 import org.cloudfoundry.multiapps.mta.model.Hook;
 import org.cloudfoundry.multiapps.mta.model.Module;
-import org.cloudfoundry.multiapps.mta.model.Resource;
 import org.cloudfoundry.multiapps.mta.model.VersionRule;
 
 import com.fasterxml.jackson.core.type.TypeReference;
@@ -381,12 +380,14 @@ public interface Variables {
                                                                                                      .type(new TypeReference<>() {
                                                                                                      })
                                                                                                      .build();
+
     Variable<List<DeployedMtaServiceKey>> SERVICE_KEYS_TO_DELETE = ImmutableJsonStringListVariable.<DeployedMtaServiceKey> builder()
                                                                                                   .name("serviceKeysToDelete")
                                                                                                   .type(new TypeReference<>() {
                                                                                                   })
                                                                                                   .defaultValue(Collections.emptyList())
                                                                                                   .build();
+
     Variable<Map<String, List<CloudServiceKey>>> SERVICE_KEYS_FOR_CONTENT_DEPLOY = ImmutableJsonBinaryVariable.<Map<String, List<CloudServiceKey>>> builder()
                                                                                                               .name("serviceKeysForContentDeploy")
                                                                                                               .type(new TypeReference<>() {
@@ -591,18 +592,18 @@ public interface Variables {
                                                                                         .name("serviceBrokerNamesJobsIds")
                                                                                         .build();
 
-    Variable<List<List<Resource>>> BATCHES_TO_PROCESS = ImmutableJsonStringListVariable.<List<Resource>> builder()
-                                                                                       .name("batchesToProcess")
-                                                                                       .type(new TypeReference<>() {
-                                                                                       })
-                                                                                       .defaultValue(Collections.emptyList())
-                                                                                       .build();
-    Variable<List<Resource>> BATCH_TO_PROCESS = ImmutableJsonStringVariable.<List<Resource>> builder()
-                                                                           .name("batchToProcess")
-                                                                           .type(new TypeReference<>() {
-                                                                           })
-                                                                           .defaultValue(Collections.emptyList())
-                                                                           .build();
+    Variable<List<List<CloudServiceInstanceExtended>>> BATCHES_TO_PROCESS = ImmutableJsonStringListVariable.<List<CloudServiceInstanceExtended>> builder()
+                                                                                                           .name("batchesToProcess")
+                                                                                                           .type(new TypeReference<>() {
+                                                                                                           })
+                                                                                                           .defaultValue(Collections.emptyList())
+                                                                                                           .build();
+    Variable<List<CloudServiceInstanceExtended>> BATCH_TO_PROCESS = ImmutableJsonStringVariable.<List<CloudServiceInstanceExtended>> builder()
+                                                                                               .name("batchToProcess")
+                                                                                               .type(new TypeReference<>() {
+                                                                                               })
+                                                                                               .defaultValue(Collections.emptyList())
+                                                                                               .build();
     Variable<Boolean> SHOULD_RECREATE_SERVICE_BINDING = ImmutableSimpleVariable.<Boolean> builder()
                                                                                .name("shouldRecreateServiceBinding")
                                                                                .defaultValue(false)
@@ -658,11 +659,11 @@ public interface Variables {
                                                                                                   .defaultValue(Collections.emptyList())
                                                                                                   .build();
     Variable<List<CloudServiceKey>> CLOUD_SERVICE_KEYS_TO_UPDATE_METADATA = ImmutableJsonStringListVariable.<CloudServiceKey> builder()
-                                                                                                  .name("cloudServiceKeysToUpdate")
-                                                                                                  .type(new TypeReference<>() {
-                                                                                                  })
-                                                                                                  .defaultValue(Collections.emptyList())
-                                                                                                  .build();
+                                                                                                           .name("cloudServiceKeysToUpdate")
+                                                                                                           .type(new TypeReference<>() {
+                                                                                                           })
+                                                                                                           .defaultValue(Collections.emptyList())
+                                                                                                           .build();
     Variable<List<ServiceDeletionActions>> SERVICE_DELETION_ACTIONS = ImmutableEnumListVariable.<ServiceDeletionActions> builder()
                                                                                                .name("serviceDeletionActions")
                                                                                                .type(ServiceDeletionActions.class)
