225e5e505ec5ed70fcaa353550cef2f54e38aaa8
theghost5800 <kristian0001@gmail.com>
2023-08-15 16:44:48 +0300

    Log properly dynatrace event and duration objects

--- multiapps-controller-process/src/main/java/org/cloudfoundry/multiapps/controller/process/dynatrace/DynatracePublisher.java
+++ multiapps-controller-process/src/main/java/org/cloudfoundry/multiapps/controller/process/dynatrace/DynatracePublisher.java
@@ -1,5 +1,7 @@
 package org.cloudfoundry.multiapps.controller.process.dynatrace;
 
+import java.text.MessageFormat;
+
 import javax.inject.Named;
 
 import org.cloudfoundry.multiapps.controller.core.util.LoggingUtil;
@@ -11,11 +13,13 @@ public class DynatracePublisher {
 
     public void publishProcessEvent(DynatraceProcessEvent dynatraceProcessEvent, Logger logger) {
         LoggingUtil.logWithCorrelationId(dynatraceProcessEvent.getProcessId(),
-                                         () -> logger.info(Messages.REGISTERING_PROCESS_EVENT_IN_DYNATRACE, dynatraceProcessEvent));
+                                         () -> logger.info(MessageFormat.format(Messages.REGISTERING_PROCESS_EVENT_IN_DYNATRACE,
+                                                                                dynatraceProcessEvent)));
     }
 
     public void publishProcessDuration(DynatraceProcessDuration dynatraceProcessDuration, Logger logger) {
         LoggingUtil.logWithCorrelationId(dynatraceProcessDuration.getProcessId(),
-                                         () -> logger.info(Messages.REGISTERING_PROCESS_DURATION_IN_DYNATRACE, dynatraceProcessDuration));
+                                         () -> logger.info(MessageFormat.format(Messages.REGISTERING_PROCESS_DURATION_IN_DYNATRACE,
+                                                                                dynatraceProcessDuration)));
     }
 }
