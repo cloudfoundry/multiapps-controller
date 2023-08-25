f80ab42f1720794eef480e3226e0468c3c1dd45f
IvanBorislavovDimitrov <starstrucks1997@gmail.com>
2023-08-25 11:10:07 +0300

    Fix invalid argument during logging

--- multiapps-controller-process/src/main/java/org/cloudfoundry/multiapps/controller/process/flowable/commands/ClearJobLockOwnersCmd.java
+++ multiapps-controller-process/src/main/java/org/cloudfoundry/multiapps/controller/process/flowable/commands/ClearJobLockOwnersCmd.java
@@ -33,8 +33,7 @@ public class ClearJobLockOwnersCmd implements Command<Void> {
             if (runningLockedJob != null) {
                 LOGGER.info(MessageFormat.format(Messages.UNLOCKING_JOB_WITH_ID_0_LOCK_EXPIRATION_TIME_1_CREATION_TIME_2_AND_ELEMENT_NAME_3,
                                                  runningLockedJob.getId(), runningLockedJob.getLockExpirationTime(),
-                                                 runningLockedJob.getCreateTime()),
-                            runningLockedJob.getElementName());
+                                                 runningLockedJob.getCreateTime(), runningLockedJob.getElementName()));
                 runningLockedJob.setLockOwner(null);
                 runningLockedJob.setLockExpirationTime(null);
             }
