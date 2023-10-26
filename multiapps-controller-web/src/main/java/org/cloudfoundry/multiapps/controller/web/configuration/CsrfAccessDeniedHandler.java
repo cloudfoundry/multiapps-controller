9ffb71be5e10dbe08a28a29a76bc22e169dae357
IvanBorislavovDimitrov <starstrucks1997@gmail.com>
2023-10-26 18:28:41 +0300

    Use CommonsMultipartResolver for multipart request processing

--- multiapps-controller-web/src/main/java/org/cloudfoundry/multiapps/controller/web/configuration/CsrfAccessDeniedHandler.java
+++ multiapps-controller-web/src/main/java/org/cloudfoundry/multiapps/controller/web/configuration/CsrfAccessDeniedHandler.java
@@ -1,10 +1,15 @@
 package org.cloudfoundry.multiapps.controller.web.configuration;
 
+import java.text.MessageFormat;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.http.HttpStatus;
 import org.cloudfoundry.multiapps.controller.web.Constants;
+import org.cloudfoundry.multiapps.controller.web.Messages;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.csrf.InvalidCsrfTokenException;
@@ -12,13 +17,18 @@ import org.springframework.security.web.csrf.MissingCsrfTokenException;
 
 public class CsrfAccessDeniedHandler implements AccessDeniedHandler {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(CsrfAccessDeniedHandler.class);
+
     private static final String CSRF_TOKEN_REQUIRED_HEADER_VALUE = "Required";
 
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) {
+        LOGGER.error(MessageFormat.format(Messages.REQUEST_0_1_FAILED_WITH_2, request.getMethod(), request.getRequestURI(),
+                                          accessDeniedException.getMessage()),
+                     accessDeniedException);
         if (accessDeniedException instanceof InvalidCsrfTokenException || accessDeniedException instanceof MissingCsrfTokenException) {
             response.setHeader(Constants.CSRF_TOKEN, CSRF_TOKEN_REQUIRED_HEADER_VALUE);
-            response.setStatus(HttpStatus.SC_FORBIDDEN);
         }
+        response.setStatus(HttpStatus.SC_FORBIDDEN);
     }
 }
--- multiapps-controller-web/src/main/java/org/cloudfoundry/multiapps/controller/web/security/RequestSizeFilter.java
+++ multiapps-controller-web/src/main/java/org/cloudfoundry/multiapps/controller/web/security/RequestSizeFilter.java
@@ -17,13 +17,18 @@ import org.springframework.web.filter.GenericFilterBean;
 @Named("requestSizeFilter")
 public class RequestSizeFilter extends GenericFilterBean {
 
-    private static final long MAX_REQUEST_SIZE_BYTES = 1024 * 1024L;
+    private static final long MAX_REQUEST_SIZE_BYTES = 1024 * 1024L; // 1MB
+    private static final long MAX_FILE_UPLOAD_REQUEST_SIZE_BYTES = 4096 * 1024 * 1024L; // 4GB
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         long requestSize = request.getContentLengthLong();
         String path = ServletUtil.decodeUri((HttpServletRequest) request);
         if (requestSize > MAX_REQUEST_SIZE_BYTES && !path.endsWith("/files")) {
+            ((HttpServletResponse) response).sendError(HttpStatus.PAYLOAD_TOO_LARGE.value());
+            return;
+        }
+        if (requestSize > MAX_FILE_UPLOAD_REQUEST_SIZE_BYTES && path.endsWith("files")) {
             ((HttpServletResponse) response).sendError(HttpStatus.PAYLOAD_TOO_LARGE.value());
             return;
         }
