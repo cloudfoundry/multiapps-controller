da52117e93c43052917480939ae7f2a2a2ce9f69
Yavor16 <y_uz@abv.bg>
2024-04-19 13:19:07 +0300

    Add wait for there is on going operation with a service while we try to update the service LMCROSSITXSADEPLOY-2783

---
+++ multiapps-controller-process/src/main/java/org/cloudfoundry/multiapps/controller/process/steps/CheckForServiceBindingOrKeyOperationStep.java
@@ -0,0 +1,44 @@
+package org.cloudfoundry.multiapps.controller.process.steps;
+
+import java.time.Duration;
+import java.util.List;
+
+import javax.inject.Named;
+
+import org.cloudfoundry.multiapps.controller.process.Messages;
+import org.cloudfoundry.multiapps.controller.process.variables.Variables;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Scope;
+
+@Named("checkForServiceBindingOrKeyOperationStep")
+@Scope(BeanDefinition.SCOPE_PROTOTYPE)
+public class CheckForServiceBindingOrKeyOperationStep extends TimeoutAsyncFlowableStep {
+
+    @Override
+    protected StepPhase executeAsyncStep(ProcessContext context) throws Exception {
+        boolean isServiceBindingKeyOperationInProgress = context.getVariable(Variables.IS_SERVICE_BINDING_KEY_OPERATION_IN_PROGRESS);
+
+        if (!isServiceBindingKeyOperationInProgress) {
+            context.setVariable(Variables.IS_SERVICE_BINDING_KEY_OPERATION_IN_PROGRESS, Boolean.FALSE);
+            return StepPhase.DONE;
+        }
+
+        getStepLogger().info(Messages.WAITING_FOR_SERVICE_OPERATION_TO_FINISH);
+        return StepPhase.POLL;
+    }
+
+    @Override
+    protected List<AsyncExecution> getAsyncStepExecutions(ProcessContext context) {
+        return List.of(new PollServiceBindingOrKeyOperationExecution());
+    }
+
+    @Override
+    protected String getStepErrorMessage(ProcessContext context) {
+        return Messages.ERROR_WAITING_FOR_OPERATION_TO_FINISH;
+    }
+
+    @Override
+    public Duration getTimeout(ProcessContext context) {
+        return context.getVariable(Variables.WAIT_BIND_SERVICE_TIMEOUT);
+    }
+}
