a465e8e62f5573b6f62bb04372ea9cd315c21858
MonikaNoeva <noevam2000@gmail.com>
2024-05-10 14:18:41 +0300

    Execution listener task

--- multiapps-controller-process/src/test/java/org/cloudfoundry/multiapps/controller/process/listeners/ManageAppServiceBindingEndListenerTest.java
+++ multiapps-controller-process/src/test/java/org/cloudfoundry/multiapps/controller/process/listeners/ManageAppServiceBindingEndListenerTest.java
@@ -10,8 +10,14 @@ import java.util.stream.Stream;
 
 import org.cloudfoundry.multiapps.common.util.JsonUtil;
 import org.cloudfoundry.multiapps.controller.api.model.ProcessType;
+import org.cloudfoundry.multiapps.controller.core.util.ApplicationConfiguration;
+import org.cloudfoundry.multiapps.controller.persistence.services.HistoricOperationEventService;
+import org.cloudfoundry.multiapps.controller.persistence.services.ProcessLoggerProvider;
+import org.cloudfoundry.multiapps.controller.persistence.services.ProcessLogsPersister;
+import org.cloudfoundry.multiapps.controller.persistence.services.ProgressMessageService;
 import org.cloudfoundry.multiapps.controller.process.flowable.FlowableFacade;
 import org.cloudfoundry.multiapps.controller.process.util.ProcessTypeParser;
+import org.cloudfoundry.multiapps.controller.process.util.StepLogger;
 import org.cloudfoundry.multiapps.controller.process.variables.Variables;
 import org.flowable.engine.delegate.DelegateExecution;
 import org.junit.jupiter.api.BeforeEach;
@@ -19,7 +25,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
@@ -32,26 +37,38 @@ class ManageAppServiceBindingEndListenerTest {
     private static final String SERVICE_NAME = "test_service";
 
     @Mock
-    private FlowableFacade flowableFacade;
-    @Mock
     private DelegateExecution execution;
     @Mock
     private ProcessTypeParser processTypeParser;
-    @InjectMocks
+    @Mock
+    private ProgressMessageService progressMessageService;
+    @Mock
+    private StepLogger.Factory stepLoggerFactory;
+    @Mock
+    private ProcessLoggerProvider processLoggerProvider;
+    @Mock
+    private ProcessLogsPersister processLogsPersister;
+    @Mock
+    private HistoricOperationEventService historicOperationEventService;
+    @Mock
+    private FlowableFacade flowableFacade;
+    @Mock
+    private ApplicationConfiguration configuration;
     private ManageAppServiceBindingEndListener manageAppServiceBindingEndListener;
 
     @BeforeEach
     void setUp() throws Exception {
         MockitoAnnotations.openMocks(this)
-                          .close();
+                .close();
+        manageAppServiceBindingEndListener = new ManageAppServiceBindingEndListener(progressMessageService, stepLoggerFactory, processLoggerProvider, processLogsPersister, historicOperationEventService, flowableFacade, configuration, processTypeParser);
     }
 
     // @formatter:off
     static Stream<Arguments> testBindUnbindServiceEndListener() {
         return Stream.of(Arguments.of(false, false, false),
-                         Arguments.of(false, true, true),
-                         Arguments.of(true, false, true),
-                         Arguments.of(true, true, true));
+                Arguments.of(false, true, true),
+                Arguments.of(true, false, true),
+                Arguments.of(true, true, true));
     }
     // @formatter:on
 
@@ -63,15 +80,15 @@ class ManageAppServiceBindingEndListenerTest {
         manageAppServiceBindingEndListener.notifyInternal(execution);
 
         verify(flowableFacade).setVariableInParentProcess(execution,
-                                                          ManageAppServiceBindingEndListener.buildExportedVariableName(APPLICATION_NAME,
-                                                                                                                       SERVICE_NAME),
-                                                          expectedBooleanValue);
+                ManageAppServiceBindingEndListener.buildExportedVariableName(APPLICATION_NAME,
+                        SERVICE_NAME),
+                expectedBooleanValue);
     }
 
     private void prepareExecution(boolean shouldUnbind, boolean shouldBind) {
         CloudApplication application = ImmutableCloudApplication.builder()
-                                                                .name(APPLICATION_NAME)
-                                                                .build();
+                .name(APPLICATION_NAME)
+                .build();
         when(execution.getVariable(Variables.APP_TO_PROCESS.getName())).thenReturn(JsonUtil.toJson(application));
         when(execution.getVariable(Variables.SERVICE_TO_UNBIND_BIND.getName())).thenReturn(SERVICE_NAME);
         when(execution.getVariable(Variables.SHOULD_UNBIND_SERVICE_FROM_APP.getName())).thenReturn(shouldUnbind);
