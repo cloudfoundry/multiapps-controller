54dfb29e40465b7bb3e586c82a437d1acee1449c
Arsalan Khan <muhammad.arsalan.khan@sap.com>
2024-06-19 16:14:36 +0200

    skip ssl to connect with cf api in webclientFactory

--- multiapps-controller-core/src/main/java/org/cloudfoundry/multiapps/controller/core/cf/clients/WebClientFactory.java
+++ multiapps-controller-core/src/main/java/org/cloudfoundry/multiapps/controller/core/cf/clients/WebClientFactory.java
@@ -1,14 +1,19 @@
 package org.cloudfoundry.multiapps.controller.core.cf.clients;
 
-import javax.inject.Inject;
-import javax.inject.Named;
-
-import org.cloudfoundry.multiapps.controller.core.cf.OAuthClientFactory;
-import org.cloudfoundry.multiapps.controller.core.util.ApplicationConfiguration;
-import org.springframework.web.reactive.function.client.WebClient;
-
 import com.sap.cloudfoundry.client.facade.CloudCredentials;
 import com.sap.cloudfoundry.client.facade.util.RestUtil;
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslContextBuilder;
+import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
+import org.cloudfoundry.multiapps.controller.core.cf.OAuthClientFactory;
+import org.cloudfoundry.multiapps.controller.core.util.ApplicationConfiguration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.net.ssl.SSLException;
 
 @Named
 public class WebClientFactory {
@@ -20,19 +25,38 @@ public class WebClientFactory {
 
     public WebClient getWebClient(CloudCredentials credentials) {
         WebClient.Builder webClientBuilder = new RestUtil().createWebClient(false)
-                                                           .mutate()
-                                                           .baseUrl(configuration.getControllerUrl()
-                                                                                 .toString());
+                .mutate()
+                .baseUrl(configuration.getControllerUrl()
+                        .toString());
         webClientBuilder.defaultHeaders(httpHeaders -> httpHeaders.setBearerAuth(computeAuthorizationToken(credentials)));
-        return webClientBuilder.build();
+
+        //do not skip ssl
+        if (!configuration.shouldSkipSslValidation()) {
+            return webClientBuilder.build();
+        }
+
+        // skip ssl
+        final SslContext sslContext;
+        try {
+            sslContext = SslContextBuilder
+                    .forClient()
+                    .trustManager(InsecureTrustManagerFactory.INSTANCE)
+                    .build();
+        } catch (SSLException e) {
+            throw new IllegalStateException("Failed to create insecure SSL context", e);
+        }
+        HttpClient httpClient = HttpClient.create().secure(t -> t.sslContext(sslContext));
+        return WebClient.builder().clientConnector(new ReactorClientHttpConnector(httpClient))
+                .build();
+
     }
 
     private String computeAuthorizationToken(CloudCredentials credentials) {
         var oAuthClient = oAuthClientFactory.createOAuthClient();
         oAuthClient.init(credentials);
         return oAuthClient.getToken()
-                          .getOAuth2AccessToken()
-                          .getTokenValue();
+                .getOAuth2AccessToken()
+                .getTokenValue();
     }
 
 }
