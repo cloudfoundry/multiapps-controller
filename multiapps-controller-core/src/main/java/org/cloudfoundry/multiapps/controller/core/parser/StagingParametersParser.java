b0b7d99836056a9bf9fd03313dd0c384e8a22eaa
Yavor16 <yavoruzbg@gmail.com>
2025-01-07 11:26:35 +0200

    Revert "Initial impl"
    This reverts commit 647dee98900c5172082d3c32851adc71383d8f80.

--- multiapps-controller-core/src/main/java/org/cloudfoundry/multiapps/controller/core/parser/StagingParametersParser.java
+++ multiapps-controller-core/src/main/java/org/cloudfoundry/multiapps/controller/core/parser/StagingParametersParser.java
@@ -30,17 +30,6 @@ public class StagingParametersParser implements ParametersParser<Staging> {
         String healthCheckHttpEndpoint = (String) PropertiesUtil.getPropertyValue(parametersList,
                                                                                   SupportedParameters.HEALTH_CHECK_HTTP_ENDPOINT,
                                                                                   getDefaultHealthCheckHttpEndpoint(healthCheckType));
-        String readinessHealthCheckType = (String) PropertiesUtil.getPropertyValue(parametersList,
-                                                                                   SupportedParameters.READINESS_HEALTH_CHECK_TYPE, null);
-        String readinessHealthCheckHttpEndpoint = (String) PropertiesUtil.getPropertyValue(parametersList,
-                                                                                           SupportedParameters.READINESS_HEALTH_CHECK_HTTP_ENDPOINT,
-                                                                                           getDefaultHealthCheckHttpEndpoint(readinessHealthCheckType));
-        Integer readinessHealthCheckInvocationTimeout = (Integer) PropertiesUtil.getPropertyValue(parametersList,
-                                                                                                  SupportedParameters.READINESS_HEALTH_CHECK_INVOCATION_TIMEOUT,
-                                                                                                  null);
-        Integer readinessHealthCheckInterval = (Integer) PropertiesUtil.getPropertyValue(parametersList,
-                                                                                         SupportedParameters.READINESS_HEALTH_CHECK_INTERVAL,
-                                                                                         null);
         Boolean isSshEnabled = (Boolean) PropertiesUtil.getPropertyValue(parametersList, SupportedParameters.ENABLE_SSH, null);
         DockerInfo dockerInfo = new DockerInfoParser().parse(parametersList);
         return ImmutableStaging.builder()
@@ -51,10 +40,6 @@ public class StagingParametersParser implements ParametersParser<Staging> {
                                .invocationTimeout(healthCheckInvocationTimeout)
                                .healthCheckType(healthCheckType)
                                .healthCheckHttpEndpoint(healthCheckHttpEndpoint)
-                               .readinessHealthCheckType(readinessHealthCheckType)
-                               .readinessHealthCheckHttpEndpoint(readinessHealthCheckHttpEndpoint)
-                               .readinessHealthCheckTimeout(readinessHealthCheckInvocationTimeout)
-                               .readinessHealthCheckInterval(readinessHealthCheckInterval)
                                .isSshEnabled(isSshEnabled)
                                .dockerInfo(dockerInfo)
                                .build();
