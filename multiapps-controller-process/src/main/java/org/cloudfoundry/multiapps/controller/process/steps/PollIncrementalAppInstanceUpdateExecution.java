2d85b5cb9b6491481e276e436a0482b24a397497
IvanBorislavovDimitrov <starstrucks1997@gmail.com>
2025-01-29 09:39:00 +0200

    Fix comments

--- multiapps-controller-process/src/main/java/org/cloudfoundry/multiapps/controller/process/steps/PollIncrementalAppInstanceUpdateExecution.java
+++ multiapps-controller-process/src/main/java/org/cloudfoundry/multiapps/controller/process/steps/PollIncrementalAppInstanceUpdateExecution.java
@@ -54,7 +54,7 @@ public class PollIncrementalAppInstanceUpdateExecution implements AsyncExecution
                              .equals(CloudApplication.State.STOPPED)) {
             continueWithIncrementalScaling(context);
         } else {
-            pollApplicationIndexesBeforeContinuingWithIncrementalScaling(context);
+            setExecutionIndexForPollingNewAppInstances(context);
         }
     }
 
@@ -62,7 +62,7 @@ public class PollIncrementalAppInstanceUpdateExecution implements AsyncExecution
         context.setVariable(Variables.ASYNC_STEP_EXECUTION_INDEX, 2);
     }
 
-    private void pollApplicationIndexesBeforeContinuingWithIncrementalScaling(ProcessContext context) {
+    private void setExecutionIndexForPollingNewAppInstances(ProcessContext context) {
         context.setVariable(Variables.ASYNC_STEP_EXECUTION_INDEX, 1);
     }
 
