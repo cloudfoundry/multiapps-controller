d97e09dbd74bbab6ce26633ad8a213e386de9020
Stefan Yonkov <156345010+s-yonkov-yonkov@users.noreply.github.com>
2025-03-14 16:26:21 +0200

    Bump dependencies (#1608)
    * bump dependencies, fix failing unit tests

--- multiapps-controller-core/src/test/java/org/cloudfoundry/multiapps/controller/core/helpers/v2/ConfigurationReferencesResolverTest.java
+++ multiapps-controller-core/src/test/java/org/cloudfoundry/multiapps/controller/core/helpers/v2/ConfigurationReferencesResolverTest.java
@@ -1,174 +1,52 @@
 package org.cloudfoundry.multiapps.controller.core.helpers.v2;
 
-import static org.mockito.Mockito.when;
-
-import java.util.List;
-import java.util.Map;
 import java.util.stream.Stream;
 
-import org.cloudfoundry.multiapps.common.test.TestUtil;
 import org.cloudfoundry.multiapps.common.test.Tester;
 import org.cloudfoundry.multiapps.common.test.Tester.Expectation;
-import org.cloudfoundry.multiapps.common.util.JsonUtil;
-import org.cloudfoundry.multiapps.common.util.YamlParser;
-import org.cloudfoundry.multiapps.controller.core.test.MockBuilder;
-import org.cloudfoundry.multiapps.controller.core.util.ApplicationConfiguration;
-import org.cloudfoundry.multiapps.controller.persistence.model.CloudTarget;
-import org.cloudfoundry.multiapps.controller.persistence.model.ConfigurationEntry;
-import org.cloudfoundry.multiapps.controller.persistence.model.filters.ConfigurationFilter;
-import org.cloudfoundry.multiapps.controller.persistence.query.ConfigurationEntryQuery;
-import org.cloudfoundry.multiapps.controller.persistence.services.ConfigurationEntryService;
-import org.cloudfoundry.multiapps.mta.builders.v2.ParametersChainBuilder;
-import org.cloudfoundry.multiapps.mta.handlers.ConfigurationParser;
-import org.cloudfoundry.multiapps.mta.handlers.v2.DescriptorParser;
-import org.cloudfoundry.multiapps.mta.model.DeploymentDescriptor;
-import org.cloudfoundry.multiapps.mta.model.Platform;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
+import org.cloudfoundry.multiapps.controller.core.helpers.common.AbstractConfigurationReferencesResolverTest;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.mockito.Answers;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-
-public class ConfigurationReferencesResolverTest {
+public class ConfigurationReferencesResolverTest extends AbstractConfigurationReferencesResolverTest {
 
     private final Tester tester = Tester.forClass(getClass());
 
-    protected static class ServiceMockConfiguration {
-
-        ConfigurationFilter filter;
-        List<ConfigurationEntry> configurationEntries;
-
-    }
-
-    private static Platform platform;
-
-    @Mock
-    protected ConfigurationEntryService configurationEntryService;
-    @Mock(answer = Answers.RETURNS_SELF)
-    protected ConfigurationEntryQuery configurationEntryQuery;
-    @Mock
-    protected ApplicationConfiguration configuration;
-
-    protected DeploymentDescriptor descriptor;
-
-    static Stream<Arguments> testResolve() {
-        return Stream.of(
-                         // (1) Reference to existing provided dependency:
-                         Arguments.of("mtad-03.yaml", "configuration-entries-01.json",
-                                      new Expectation(Expectation.Type.JSON, "result-01.json")),
-                         // (2) Use new syntax:
-                         Arguments.of("mtad-05.yaml", "configuration-entries-01.json",
-                                      new Expectation(Expectation.Type.JSON, "result-01.json")),
-                         // (3) Use new syntax when more than one configuration entries are available:
-                         Arguments.of("mtad-05.yaml", "configuration-entries-05.json",
-                                      new Expectation(Expectation.Type.EXCEPTION,
-                                                      "Multiple configuration entries were found matching the filter specified in resource \"resource-2\"")),
-                         // (4) Use new syntax when more than one configuration entries are available:
-                         Arguments.of("mtad-07.yaml", "configuration-entries-06.json",
-                                      new Expectation(Expectation.Type.JSON, "result-02.json")),
-                         // (5) Use new syntax when there is no configuration entry available:
-                         Arguments.of("mtad-05.yaml", "configuration-entries-04.json",
-                                      new Expectation(Expectation.Type.EXCEPTION,
-                                                      "No configuration entries were found matching the filter specified in resource \"resource-2\"")),
-                         // (6) Use new syntax when there is no configuration entry available:
-                         Arguments.of("mtad-07.yaml", "configuration-entries-07.json",
-                                      new Expectation(Expectation.Type.JSON, "result-03.json")),
-                         // (7) Use new syntax (missing org parameter):
-                         Arguments.of("mtad-06.yaml", "configuration-entries-01.json",
-                                      new Expectation(Expectation.Type.EXCEPTION, "Could not find required property \"org\"")),
-                         // (8) Subscriptions should be created:
-                         Arguments.of("mtad-08.yaml", "configuration-entries-06.json",
-                                      new Expectation(Expectation.Type.JSON, "result-04.json")));
-    }
-
-    @BeforeAll
-    static void initializePlatform() {
-        ConfigurationParser parser = new ConfigurationParser();
-        platform = parser.parsePlatformJson(ConfigurationReferencesResolverTest.class.getResourceAsStream("/mta/xs-platform.json"));
-    }
-
-    @BeforeEach
-    void setUp() throws Exception {
-        MockitoAnnotations.openMocks(this)
-                          .close();
-    }
-
     @ParameterizedTest
-    @MethodSource
+    @MethodSource("testResolveSource")
     void testResolve(String descriptorLocation, String configurationEntriesLocation, Expectation expectation) {
-        prepareService();
-        prepareConfigurationEntries(configurationEntriesLocation);
-        prepareDeploymentDescriptor(descriptorLocation);
-        ConfigurationReferencesResolver referencesResolver = getConfigurationResolver(descriptor);
-
-        tester.test(() -> {
-
-            referencesResolver.resolve(descriptor);
-            return descriptor;
-
-        }, expectation);
+        executeTestResolve(tester, descriptorLocation, configurationEntriesLocation, expectation);
     }
 
-    protected void prepareService() {
-        when(configurationEntryService.createQuery()).thenReturn(configurationEntryQuery);
-    }
-
-    protected void prepareConfigurationEntries(String configurationEntriesLocation) {
-        List<ServiceMockConfiguration> serviceConfigurations = JsonUtil.fromJson(TestUtil.getResourceAsString(configurationEntriesLocation,
-                                                                                                              getClass()),
-                                                                                 new TypeReference<List<ServiceMockConfiguration>>() {
-                                                                                 });
-        for (ServiceMockConfiguration configuration : serviceConfigurations) {
-            ConfigurationFilter filter = configuration.filter;
-            ConfigurationEntryQuery configurationEntryQueryMock = getConfigurationEntryQueryMock(filter);
-            when(configurationEntryQueryMock.list()).thenReturn(configuration.configurationEntries);
-        }
-    }
-
-    private ConfigurationEntryQuery getConfigurationEntryQueryMock(ConfigurationFilter filter) {
-        return new MockBuilder<>(configurationEntryQuery).on(query -> query.providerNid(filter.getProviderNid()))
-                                                         .on(query -> query.providerId(filter.getProviderId()))
-                                                         .on(query -> query.version(filter.getProviderVersion()))
-                                                         .on(query -> query.target(filter.getTargetSpace()))
-                                                         .on(query -> query.requiredProperties(filter.getRequiredContent()))
-                                                         .on(query -> query.visibilityTargets(Mockito.any()))
-                                                         .build();
-    }
-
-    protected void prepareDeploymentDescriptor(String descriptorLocation) {
-        Map<String, Object> deploymentDescriptorMap = new YamlParser().convertYamlToMap(getClass().getResourceAsStream(descriptorLocation));
-        this.descriptor = getDescriptorParser().parseDeploymentDescriptor(deploymentDescriptorMap);
-    }
-
-    protected DescriptorParser getDescriptorParser() {
-        return new DescriptorParser();
-    }
-
-    protected ConfigurationReferencesResolver getConfigurationResolver(DeploymentDescriptor deploymentDescriptor) {
-        return new ConfigurationReferencesResolver(configurationEntryService,
-                                                   new ConfigurationFilterParser(getCloudTarget(),
-                                                                                 getPropertiesChainBuilder(descriptor),
-                                                                                 null),
-                                                   null,
-                                                   configuration);
-    }
-
-    protected CloudTarget getCloudTarget() {
-        String currentOrg = (String) platform.getParameters()
-                                             .get("org");
-        String currentSpace = (String) platform.getParameters()
-                                               .get("space");
-        return new CloudTarget(currentOrg, currentSpace);
-    }
-
-    protected ParametersChainBuilder getPropertiesChainBuilder(DeploymentDescriptor descriptor) {
-        return new ParametersChainBuilder(descriptor, platform);
+    static Stream<Arguments> testResolveSource() {
+        return Stream.of(
+                // (1) Reference to existing provided dependency:
+                Arguments.of("mtad-03.yaml", "configuration-entries-01.json",
+                        new Expectation(Expectation.Type.JSON, "result-01.json")),
+                // (2) Use new syntax:
+                Arguments.of("mtad-05.yaml", "configuration-entries-01.json",
+                        new Expectation(Expectation.Type.JSON, "result-01.json")),
+                // (3) Use new syntax when more than one configuration entries are available:
+                Arguments.of("mtad-05.yaml", "configuration-entries-05.json",
+                        new Expectation(Expectation.Type.EXCEPTION,
+                                "Multiple configuration entries were found matching the filter specified in resource \"resource-2\"")),
+                // (4) Use new syntax when more than one configuration entries are available:
+                Arguments.of("mtad-07.yaml", "configuration-entries-06.json",
+                        new Expectation(Expectation.Type.JSON, "result-02.json")),
+                // (5) Use new syntax when there is no configuration entry available:
+                Arguments.of("mtad-05.yaml", "configuration-entries-04.json",
+                        new Expectation(Expectation.Type.EXCEPTION,
+                                "No configuration entries were found matching the filter specified in resource \"resource-2\"")),
+                // (6) Use new syntax when there is no configuration entry available:
+                Arguments.of("mtad-07.yaml", "configuration-entries-07.json",
+                        new Expectation(Expectation.Type.JSON, "result-03.json")),
+                // (7) Use new syntax (missing org parameter):
+                Arguments.of("mtad-06.yaml", "configuration-entries-01.json",
+                        new Expectation(Expectation.Type.EXCEPTION, "Could not find required property \"org\"")),
+                // (8) Subscriptions should be created:
+                Arguments.of("mtad-08.yaml", "configuration-entries-06.json",
+                        new Expectation(Expectation.Type.JSON, "result-04.json")));
     }
 
 }
