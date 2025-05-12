08627acf7dca73d085152f960bf7c3fb4310bfbf
vkalapov <87693906+vkalapov@users.noreply.github.com>
2025-05-12 10:43:51 +0300

    Add more info for message

--- multiapps-controller-process/src/main/java/org/cloudfoundry/multiapps/controller/process/util/DeprecatedBuildpackChecker.java
+++ multiapps-controller-process/src/main/java/org/cloudfoundry/multiapps/controller/process/util/DeprecatedBuildpackChecker.java
@@ -14,9 +14,10 @@ import org.cloudfoundry.multiapps.mta.util.PropertiesUtil;
 @Named
 public class DeprecatedBuildpackChecker {
 
-    private static final String DEPRECATION_MESSAGE = "SAP Java Buildpack 1 has been deprecated and is scheduled for removal from the SAP BTP, Cloud Foundry environment on June 30, 2025!";
-    private static final String DEFAULT_BUILDPACK_MESSAGE = "Currently, the module types java.tomee and java default to SAP Java Buildpack 1 if no buildpack is explicitly specified. This behavior will remain in effect until the buildpack's removal.";
-    private static final String DEPRECATION_LINK = "For more information, see: https://help.sap.com/whats-new/cf0cb2cb149647329b5d02aa96303f56?Component=SAP+Java+Buildpack&Valid_as_Of=2025-04-01:2025-04-10&locale=en-US";
+    private static final String DEPRECATION_MESSAGE = "SAP Java Buildpack 1 has been deprecated and is going to be removed from SAP BTP, Cloud Foundry environment after June, 2025!";
+    private static final String DEFAULT_BUILDPACK_MESSAGE_1 = "If no buildpack is specified, sap_java_buildpack is currently applied by default for certain module types (java.tomee, java.tomcat, java).";
+    private static final String DEFAULT_BUILDPACK_MESSAGE_2 = "This default will change after June - we strongly recommend that you manually migrate to the supported buildpack in advance to avoid deployment issues.";
+    private static final String DEPRECATION_LINK = "https://help.sap.com/whats-new/cf0cb2cb149647329b5d02aa96303f56?Component=SAP+Java+Buildpack&Valid_as_Of=2025-04-01:2025-04-10&locale=en-US";
 
     private static final String DEPRECATED_BUILDPACK = "sap_java_buildpack";
 
@@ -48,16 +49,15 @@ public class DeprecatedBuildpackChecker {
 
     private void logDeprecationNotice(StepLogger stepLogger, List<String> appsWithDeprecatedBuildpacks) {
         String separator = "=".repeat(80);
-        String message = String.join("\n",
-                                     "==   ATTENTION:   ==",
-                                     separator,
-                                     DEPRECATION_MESSAGE,
-                                     DEFAULT_BUILDPACK_MESSAGE,
-                                     "Affected modules: " + appsWithDeprecatedBuildpacks,
-                                     DEPRECATION_LINK,
-                                     separator
-        );
-        stepLogger.warn(message);
+
+        stepLogger.warn("==   ATTENTION:   ==");
+        stepLogger.warn(separator);
+        stepLogger.warn(DEPRECATION_MESSAGE);
+        stepLogger.warn(DEFAULT_BUILDPACK_MESSAGE_1);
+        stepLogger.warn(DEFAULT_BUILDPACK_MESSAGE_2);
+        stepLogger.warn("Affected modules: " + appsWithDeprecatedBuildpacks);
+        stepLogger.warn("For more information, see: " + DEPRECATION_LINK);
+        stepLogger.warn(separator);
     }
 
 }
