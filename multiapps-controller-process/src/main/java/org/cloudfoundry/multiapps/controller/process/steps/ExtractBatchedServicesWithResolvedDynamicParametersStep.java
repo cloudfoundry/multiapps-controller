4cb6d87dafc75d6771895d61150ce0fbcced112a
Krasimir Kargov <43464252+karrgov@users.noreply.github.com>
2025-08-01 11:46:09 +0300

    Validation for using the same service-name for different resources (#1673)
    LMCROSSITXSADEPLOY-1661

--- multiapps-controller-process/src/main/java/org/cloudfoundry/multiapps/controller/process/steps/ExtractBatchedServicesWithResolvedDynamicParametersStep.java
+++ multiapps-controller-process/src/main/java/org/cloudfoundry/multiapps/controller/process/steps/ExtractBatchedServicesWithResolvedDynamicParametersStep.java
@@ -1,5 +1,6 @@
 package org.cloudfoundry.multiapps.controller.process.steps;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -7,8 +8,9 @@ import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import com.sap.cloudfoundry.client.facade.CloudControllerClient;
+import com.sap.cloudfoundry.client.facade.domain.CloudEntity;
 import jakarta.inject.Named;
-
 import org.cloudfoundry.multiapps.common.util.MiscUtil;
 import org.cloudfoundry.multiapps.controller.client.lib.domain.CloudServiceInstanceExtended;
 import org.cloudfoundry.multiapps.controller.client.lib.domain.ImmutableCloudServiceInstanceExtended;
@@ -23,8 +25,6 @@ import org.cloudfoundry.multiapps.mta.helpers.VisitableObject;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Scope;
 
-import com.sap.cloudfoundry.client.facade.CloudControllerClient;
-
 @Named("extractBatchedServicesWithResolvedDynamicParametersStep")
 @Scope(BeanDefinition.SCOPE_PROTOTYPE)
 public class ExtractBatchedServicesWithResolvedDynamicParametersStep extends SyncFlowableStep {
@@ -34,16 +34,21 @@ public class ExtractBatchedServicesWithResolvedDynamicParametersStep extends Syn
         getStepLogger().debug(Messages.EXTRACT_SERVICES_AND_RESOLVE_DYNAMIC_PARAMETERS_FROM_BATCH);
 
         Set<DynamicResolvableParameter> dynamicResolvableParameters = context.getVariable(Variables.DYNAMIC_RESOLVABLE_PARAMETERS);
-        List<CloudServiceInstanceExtended> servicesCalculatedForDeployment = context.getVariableBackwardsCompatible(Variables.BATCH_TO_PROCESS);
-        Set<DynamicResolvableParameter> dynamicParametersWithResolvedExistingInstances = resolveDynamicPramatersWithExistingInstances(context.getControllerClient(),
-                                                                                                                                      dynamicResolvableParameters,
-                                                                                                                                      servicesCalculatedForDeployment);
+        List<CloudServiceInstanceExtended> servicesCalculatedForDeployment = context.getVariableBackwardsCompatible(
+            Variables.BATCH_TO_PROCESS);
+        Set<DynamicResolvableParameter> dynamicParametersWithResolvedExistingInstances = resolveDynamicPramatersWithExistingInstances(
+            context.getControllerClient(),
+            dynamicResolvableParameters,
+            servicesCalculatedForDeployment);
 
         List<CloudServiceInstanceExtended> resolvedServiceInstances = servicesCalculatedForDeployment.stream()
-                                                                                                     .map(service -> resolveDynamicParametersOfServiceInstance(service,
-                                                                                                                                                               dynamicParametersWithResolvedExistingInstances))
+                                                                                                     .map(
+                                                                                                         service -> resolveDynamicParametersOfServiceInstance(
+                                                                                                             service,
+                                                                                                             dynamicParametersWithResolvedExistingInstances))
                                                                                                      .collect(Collectors.toList());
 
+        checkForDuplicatedServiceNameFields(resolvedServiceInstances);
         setServicesToCreate(context, resolvedServiceInstances);
         context.setVariable(Variables.DYNAMIC_RESOLVABLE_PARAMETERS, dynamicParametersWithResolvedExistingInstances);
         return StepPhase.DONE;
@@ -55,9 +60,9 @@ public class ExtractBatchedServicesWithResolvedDynamicParametersStep extends Syn
     }
 
     private Set<DynamicResolvableParameter>
-            resolveDynamicPramatersWithExistingInstances(CloudControllerClient client,
-                                                         Set<DynamicResolvableParameter> dynamicResolvableParameters,
-                                                         List<CloudServiceInstanceExtended> servicesCalculatedForDeployment) {
+    resolveDynamicPramatersWithExistingInstances(CloudControllerClient client,
+                                                 Set<DynamicResolvableParameter> dynamicResolvableParameters,
+                                                 List<CloudServiceInstanceExtended> servicesCalculatedForDeployment) {
         Map<String, String> existingServiceGuids = getExistingServiceGuidsIfNeeded(client, dynamicResolvableParameters,
                                                                                    servicesCalculatedForDeployment);
         Set<DynamicResolvableParameter> resolvedDynamicParameters = new HashSet<>(dynamicResolvableParameters);
@@ -65,7 +70,8 @@ public class ExtractBatchedServicesWithResolvedDynamicParametersStep extends Syn
             if (existingServiceGuids.containsKey(dynamicParameter.getRelationshipEntityName())) {
                 resolvedDynamicParameters.remove(dynamicParameter);
                 resolvedDynamicParameters.add(ImmutableDynamicResolvableParameter.copyOf(dynamicParameter)
-                                                                                 .withValue(existingServiceGuids.get(dynamicParameter.getRelationshipEntityName())));
+                                                                                 .withValue(existingServiceGuids.get(
+                                                                                     dynamicParameter.getRelationshipEntityName())));
             }
         }
         return resolvedDynamicParameters;
@@ -92,14 +98,16 @@ public class ExtractBatchedServicesWithResolvedDynamicParametersStep extends Syn
                                                   CloudServiceInstanceExtended serviceCalculatedForDeployment) {
         return dynamicResolvableParameters.stream()
                                           .anyMatch(dynamicParameter -> dynamicParameter.getRelationshipEntityName()
-                                                                                        .equals(serviceCalculatedForDeployment.getResourceName()));
+                                                                                        .equals(
+                                                                                            serviceCalculatedForDeployment.getResourceName()));
     }
 
     private CloudServiceInstanceExtended
-            resolveDynamicParametersOfServiceInstance(CloudServiceInstanceExtended service,
-                                                      Set<DynamicResolvableParameter> dynamicResolvableParameters) {
+    resolveDynamicParametersOfServiceInstance(CloudServiceInstanceExtended service,
+                                              Set<DynamicResolvableParameter> dynamicResolvableParameters) {
         DynamicParametersResolver resolver = new DynamicParametersResolver(service.getResourceName(),
-                                                                           new DynamicResolvableParametersHelper(dynamicResolvableParameters));
+                                                                           new DynamicResolvableParametersHelper(
+                                                                               dynamicResolvableParameters));
         Map<String, Object> resolvedServiceParameters = MiscUtil.cast(new VisitableObject(service.getCredentials()).accept(resolver));
         return ImmutableCloudServiceInstanceExtended.copyOf(service)
                                                     .withCredentials(resolvedServiceParameters);
@@ -109,11 +117,39 @@ public class ExtractBatchedServicesWithResolvedDynamicParametersStep extends Syn
     private void setServicesToCreate(ProcessContext context, List<CloudServiceInstanceExtended> servicesCalculatedForDeployment) {
 
         List<CloudServiceInstanceExtended> servicesToCreate = servicesCalculatedForDeployment.stream()
-                                                                                             .filter(CloudServiceInstanceExtended::isManaged)
+                                                                                             .filter(
+                                                                                                 CloudServiceInstanceExtended::isManaged)
                                                                                              .collect(Collectors.toList());
         getStepLogger().debug(Messages.SERVICES_TO_CREATE, SecureSerialization.toJson(servicesToCreate));
         context.setVariable(Variables.SERVICES_TO_CREATE, servicesToCreate);
         context.setVariable(Variables.SERVICES_TO_CREATE_COUNT, servicesToCreate.size());
+    }
+
+    private void checkForDuplicatedServiceNameFields(List<CloudServiceInstanceExtended> resolvedServiceInstances) {
+        List<String> resolvedServiceInstancesNames = resolvedServiceInstances.stream()
+                                                                             .map(CloudEntity::getName)
+                                                                             .toList();
+
+        List<String> duplicatedNames = getDuplicatedNames(resolvedServiceInstancesNames);
+        if (!duplicatedNames.isEmpty()) {
+            getStepLogger().warn(
+                Messages.ONLY_FIRST_SERVICE_WILL_BE_CREATED, String.join(" ", duplicatedNames));
+        }
+    }
+
+    private List<String> getDuplicatedNames(List<String> resolvedServiceInstancesNames) {
+        List<String> duplicatedNames = new ArrayList<>();
+        Map<String, Integer> frequencyOfNamesMap = new HashMap<>();
+        for (String name : resolvedServiceInstancesNames) {
+            frequencyOfNamesMap.merge(name, 1, Integer::sum);
+        }
+
+        for (String name : frequencyOfNamesMap.keySet()) {
+            if (frequencyOfNamesMap.get(name) > 1) {
+                duplicatedNames.add(name);
+            }
+        }
+        return duplicatedNames;
     }
 
 }
