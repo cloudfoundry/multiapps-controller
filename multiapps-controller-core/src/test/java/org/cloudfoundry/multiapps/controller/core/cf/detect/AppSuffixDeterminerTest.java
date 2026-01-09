2fe0804af52f334dec6dfb8ed97f27077869687c
Ivan Kasarov <ivan.kasarov@sap.com>
2026-01-09 16:54:06 +0200

    additional review changes

--- multiapps-controller-core/src/test/java/org/cloudfoundry/multiapps/controller/core/cf/detect/AppSuffixDeterminerTest.java
+++ multiapps-controller-core/src/test/java/org/cloudfoundry/multiapps/controller/core/cf/detect/AppSuffixDeterminerTest.java
@@ -12,7 +12,6 @@ class AppSuffixDeterminerTest {
 
     static Stream<Arguments> testAppSuffixDeterminer() {
         return Stream.of(
-            //@formatter:off
             // (1) Keep original app names is not set and the process is not after resume phase
             Arguments.of(false, false, false),
             // (2) Keep original app names is set but the process is not after resume phase
@@ -21,7 +20,6 @@ class AppSuffixDeterminerTest {
             Arguments.of(false, true, false),
             // (4) Keep original app names is set and the process is after resume phase
             Arguments.of(true, true, true)
-        //@formatter:on
         );
     }
 
