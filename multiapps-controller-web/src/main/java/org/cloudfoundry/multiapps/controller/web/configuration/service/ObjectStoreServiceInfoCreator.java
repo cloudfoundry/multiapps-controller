b6773e35480318343a24d3752db309bacec0d307
Yavor16 <yavoruzbg@gmail.com>
2026-02-19 14:00:22 +0200

    Adopt Azure sdk for Objectstore

--- multiapps-controller-web/src/main/java/org/cloudfoundry/multiapps/controller/web/configuration/service/ObjectStoreServiceInfoCreator.java
+++ multiapps-controller-web/src/main/java/org/cloudfoundry/multiapps/controller/web/configuration/service/ObjectStoreServiceInfoCreator.java
@@ -1,18 +1,15 @@
 package org.cloudfoundry.multiapps.controller.web.configuration.service;
 
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.List;
 import java.util.Map;
 
 import org.cloudfoundry.multiapps.controller.web.Constants;
-import org.cloudfoundry.multiapps.controller.web.Messages;
 
 public class ObjectStoreServiceInfoCreator {
 
     public List<ObjectStoreServiceInfo> getAllProvidersServiceInfo(Map<String, Object> credentials) {
         return List.of(createServiceInfoForAws(credentials), createServiceInfoForAliCloud(credentials),
-                       createServiceInfoForAzure(credentials), createServiceInfoForCcee(credentials));
+                       createServiceInfoForCcee(credentials), createServiceInfoForAzure(credentials), createServiceInfoForGcp(credentials));
     }
 
     private ObjectStoreServiceInfo createServiceInfoForAws(Map<String, Object> credentials) {
@@ -45,20 +42,6 @@ public class ObjectStoreServiceInfoCreator {
                                               .build();
     }
 
-    private ObjectStoreServiceInfo createServiceInfoForAzure(Map<String, Object> credentials) {
-        String accountName = (String) credentials.get(Constants.ACCOUNT_NAME);
-        String sasToken = (String) credentials.get(Constants.SAS_TOKEN);
-        String containerName = (String) credentials.get(Constants.CONTAINER_NAME);
-        URL containerUrl = getContainerUriEndpoint(credentials);
-        return ImmutableObjectStoreServiceInfo.builder()
-                                              .provider(Constants.AZUREBLOB)
-                                              .identity(accountName)
-                                              .credential(sasToken)
-                                              .endpoint(containerUrl == null ? null : containerUrl.toString())
-                                              .container(containerName)
-                                              .build();
-    }
-
     private ObjectStoreServiceInfo createServiceInfoForCcee(Map<String, Object> credentials) {
         String accessKeyId = (String) credentials.get(Constants.ACCESS_KEY_ID);
         String containerName = (String) credentials.get(Constants.CONTAINER_NAME_WITH_DASH);
@@ -75,15 +58,17 @@ public class ObjectStoreServiceInfoCreator {
                                               .build();
     }
 
-    private URL getContainerUriEndpoint(Map<String, Object> credentials) {
-        if (!credentials.containsKey(Constants.CONTAINER_URI)) {
-            return null;
-        }
-        try {
-            URL containerUri = new URL((String) credentials.get(Constants.CONTAINER_URI));
-            return new URL(containerUri.getProtocol(), containerUri.getHost(), containerUri.getPort(), "");
-        } catch (MalformedURLException e) {
-            throw new IllegalStateException(Messages.CANNOT_PARSE_CONTAINER_URI_OF_OBJECT_STORE, e);
-        }
+    private ObjectStoreServiceInfo createServiceInfoForAzure(Map<String, Object> credentials) {
+        return ImmutableObjectStoreServiceInfo.builder()
+                                              .provider(Constants.AZUREBLOB)
+                                              .credentials(credentials)
+                                              .build();
+    }
+
+    private ObjectStoreServiceInfo createServiceInfoForGcp(Map<String, Object> credentials) {
+        return ImmutableObjectStoreServiceInfo.builder()
+                                              .provider(Constants.GOOGLE_CLOUD_STORAGE)
+                                              .credentials(credentials)
+                                              .build();
     }
 }
