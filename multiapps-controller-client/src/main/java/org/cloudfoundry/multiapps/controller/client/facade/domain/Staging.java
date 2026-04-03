006048b7a0b878832ef2cde80d2e9773bba0ad05
Yavor16 <yavoruzbg@gmail.com>
2026-04-03 10:32:43 +0300

    Remove readniness env

--- multiapps-controller-client/src/main/java/org/cloudfoundry/multiapps/controller/client/facade/domain/Staging.java
+++ multiapps-controller-client/src/main/java/org/cloudfoundry/multiapps/controller/client/facade/domain/Staging.java
@@ -74,9 +74,6 @@ public interface Staging {
     @Nullable
     String getReadinessHealthCheckHttpEndpoint();
 
-    @Nullable
-    Boolean isReadinessHealthCheckEnabled();
-
     /**
      * @return boolean value to see if ssh is enabled
      */
