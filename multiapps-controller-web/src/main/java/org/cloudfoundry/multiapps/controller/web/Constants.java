e333111737bb2c8dc03e4721a1301520bb089441
IvanBorislavovDimitrov <starstrucks1997@gmail.com>
2026-05-20 13:12:25 +0300

    Use X-CF-True-Client-IP for rate limiting if available

--- multiapps-controller-web/src/main/java/org/cloudfoundry/multiapps/controller/web/Constants.java
+++ multiapps-controller-web/src/main/java/org/cloudfoundry/multiapps/controller/web/Constants.java
@@ -16,6 +16,7 @@ public class Constants {
     public static final String RATE_LIMIT = "X-RateLimit-Limit";
     public static final String RATE_LIMIT_REMAINING = "X-RateLimit-Remaining";
     public static final String RATE_LIMIT_RESET = "X-RateLimit-Reset";
+    public static final String X_CF_TRUE_CLIENT_IP_HEADER = "X-CF-True-Client-IP";
     public static final String CSRF_TOKEN = "X-CSRF-TOKEN";
     public static final String CSRF_PARAM_NAME = "X-CSRF-PARAM";
     public static final String CSRF_HEADER_NAME = "X-CSRF-HEADER";
