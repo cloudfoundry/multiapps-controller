09c663dc92bc6876147dfe12153ca869dce5e2d1
Krasimir Kargov <krasikargov0703@gmail.com>
2026-08-03 17:58:03 +0300

    Fix alphabetical order of netty
    LMCROSSITXSADEPLOY-3054

--- multiapps-controller-persistence/src/main/java/module-info.java
+++ multiapps-controller-persistence/src/main/java/module-info.java
@@ -81,5 +81,4 @@ open module org.cloudfoundry.multiapps.controller.persistence {
     requires software.amazon.awssdk.retries.api;
     requires static java.compiler;
     requires static org.immutables.value;
-    requires io.netty.handler;
 }
