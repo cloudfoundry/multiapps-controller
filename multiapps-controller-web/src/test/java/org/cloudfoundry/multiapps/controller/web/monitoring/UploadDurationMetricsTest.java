e1320efb16a4a5713f73617a85f33b992ead5b5b
Krasimir Kargov <krasikargov0703@gmail.com>
2026-08-06 13:40:29 +0300

    Fixing code smells
    LMCROSSITXSADEPLOY-3054

--- multiapps-controller-web/src/test/java/org/cloudfoundry/multiapps/controller/web/monitoring/UploadDurationMetricsTest.java
+++ multiapps-controller-web/src/test/java/org/cloudfoundry/multiapps/controller/web/monitoring/UploadDurationMetricsTest.java
@@ -5,10 +5,11 @@ import org.cloudfoundry.multiapps.controller.persistence.monitoring.UploadDurati
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 class UploadDurationMetricsTest {
 
@@ -30,137 +31,137 @@ class UploadDurationMetricsTest {
     void setUp() throws Exception {
         MockitoAnnotations.openMocks(this)
                           .close();
-        Mockito.when(uploadDurationTracker.getAppBinaryStatistics())
-               .thenReturn(appBinaryStatistics);
-        Mockito.when(uploadDurationTracker.getObjectStoreStatistics())
-               .thenReturn(objectStoreStatistics);
+        when(uploadDurationTracker.getAppBinaryStatistics())
+            .thenReturn(appBinaryStatistics);
+        when(uploadDurationTracker.getObjectStoreStatistics())
+            .thenReturn(objectStoreStatistics);
         uploadDurationMetrics = new UploadDurationMetrics(uploadDurationTracker, appUploaderInfo);
     }
 
     @Test
     void testGetAppBinaryUploadTotalCount() {
-        Mockito.when(appBinaryStatistics.totalCount())
-               .thenReturn(11L);
+        when(appBinaryStatistics.totalCount())
+            .thenReturn(11L);
         assertEquals(11L, uploadDurationMetrics.getAppBinaryUploadTotalCount());
-        Mockito.verify(appBinaryStatistics)
-               .totalCount();
+        verify(appBinaryStatistics)
+            .totalCount();
     }
 
     @Test
     void testGetAppBinaryUploadTimeoutCount() {
-        Mockito.when(appBinaryStatistics.timeoutCount())
-               .thenReturn(12L);
+        when(appBinaryStatistics.timeoutCount())
+            .thenReturn(12L);
         assertEquals(12L, uploadDurationMetrics.getAppBinaryUploadTimeoutCount());
-        Mockito.verify(appBinaryStatistics)
-               .timeoutCount();
+        verify(appBinaryStatistics)
+            .timeoutCount();
     }
 
     @Test
     void testGetAppBinaryUploadMaxDurationMs() {
-        Mockito.when(appBinaryStatistics.maxDurationMs())
-               .thenReturn(13L);
+        when(appBinaryStatistics.maxDurationMs())
+            .thenReturn(13L);
         assertEquals(13L, uploadDurationMetrics.getAppBinaryUploadMaxDurationMs());
-        Mockito.verify(appBinaryStatistics)
-               .maxDurationMs();
+        verify(appBinaryStatistics)
+            .maxDurationMs();
     }
 
     @Test
     void testGetAppBinaryUploadSumDurationMs() {
-        Mockito.when(appBinaryStatistics.sumDurationMs())
-               .thenReturn(14L);
+        when(appBinaryStatistics.sumDurationMs())
+            .thenReturn(14L);
         assertEquals(14L, uploadDurationMetrics.getAppBinaryUploadSumDurationMs());
-        Mockito.verify(appBinaryStatistics)
-               .sumDurationMs();
+        verify(appBinaryStatistics)
+            .sumDurationMs();
     }
 
     @Test
     void testGetAppUploaderActiveThreads() {
-        Mockito.when(appUploaderInfo.getActiveThreads())
-               .thenReturn(3);
+        when(appUploaderInfo.getActiveThreads())
+            .thenReturn(3);
         assertEquals(3, uploadDurationMetrics.getAppUploaderActiveThreads());
-        Mockito.verify(appUploaderInfo)
-               .getActiveThreads();
+        verify(appUploaderInfo)
+            .getActiveThreads();
     }
 
     @Test
     void testGetAppUploaderMaxThreads() {
-        Mockito.when(appUploaderInfo.getMaxThreads())
-               .thenReturn(7);
+        when(appUploaderInfo.getMaxThreads())
+            .thenReturn(7);
         assertEquals(7, uploadDurationMetrics.getAppUploaderMaxThreads());
-        Mockito.verify(appUploaderInfo)
-               .getMaxThreads();
+        verify(appUploaderInfo)
+            .getMaxThreads();
     }
 
     @Test
     void testGetAppUploaderRejectionCount() {
-        Mockito.when(appBinaryStatistics.rejectionCount())
-               .thenReturn(15L);
+        when(appBinaryStatistics.rejectionCount())
+            .thenReturn(15L);
         assertEquals(15L, uploadDurationMetrics.getAppUploaderRejectionCount());
-        Mockito.verify(appBinaryStatistics)
-               .rejectionCount();
+        verify(appBinaryStatistics)
+            .rejectionCount();
     }
 
     @Test
     void testGetAppUploaderRejectionCountInWindow() {
-        Mockito.when(appBinaryStatistics.rejectionsInWindow())
-               .thenReturn(16L);
+        when(appBinaryStatistics.rejectionsInWindow())
+            .thenReturn(16L);
         assertEquals(16L, uploadDurationMetrics.getAppUploaderRejectionCountInWindow());
-        Mockito.verify(appBinaryStatistics)
-               .rejectionsInWindow();
+        verify(appBinaryStatistics)
+            .rejectionsInWindow();
     }
 
     @Test
     void testGetObjectStoreUploadTotalCount() {
-        Mockito.when(objectStoreStatistics.totalCount())
-               .thenReturn(21L);
+        when(objectStoreStatistics.totalCount())
+            .thenReturn(21L);
         assertEquals(21L, uploadDurationMetrics.getObjectStoreUploadTotalCount());
-        Mockito.verify(objectStoreStatistics)
-               .totalCount();
+        verify(objectStoreStatistics)
+            .totalCount();
     }
 
     @Test
     void testGetObjectStoreUploadTimeoutCount() {
-        Mockito.when(objectStoreStatistics.timeoutCount())
-               .thenReturn(22L);
+        when(objectStoreStatistics.timeoutCount())
+            .thenReturn(22L);
         assertEquals(22L, uploadDurationMetrics.getObjectStoreUploadTimeoutCount());
-        Mockito.verify(objectStoreStatistics)
-               .timeoutCount();
+        verify(objectStoreStatistics)
+            .timeoutCount();
     }
 
     @Test
     void testGetObjectStoreUploadMaxDurationMs() {
-        Mockito.when(objectStoreStatistics.maxDurationMs())
-               .thenReturn(23L);
+        when(objectStoreStatistics.maxDurationMs())
+            .thenReturn(23L);
         assertEquals(23L, uploadDurationMetrics.getObjectStoreUploadMaxDurationMs());
-        Mockito.verify(objectStoreStatistics)
-               .maxDurationMs();
+        verify(objectStoreStatistics)
+            .maxDurationMs();
     }
 
     @Test
     void testGetObjectStoreUploadSumDurationMs() {
-        Mockito.when(objectStoreStatistics.sumDurationMs())
-               .thenReturn(24L);
+        when(objectStoreStatistics.sumDurationMs())
+            .thenReturn(24L);
         assertEquals(24L, uploadDurationMetrics.getObjectStoreUploadSumDurationMs());
-        Mockito.verify(objectStoreStatistics)
-               .sumDurationMs();
+        verify(objectStoreStatistics)
+            .sumDurationMs();
     }
 
     @Test
     void testGetAppBinaryUploadTimeoutsInWindow() {
-        Mockito.when(appBinaryStatistics.timeoutsInWindow())
-               .thenReturn(17L);
+        when(appBinaryStatistics.timeoutsInWindow())
+            .thenReturn(17L);
         assertEquals(17L, uploadDurationMetrics.getAppBinaryUploadTimeoutsInWindow());
-        Mockito.verify(appBinaryStatistics)
-               .timeoutsInWindow();
+        verify(appBinaryStatistics)
+            .timeoutsInWindow();
     }
 
     @Test
     void testGetObjectStoreUploadTimeoutsInWindow() {
-        Mockito.when(objectStoreStatistics.timeoutsInWindow())
-               .thenReturn(25L);
+        when(objectStoreStatistics.timeoutsInWindow())
+            .thenReturn(25L);
         assertEquals(25L, uploadDurationMetrics.getObjectStoreUploadTimeoutsInWindow());
-        Mockito.verify(objectStoreStatistics)
-               .timeoutsInWindow();
+        verify(objectStoreStatistics)
+            .timeoutsInWindow();
     }
 
 }
